8d8b2a04c475dff3c89a607e005bf166e117e451
Andrej Bauer <Andrej.Bauer@andrej.com>
2012-04-18 16:44:54 +0200

    Changed definition of paths so that the codomain is Set instead of Type.
    If we specify Type as the codomain of paths, Coq will actually infer that
    paths maps into Prop (which is one of the possible Type's...) This is very
    bad for HoTT. So it is better to specify that the codomain is Set. I am
    not sure what this implies in terms of universes and sizes. It seems
    strange that, for example, the identity type of a universe U should map
    into Set. Surely, U ~~> U ought to live at the same level as U? Nevertheless,
    this seems the best we can do with the current version of Coq.

--- Coq/Paths.v
+++ Coq/Paths.v
@@ -5,9 +5,18 @@ Require Export Functions.
 (** We define the space of paths so that it matches the definitionof Coq
    equality [eq], except that we put paths in [Type] instead of [Prop].
    Older versions of HoTT had a different definition, but we want this
-   one so that we can use Coq rewriting, which is a huge advantage. *)
+   one so that we can use Coq rewriting, which is a huge advantage.
 
-Inductive paths {A : Type} (x : A) : A -> Type := idpath : paths x x.
+   Note that we write [Set] as the codomain of [paths]. This seems ugly,
+   but if we write [Type] then Coq will infer that the codomain is [Prop]!
+   Thus, for example the definition of [identity] in [Coq.Init.Datatypes] 
+   lands in [Prop], even though the designers of the library probably
+   did not have that in mind.
+ *)
+
+Inductive paths {A : Type} (x : A) : A -> Set := idpath : paths x x.
+
+Implicit Arguments idpath [A].
 
 (* The next line tells [coqdoc] to print [paths] as an wigly arrow in LaTeX. *)
 (** printing ~~> $\leadsto$ *)
@@ -1041,3 +1050,9 @@ Ltac moveright_onleft :=
     | |- (?r ~~> ?q) =>
       path_via (r @ idpath _); apply concat_moveright_onleft
   end; do_opposite_opposite.
+
+Inductive bovine (A:Type) (a:A) : A -> Type :=
+|  cow : bovine A a a
+| bull : bovine A a a.
+
+
