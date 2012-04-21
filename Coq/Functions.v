7a983af57f4925294cb3dccc8b70b4507200b7ca
Andrej Bauer <Andrej.Bauer@andrej.com>
2012-04-21 22:44:30 +0200

    removed Functions.v (again?)

--- Coq/Functions.v
+++
@@ -1,9 +0,0 @@
-(* The identity map and composition. *)
-
-Definition idmap A := fun x : A => x.
-
-Definition compose {A B C} (g : B -> C) (f : A -> B) (x : A) := g (f x).
-
-(** printing o $\circ$ *)
-
-Notation "g 'o' f" := (compose g f) (left associativity, at level 37).
