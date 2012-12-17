e46a0a1a8ecb030b8608c024d749e021926d3307
Andrej Bauer <Andrej.Bauer@andrej.com>
2012-12-17 12:10:06 -0500

    conforms to new Paths

--- theories/Conjugation.v
+++ theories/Conjugation.v
@@ -4,7 +4,6 @@
 
 Require Import Common Paths.
 
-Import PathNotations.
 Open Scope path_scope.
 
 (** We shall use a conjugation-like operation which is not present in the standard library. *)
@@ -25,43 +24,43 @@ Proof.
   reflexivity.
 Qed.
 
-Lemma pmap_to_conjp {A B : Type} {f g : A -> B} (p : forall x, f x = g x) {x y : A} (q : x = y) :
-  pmap g q = conjp p (pmap f q).
+Lemma ap_to_conjp {A B : Type} {f g : A -> B} (p : forall x, f x = g x) {x y : A} (q : x = y) :
+  ap g q = conjp p (ap f q).
 Proof.
   destruct q.  unfold conjp.  simpl.
-  path_via ((p x)^-1 @ p x).  symmetry; apply concat_Vp.
-  apply whiskerR.  symmetry.  apply concat_p1.
+  path_via ((p x)^-1 @ p x).  apply inverse ; apply concat_Vp.
+  apply whiskerR.  apply inverse, concat_p1.
 Qed.
 
-Lemma conjp_pmap {A : Type} {f : A -> A} (p : forall x, f x = x) {x y : A} (q : x = y) :
-  conjp p (pmap f q) = q.
+Lemma conjp_ap {A : Type} {f : A -> A} (p : forall x, f x = x) {x y : A} (q : x = y) :
+  conjp p (ap f q) = q.
 Proof.
   destruct q.  unfold conjp.  simpl.
   path_via ((p x)^-1 @ p x). apply whiskerR.  apply concat_p1.
   apply concat_Vp.
 Qed.
 
-Lemma pmap1_to_conjp {A : Type} {f : A -> A} (p : forall x, idmap x = f x) {x y : A} (q : x = y) :
-  pmap f q = conjp p q.
+Lemma ap1_to_conjp {A : Type} {f : A -> A} (p : forall x, idmap x = f x) {x y : A} (q : x = y) :
+  ap f q = conjp p q.
 Proof.
-  path_via (conjp p (pmap idmap q)).
-  apply pmap_to_conjp.  apply pmap; apply pmap_idmap.
+  path_via (conjp p (ap idmap q)).
+  apply ap_to_conjp.  apply ap; apply ap_idmap.
 Defined.
 
 (* TEMPORARILY COMMENTED OUT.
-Lemma conjp_pmap_cancel {A B : Type} {f : A -> B} {g : B -> A}
+Lemma conjp_ap_cancel {A B : Type} {f : A -> B} {g : B -> A}
                 (p : forall x, g (f x) = x) {x y : A} (q : x = y) :
-      conjp p (pmap g (pmap f q)) = q.
+      conjp p (ap g (ap f q)) = q.
 Proof.
-  path_via (conjp p (pmap (compose g f) q)).
-  apply pmap.  symmetry.  apply (pmap_compose f g q).
+  path_via (conjp p (ap (compose g f) q)).
+  apply ap.  symmetry.  apply (ap_compose f g q).
   (* Todo: give, for here, a lemma that [conjp] preserves homotopy. *)
 Defined.
 
 (* Was not in the original file ? *)
 Lemma conj_canV {A B : Type} {f : A -> B} {g : B -> A} (p : forall x, x = g (f x))
       {x y : A} (q : x = y) :
-      pmap g (pmap f q) = conjp p q.
+      ap g (ap f q) = conjp p q.
 Proof.
   destruct q.
   symmetry; apply concat_Vp.
