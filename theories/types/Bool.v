1ce5fa5b580456ae264deee7724835dec9730f87
Andrej Bauer <Andrej.Bauer@andrej.com>
2012-12-23 10:48:23 -0500

    Added type/Bool.v

---
+++ theories/types/Bool.v
@@ -0,0 +1,35 @@
+(* -*- mode: coq; mode: visual-line -*- *)
+(** * Theorems about the booleans *)
+
+Require Import Common Paths Contractible Equivalences Funext types.Prod.
+
+Local Open Scope equiv_scope.
+
+Section BoolForall.
+  Variable P : bool -> Type.
+  
+  Let f (s : forall b, P b) := (s false, s true).
+
+  Let g (u : P false * P true) (b : bool) : P b :=
+    match b with
+      | false => fst u
+      | true => snd u
+    end.
+
+  Let eissect' `{FunextAxiom} : Sect f g.
+  Proof.
+    intro s; apply path_forall; intro b; destruct b; reflexivity.
+  Defined.
+
+  Let eisretr' : Sect g f := fun _ => eta_prod _.
+
+  Definition equiv_bool_forall_prod `{FunextAxiom} :
+    (forall b, P b) <~> P false * P true.
+  Proof.
+    exists f.
+    refine {| equiv_inv := g ; eisretr := eisretr' ; eissect := eissect' |}.
+    admit.
+    (* must first show that decidable types are in HSet. *)
+  Defined.
+
+End BoolForall.
