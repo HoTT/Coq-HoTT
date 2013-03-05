28a6158e50726a0e8ad9bd3cafe8cef1a61c1343
mikeshulman <viritrilbia@gmail.com>
2013-03-04 19:13:13 -0500

    Starting to try S^1 as a private inductive type

---
+++ theories/types/Circle.v
@@ -0,0 +1,126 @@
+(* -*- mode: coq; mode: visual-line -*- *)
+
+(** * Theorems about the circle S^1 *)
+
+Require Import Overture PathGroupoids Equivalences Trunc.
+Require Import Paths Forall Arrow Universe.
+Local Open Scope path_scope.
+Local Open Scope equiv_scope.
+Generalizable Variables X A B f g n.
+
+(* *** Definition of the circle. *)
+
+Module Circle.
+
+Local Inductive S1 : Type :=
+| base : S1.
+
+Axiom loop : base = base.
+
+Definition S1_rech (P : S1 -> Type) (b : P base) (l : loop # b = b)
+  : forall (x:S1), P x
+  := fun x => match x with base => b end.
+
+Axiom S1_rech_beta_loop
+  : forall (P : S1 -> Type) (b : P base) (l : loop # b = b),
+  apD (S1_rech P b l) loop = l.
+
+End Circle.
+
+Import Circle.
+
+(* *** The non-dependent eliminator *)
+
+Definition S1_rechnd (P : Type) (b : P) (l : b = b)
+  : S1 -> P
+  := S1_rech (fun _ => P) b (transport_const _ _ @ l).
+
+Definition S1_rechnd_beta_loop (P : Type) (b : P) (l : b = b)
+  : ap (S1_rechnd P b l) loop = l.
+Proof.
+  unfold S1_rechnd.
+  set (l' := transport_const loop b @ l).
+  path_via ((transport_const loop _)^
+    @ apD (S1_rech (fun _ => P) b l') loop).
+  apply moveL_Vp.  apply symmetry, apD_const.
+  apply moveR_Vp.  simpl.  fold l'.
+  refine (S1_rech_beta_loop (fun _ => P) _ _).
+Defined.
+
+(* *** The loop space of the circle is the Integers. *)
+
+Inductive Pos : Type :=
+| one : Pos
+| psucc : Pos -> Pos.
+
+Inductive Int : Type :=
+| neg : Pos -> Int
+| zero : Int
+| pos : Pos -> Int.
+
+Definition succ_int (z : Int) : Int
+  := match z with
+       | neg (psucc n) => neg n
+       | neg one => zero
+       | zero => pos one
+       | pos n => pos (psucc n)
+     end.
+
+Definition pred_int (z : Int) : Int
+  := match z with
+       | neg n => neg (psucc n)
+       | zero => neg one
+       | pos one => zero
+       | pos (psucc n) => pos n
+     end.
+
+Instance isequiv_succ_int : IsEquiv succ_int
+  := isequiv_adjointify succ_int pred_int _ _.
+Proof.
+  intros [[|n] | | [|n]]; reflexivity.
+  intros [[|n] | | [|n]]; reflexivity.
+Defined.
+
+Section AssumeUnivalence.
+
+Context `{Univalence} `{Funext}.
+
+Definition S1_code : S1 -> Type
+  := S1_rechnd Type Int (path_universe succ_int).
+
+Definition S1_encode (x:S1) : (base = x) -> S1_code x
+  := fun p => p # zero.
+
+Fixpoint loopexp {A : Type} {x : A} (p : x = x) (n : Pos) : (x = x)
+  := match n with
+       | one => p
+       | psucc n => loopexp p n @ p
+     end.
+
+Definition looptothe (z : Int) : (base = base)
+  := match z with
+       | neg n => loopexp (loop^) n
+       | zero => 1
+       | pos n => loopexp (loop) n
+     end.
+
+Definition S1_decode (x:S1) : S1_code x -> (base = x).
+Proof.
+  refine (S1_rech (fun x => S1_code x -> base = x) looptothe _ x).
+  apply path_forall; intros z; simpl in z.
+  refine (transport_arrow _ _ _ @ _).
+  refine (transport_paths_r loop _ @ _).
+  refine (whiskerR (ap _ (transport_compose idmap S1_code loop^ z)) loop @ _).
+  rewrite ap_V.
+  unfold S1_code; rewrite S1_rechnd_beta_loop.
+  rewrite path_universe_inverse.
+  rewrite transport_path_universe.
+  destruct z as [[|n] | | [|n]]; simpl.
+  by apply concat_pV_p.
+  by apply concat_pV_p.
+  by apply concat_Vp.
+  by apply concat_1p.
+  reflexivity.
+Defined.
+
+End AssumeUnivalence.
--- theories/types/Universe.v
+++ theories/types/Universe.v
@@ -2,12 +2,13 @@
 (** * Theorems about the universe, including the Univalence Axiom. *)
 
 Require Import Overture PathGroupoids Equivalences.
+Require Import HProp EquivalenceVarieties.
 Local Open Scope path_scope.
 Local Open Scope equiv_scope.
 
-(** *** Paths *)
-
 Generalizable Variables A B f.
+
+(** *** Paths *)
 
 Instance isequiv_path {A B : Type} (p : A = B)
   : IsEquiv (transport (fun X:Type => X) p)
@@ -18,6 +19,13 @@ Instance isequiv_path {A B : Type} (p : A = B)
 
 Definition equiv_path (A B : Type) (p : A = B) : A <~> B
   := BuildEquiv _ _ (transport (fun X:Type => X) p) _.
+
+Definition equiv_path_inverse `{Funext} (A B : Type) (p : A = B) :
+  transport idmap (p^) = (transport idmap p)^-1.
+Proof.
+  apply path_forall; intros b.
+  destruct p. reflexivity.
+Defined.
 
 Class Univalence := {
   isequiv_equiv_path :> forall (A B : Type), IsEquiv (equiv_path A B)
@@ -44,7 +52,27 @@ Definition isequiv_path_universe {A B : Type}
   : IsEquiv (@path_universe_uncurried A B)
  := _.
 
-Definition equiv_path_universe {A B : Type} : (A <~> B) <~> (A = B)
+Definition equiv_path_universe (A B : Type) : (A <~> B) <~> (A = B)
   := BuildEquiv _ _ (@path_universe_uncurried A B) isequiv_path_universe.
+
+Definition path_universe_inverse_uncurried `{Funext} {A B : Type} (f : A <~> B)
+  : (path_universe f)^ = path_universe (f^-1).
+Proof.
+  revert f. equiv_intro ((equiv_path_universe A B)^-1) p. simpl.
+  path_via (p^).
+  exact (inverse2 (eisretr (equiv_path_universe A B) p)).
+  (* This really ought to do it *)
+  refine ((eisretr (equiv_path_universe B A) p^)^ @ _).
+  (* But we have two different proofs that [transport idmap p^] is an equivalence, one coming from the fact that it is transporting along [p^], the other from the fact that it is the inverse of transporting along [p].  So we have to use the fact that [IsEquiv] is an HProp. *)
+  unfold path_universe. apply ap.
+  simpl; unfold compose, equiv_path; simpl. apply ap.
+  (* Why can't it find this instance? *)
+  set (K := hprop_isequiv (transport idmap p^)).
+  apply allpath_hprop.
+Defined.
+
+Definition path_universe_inverse `{Funext} `(f : A -> B) `{IsEquiv A B f}
+  : (path_universe f)^ = path_universe (f^-1)
+  := path_universe_inverse_uncurried (BuildEquiv A B f _).
 
 End Univalence.
