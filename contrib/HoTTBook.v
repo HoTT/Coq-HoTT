ad33ee5f0a75d3557c043b450cc8f9e781724fc4
Andrej Bauer <Andrej.Bauer@andrej.com>
2013-03-06 11:05:57 -0500

    Added contrib/HoTTBook.v

---
+++ contrib/HoTTBook.v
@@ -0,0 +1,6 @@
+(** This is a correspondence between theorem in The HoTT Book and the HoTT library. *)
+
+Require HoTT.
+
+Definition Book_2_5_4 (* page 43, label thm:path-prod *)
+  := @HoTT.types.Prod.equiv_path_prod.
