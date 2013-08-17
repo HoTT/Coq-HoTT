0336abd660c43fe26e0ce9ca03d9b6de41fc24f3
Jason Gross <jgross@mit.edu>
2013-08-17 12:53:32 -0400

    Autobuild documentation with travis
    Now that we have a `make html` target, we can make the html
    automatically on travis.  If travis builds something which is the same
    as origin's master, then we autocommit updated documentation and push to
    the gh-pages branch, where documentation is accessible at
    http://hott.github.io/HoTT/coqdoc-html/.
    
    Generating the files was a pain.  I record the process here, so that if
    we need to update the keys, there will be a reference.
    
    First, generate the keys that will be used
    ```bash
    $ ROOT="$(git rev-parse --show-toplevel)"
    $ ssh-keygen -t rsa -C "Travis-CI-bot@travis.fake" -f "$(readlink -f "$ROOT"/etc/ci/id_rsa)"
    ```
    When it prompts you for a passphrase, you must leave it blank.
    
    Then add the contents of "$ROOT"/etc/ci/id_rsa.pub as a deploy key to
    the HoTT repository at https://github.com/HoTT/HoTT/settings/keys.
    
    Now, following a modified version of
    http://about.travis-ci.org/docs/user/travis-pro/#How-can-I-encrypt-files-that-include-sensitive-data%3F,
    encrypt the id_rsa file:
    ```bash
    $ password=`cat /dev/urandom | head -c 10000 | openssl sha1`
    $ openssl aes-256-cbc -k "$password" -in "$ROOT"/etc/ci/id_rsa -out "$ROOT"/etc/ci/id_rsa.enc -a
    ```
    Now, the hard part (at least if you don't have sudo). Hope that you
    either have ruby and gem installed, or a version of ssh-keygen which
    supports the `-e -m` flag combination (check with `ssh-keygen -\?`).  If
    you have ruby and gem, then you can follow
    http://about.travis-ci.org/docs/user/encryption-keys/, and run
    ```bash
    $ gem install travis
    $ travis encrypt -r HoTT/HoTT "SSHKEY_PASSWORD=$password"
    ```
    and update the `secure:` part of the `.travis.yml` file with the resulting
    secret.  Otherwise, you can follow
    http://about.travis-ci.org/docs/user/travis-pro/#How-can-I-encrypt-files-that-include-sensitive-data%3F
    and run
    ```bash
    $ \curl https://api.travis-ci.org/repos/HoTT/HoTT/key | perl -pe 's/^{"key"://; s/^"//; s/"}$//' | sed s'/\\n/\n/g' > id_travis.pub
    $ ssh-keygen -e -m PKCS8 -f id_travis.pub > id_travis.pub.pem
    $ echo "SSHKEY_PASSWORD='$password'" | openssl rsautl -encrypt -pubin -inkey id_travis.pub.pem
    ```
    However, if `ssh-keygen` fails, complaining about `-m` being
    unrecognized, then you must clone or copy
    https://gist.github.com/thwarted/1024558
    (https://gist.github.com/1024558.git), install the required python
    dependencies (for me, pyasn1), and then run that on `id_travis.pub` to
    get the `id_travis.pub.pem` file, and then run the `openssl rsautl`
    commmand.  Either way, the output of that third command should be placed
    in the `.travis.yml` file, replacing the existing secret.
    
    Perhaps we should include these instructions somewhere in the
    repository?

---
+++ etc/ci/after_success.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# in case we're run from out of git repo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" 1>/dev/null
+
+# only make if we should ($UPDATE_HTML is not empty) and we're the same as origin/master
+if [ ! -z "$UPDATE_HTML" ]; then
+    if [ -z "$(git diff origin/master)" ]; then
+        # make the ssh.sh file; use `source`, so that we get the GIT_SSH variable
+        source ./make_ssh.sh
+        # make the html and push it, if we should
+        ./generate_and_push_doc.sh
+    fi
+fi
+
+popd 1>/dev/null
---
+++ etc/ci/before_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# in case we're run from out of git repo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" 1>/dev/null
+
+# install coq
+./install_coq.sh
+
+popd 1>/dev/null
