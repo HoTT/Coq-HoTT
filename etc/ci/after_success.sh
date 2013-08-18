082425c2b6633dc66ebe2365b860e2ad88c5610f
Jason Gross <jgross@mit.edu>
2013-08-18 12:05:15 -0400

    Use OAuth instead of ssh to autogen doc on travis
    This is a much simpler way of doing things.
    
    Now that we have a `make html` target, we can make the html
    automatically on travis.  If travis builds something which is the same
    as origin's master, then we autocommit updated documentation and push to
    the gh-pages branch, where documentation is accessible at
    http://hott.github.io/HoTT/coqdoc-html/.
    
    Generating the files was much less of a pain.  I record the process
    here, so that if we need to update the keys, there will be a reference.
    
    First, generate the OAuth token that will be used, replacing
    '<username>' with your username.  You must have permission to push to
    HoTT/HoTT.
    ```bash
    $ TOKEN="$(curl -u '<username>' -d '{"scopes":["repo"],"note":"For Travis-CI Bot"}' https://api.github.com/authorizations | grep -o 'token": "[^"]*' | sed s'/token": "//g')"
    ```
    It will prompt you for your password.
    
    Now, the hard part (at least if you don't have sudo). Hope that you
    either have ruby and gem installed, or a version of ssh-keygen which
    supports the `-e -m` flag combination (check with `ssh-keygen -\?`).  If
    you have ruby and gem, then you can follow
    http://about.travis-ci.org/docs/user/encryption-keys/, and run
    ```bash
    $ gem install travis
    $ travis encrypt -r HoTT/HoTT "OAUTH_TOKEN=$TOKEN"
    ```
    and update the `secure:` part of the `.travis.yml` file with the resulting
    secret.  Otherwise, you can follow
    http://about.travis-ci.org/docs/user/travis-pro/#How-can-I-encrypt-files-that-include-sensitive-data%3F
    and run
    ```bash
    $ \curl https://api.travis-ci.org/repos/HoTT/HoTT/key | perl -pe 's/^{"key"://; s/^"//; s/"}$//' | sed s'/\\n/\n/g' > id_travis.pub
    $ ssh-keygen -e -m PKCS8 -f id_travis.pub > id_travis.pub.pem
    $ echo "OAUTH_TOKEN=$TOKEN" | openssl rsautl -encrypt -pubin -inkey id_travis.pub.pem
    ```
    However, if `ssh-keygen` fails, complaining about `-m` being
    unrecognized, then you must clone or copy
    https://gist.github.com/thwarted/1024558
    (https://gist.github.com/1024558.git), install the required python
    dependencies (for me, pyasn1), and then run that on `id_travis.pub` to
    get the `id_travis.pub.pem` file, and then run the `openssl rsautl`
    commmand.  Either way, the output of that third command should be placed
    in the `.travis.yml` file, replacing the existing secret.
    
    Perhaps we should include these instructions somewhere in the
    repository?

--- etc/ci/after_success.sh
+++ etc/ci/after_success.sh
@@ -7,8 +7,6 @@ pushd "$DIR" 1>/dev/null
 # only make if we should ($UPDATE_HTML is not empty) and we're the same as origin/master
 if [ ! -z "$UPDATE_HTML" ]; then
     if [ -z "$(git diff origin/master)" ]; then
-        # make the ssh.sh file; use `source`, so that we get the GIT_SSH variable
-        source ./make_ssh.sh
         # make the html and push it, if we should
         ./generate_and_push_doc.sh
     fi
