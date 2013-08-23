b12a62812fe3cbbba3e70930adc1739be20dd84d
Jason Gross <jgross@mit.edu>
2013-08-22 23:45:35 -0400

    Update autogen.sh to get files from git
    But only if we don't have autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
 autoreconf -fvi
+if test $? -ne 0
+then
+    echo 'Warning: autoreconf failed.  Falling back on git.'
+    if test -d .git
+    then
+	git remote update
+	FILES=`cat etc/autoreconf-files`
+	BRANCH=`cat etc/autoreconf-branch`
+	git checkout $BRANCH $FILES
+	if test $? -ne 0 # we failed to find the branch, so try to get it remotely
+	then
+	    git remote add autogen-temp-upstream git://github.com:HoTT/HoTT.git
+	    git remote update
+	    git checkout autogen-temp-upstream/$BRANCH $FILES
+	    if test $? -ne 0
+	    then
+		echo 'Error: Failed to get autoreconf files.  Try installing autoconf or autoreconf.'
+	    fi
+	    git remote rm autogen-temp-upstream
+	fi
+    else
+	echo 'Error: autoreconf failed, and you are not using git.  Try installing autoconf or autoreconf.'
+    fi
+fi
