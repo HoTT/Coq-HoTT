4179b8f7936f4da471605cf7d2ff34c106ec2320
Jason Gross <jgross@mit.edu>
2013-08-26 20:51:13 -0400

    Only build the bundled coq with travis

--- etc/ci/before_script.sh
+++ etc/ci/before_script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# in case we're run from out of git repo
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd "$DIR" 1>/dev/null
-
-# install coq
-./install_coq.sh
-
-popd 1>/dev/null
+sudo apt-get update -q
+sudo apt-get install -q ocaml camlp5 sed grep
+if [ ! -z "$WITH_AUTORECONF" ]; then
+    sudo apt-get install -q dh-autoreconf
+fi
