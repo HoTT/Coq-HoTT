936372ec9aae1152545aa8984e0250f4556a05ec
Bas Spitters <spitters@cs.ru.nl>
2013-09-01 09:13:20 +0200

    Adding more documentation, applying JasonGross suggestions.

--- theories/HProp.v
+++ theories/HProp.v
@@ -20,15 +20,13 @@ Lemma contr_inhabited_hprop (A : Type) `{H : IsHProp A} (x : A)
 Proof.
   exists x.
   intro y.
-  simpl in H.
   apply center, H.
 Defined.
 
 (** If inhabitation implies contractibility, then we have an h-proposition.  We probably won't often have a hypothesis of the form [A -> Contr A], so we make sure we give priority to other instances. *)
 Instance hprop_inhabited_contr (A : Type) : (A -> Contr A) -> IsHProp A | 10000.
 Proof.
-  intro H.
-  intros x y.
+  intros H x y.
   pose (C := H x).
   apply contr_paths_contr.
 Defined.
@@ -156,10 +154,20 @@ Defined.
 (** We use both the Sigma type of propositions and the record type of propositions.
 The latter allows us to automatically find a proof that a type is a proposition.
 
-Setting things up in this way allows us to define coercions from one type to the other.
-So, a theory stated about one type can be apply to the other. It's not magic, but it is convenient.
+At first, we may just want to look at the record type hProp and (sigT IsHProp).
+However, the generalization with total2 will allow us to define two coercions.
+So, a theorem about one type can be applied to the other. It's not magic, but it is convenient.
 
-We should explore this some more and then apply this to more record types.
+We make total2 a Class so that sig_to_total2 can be an instance.
+Also, this seems to be convenient to make tp1 and tp2 into coercions.
+
+We should explore this some more and then apply this to other record types.
+
+For now, it allows us to finish the proofs in epi.v which explicitly use the type 
+hProp of all propositions, as opposed to just individual propositions.
+
+More experiments with Canonical Structures (for Equivs) can be found in the deprecated
+ssreflect branch.
 *)
 
 Class total2 {T} (P: T -> Type) := tpair2 { tp1 :> T ; tp2 :> P tp1 }.
@@ -168,7 +176,8 @@ Lemma total2_to_sig {T} (P: T -> Type): total2 P -> sigT P.
 intros [p1 p2]. exact (p1;p2).
 Defined.
 
-Lemma sig_to_total2 {T} (P: T -> Type): sigT P -> total2 P.
+(** sigT will be accepted as a total2 *)
+Instance sig_to_total2 {T} (P: T -> Type): sigT P -> total2 P.
 intros [p1 p2]. exact {| tp1 := p1; tp2 := p2 |}.
 Defined.
 
@@ -183,20 +192,26 @@ Defined.
 (** Now we can write P-> Q for propositions *)
 Coercion hProp2Type: hProp >-> Sortclass.
 
+(* Here is a simple example.
+Variable P:Type.
+Variable p:(IsHProp P).
+Goal hProp.
+exact (P;_).*)
+
 (** We want to find side conditions automatically. *)
-Global Instance hProp2IsHProp (P: hProp): IsHProp P.
-destruct P as [? p4]. exact p4.
+Instance hProp2IsHProp (P: hProp): IsHProp P.
+apply tp2.
 Defined.
 
 Hint Resolve hProp2IsHProp.
 
-(* It is important to give the type :hProp explicitly to avoid universe inconsistencies in epi.v *)
+(** It is important to give the type :hProp explicitly to avoid universe inconsistencies in epi.v *)
 Canonical Structure default_HProp (T:Type)(P:IsHProp T) :hProp:= (@tpair2 Type IsHProp T P).
 (** In this way we can write e.g.
 <<
 (default_HProp Unit _)
 >>
-For the Unit proposition. Coq will automatically a proof that it is a proposition.)
+For the Unit proposition. Coq will automatically find a proof that it is a proposition.)
 *)
 
 Definition equiv_issig_total {T} {P:T->Type}
