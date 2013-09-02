c1540bc49830b3149e16a49220148138546ae55a
Bas Spitters <spitters@cs.ru.nl>
2013-09-02 21:55:45 +0200

    Adding documentation on Canonical Structures

--- theories/HProp.v
+++ theories/HProp.v
@@ -153,6 +153,8 @@ Defined.
 (** The type of Propositions *)
 (** Every hProp is a subclass of IsHProp *)
 Record hProp := hp { hproptype : Type ; isp : IsHProp hproptype}.
+(** This one would allow us to turn the record type of contractible types into an hProp.
+Canonical Structure default_HProp:= fun T P => (@hp T P).*)
 Existing Instance isp.
 (** Avoid hproptype, isp automatically eating a type *)
 Arguments hproptype :clear implicits.
