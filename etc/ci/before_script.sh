0f550fc2bb2e1e813cdc3042046f16ac3e84b5b9
Jason Gross <jgross@mit.edu>
2013-09-07 17:36:44 -0400

    Change to the right directory for travis

--- etc/ci/before_script.sh
+++ etc/ci/before_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# in case we're run from out of git repo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" 1>/dev/null
+
 sudo apt-get update -q
 sudo apt-get install -q ocaml camlp5 sed grep
 if [ ! -z "$WITH_AUTORECONF" ]; then
@@ -9,3 +13,5 @@ fi
 if [ ! -z "$COQBIN" ]; then
     ./install_coq.sh
 fi
+
+popd 1>/dev/null
