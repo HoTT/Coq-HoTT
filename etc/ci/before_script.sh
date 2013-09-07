61c89322878b18229948c8e3beafff3b1096dfbc
Jason Gross <jgross@mit.edu>
2013-09-07 19:09:40 -0400

    Be a bit noiser about changing directories

--- etc/ci/before_script.sh
+++ etc/ci/before_script.sh
@@ -2,7 +2,7 @@
 
 # in case we're run from out of git repo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd "$DIR" 1>/dev/null
+pushd "$DIR"
 
 sudo apt-get update -q
 sudo apt-get install -q ocaml camlp5 sed grep
@@ -10,8 +10,9 @@ if [ ! -z "$WITH_AUTORECONF" ]; then
     sudo apt-get install -q dh-autoreconf
 fi
 
+pwd
 if [ ! -z "$COQBIN" ]; then
     ./install_coq.sh
 fi
 
-popd 1>/dev/null
+popd
