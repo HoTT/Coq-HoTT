89559348b8bbeef3ae1318876a57d05538b6404f
Andrej Bauer <Andrej.Bauer@andrej.com>
2013-09-26 16:57:50 +0900

    Make separate etc/install_coq.sh script for users

---
+++ etc/install_coq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# in case we're run from out of git repo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" 1>/dev/null
+
+# now change to the git root
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+pushd "$ROOT_DIR" 1>/dev/null
+
+if test ! -d .git
+then
+    echo 'Error: .git directory does not exist.'
+    echo 'This script only works on a git clone of the HoTT repository.'
+    exit 1
+fi
+
+echo '$ git submodule sync'
+git submodule sync
+echo '$ git submodule update --init --recursive'
+git submodule update --init --recursive
+
+pushd coq-HoTT
+echo '$ ./configure -j -local '"$@"
+./configure -j -local "$@"
+echo '$ make coqlight'
+make coqlight
+popd
+
+popd 1>/dev/null
+popd 1>/dev/null
