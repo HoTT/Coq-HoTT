a00a2129255eb8978a50ab5e2677430d44376239
Jason Gross <jgross@mit.edu>
2013-10-20 02:08:03 -0400

    Update Notations file

--- theories/categories/Notations.v
+++ theories/categories/Notations.v
@@ -1,3 +1,6 @@
 Require Export Category.Notations.
 Require Export Functor.Notations.
 Require Export NaturalTransformation.Notations.
+Require Export FunctorCategory.Notations.
+Require NatCategory.
+Export NatCategory.Notations.
