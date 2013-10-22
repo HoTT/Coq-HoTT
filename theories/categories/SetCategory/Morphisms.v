0ec43a4ba0d2377e9d9eb77d1eb603c6856676f5
Jason Gross <jgross@mit.edu>
2013-10-22 01:21:41 -0400

    Prove that the precategory of sets is a category

--- theories/categories/SetCategory/Morphisms.v
+++ theories/categories/SetCategory/Morphisms.v
@@ -1,7 +1,8 @@
 (** * Morphisms in [set_cat] *)
 Require Import Category.Core Category.Morphisms.
+Require Import Category.Univalent.
 Require Import SetCategory.Core.
-Require Import HSet HProp Equivalences.
+Require Import Overture types.Record types.Sigma HProp HSet types.Universe Equivalences HoTT.Misc.
 
 Set Universe Polymorphism.
 Set Implicit Arguments.
@@ -58,6 +59,14 @@ Section equiv_iso_set_cat.
         apply allpath_hprop
       ).
   Defined.
+
+  Lemma path_idtoequiv_idtoiso (s d : set_cat) (p : s = d)
+  : iso_equiv s d (equiv_path _ _ (ap setT p)) = idtoiso set_cat p.
+  Proof.
+    apply path_isomorphic.
+    case p.
+    reflexivity.
+  Defined.
 End equiv_iso_set_cat.
 
 Section equiv_iso_prop_cat.
@@ -105,4 +114,31 @@ Section equiv_iso_prop_cat.
         apply allpath_hprop
       ).
   Defined.
+
+  Lemma path_idtoequiv_idtoiso_prop (s d : prop_cat) (p : s = d)
+  : iso_equiv_prop s d (equiv_path _ _ (ap hproptype p)) = idtoiso prop_cat p.
+  Proof.
+    apply path_isomorphic.
+    case p.
+    reflexivity.
+  Defined.
 End equiv_iso_prop_cat.
+
+Local Close Scope morphism_scope.
+Instance iscategory_set_cat `{Funext, Univalence}
+: IsCategory set_cat.
+Proof.
+  intros C D.
+  eapply @isequiv_homotopic; [ | intro; apply path_idtoequiv_idtoiso ].
+  change (IsEquiv (iso_equiv C D o equiv_path C D o @ap _ _ setT C D)).
+  typeclasses eauto.
+Defined.
+
+Instance iscategory_prop_cat `{Funext, Univalence}
+: IsCategory prop_cat.
+Proof.
+  intros C D.
+  eapply @isequiv_homotopic; [ | intro; apply path_idtoequiv_idtoiso_prop ].
+  change (IsEquiv (iso_equiv_prop C D o equiv_path C D o @ap _ _ hproptype C D)).
+  typeclasses eauto.
+Defined.
