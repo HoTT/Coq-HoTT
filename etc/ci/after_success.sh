971a51ddd94cbf3c39a6a9146be0a1f6a3a003d0
Jason Gross <jgross@mit.edu>
2013-11-13 21:49:16 -0500

    More dependency fixing

--- etc/ci/after_success.sh
+++ etc/ci/after_success.sh
@@ -6,6 +6,7 @@ pushd "$DIR" 1>/dev/null
 
 # only make if we should ($UPDATE_HTML is not empty) and we're the same as origin/master
 if [ ! -z "$UPDATE_HTML" ]; then
+    git remote update
     if [ -z "$(git diff origin/master)" ]; then
         # make the html and push it, if we should
         ./generate_and_push_doc.sh
