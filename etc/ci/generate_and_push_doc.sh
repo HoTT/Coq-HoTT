386b79d6c1f24c0f1bbd16a2c33162b3c52e8af3
Jason Gross <jgross@mit.edu>
2013-11-13 22:19:21 -0500

    Try harder to make, also use py 3.3 for unicode

--- etc/ci/generate_and_push_doc.sh
+++ etc/ci/generate_and_push_doc.sh
@@ -41,7 +41,7 @@ Generated with \`make html proviola\`"
 
 echo '$ make html'
 make html
-make proviola -j16
+make proviola -j16 -k
 mv proviola-html proviola-html-bak
 echo '$ git checkout -b gh-pages upstream/gh-pages'
 git checkout -b gh-pages upstream/gh-pages
