d8b8d9142dd39c837709243dfbb35bc97ba52123
Andrej Bauer <Andrej.Bauer@andrej.com>
2013-11-20 20:52:28 +0100

    Separate ci/install_coq_deps.sh and install_coq_deps.sh

--- etc/ci/install_coq_deps.sh
+++ etc/ci/install_coq_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update -q
-sudo apt-get install -q sed grep wget tar xsltproc mercurial python-lxml python-pexpect libxml2-dev libxslt1-dev ocaml camlp5 liblablgtk2-ocaml-dev liblablgtksourceview2-ocaml-dev
+sudo apt-get install -q sed grep wget tar xsltproc mercurial python-lxml python-pexpect libxml2-dev libxslt1-dev ocaml camlp5
