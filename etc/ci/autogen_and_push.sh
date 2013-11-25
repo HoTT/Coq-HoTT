2abb9066a6bd02836f083820a8edbd3ccfb7c17f
Jason Gross <jgross@mit.edu>
2013-11-25 15:44:29 -0500

    Improve travis scripts
    Now we will always build html, even on pull requests, and the checking
    for whether or not to push is more robust.

--- etc/ci/autogen_and_push.sh
+++ etc/ci/autogen_and_push.sh
@@ -6,24 +6,12 @@ pushd "$DIR" 1>/dev/null
 
 # now change to the git root
 ROOT_DIR="$(git rev-parse --show-toplevel)"
-pushd "$ROOT_DIR" 1>/dev/null
+cd "$ROOT_DIR" 1>/dev/null
 
-"$DIR"/add_upstream.sh
+EXTRA_ARGS="$("$DIR"/check_should_dry_run.sh "$@")"
+
 # copy the push_remote script so it stays around after we change branches
-cp "$DIR"/{push_remote,push_remote_tmp}.sh
-
-if [ -z "$UPDATE_HTML" ]; then
-    echo 'Not making html becuase $UPDATE_HTML variable not set.'
-    exit 0
-fi
-
-# only make the autogoo with -f, or if we're the same as origin/master
-if [ "$1" != "-f" ]; then
-    if [ ! -z "$(git diff origin/master)" ]; then
-	echo "Not making autogen beause we do not match with origin/master; call '$0 -f' to force"
-	exit 0
-    fi
-fi
+cp -f "$DIR"/{push_remote,push_remote_tmp}.sh
 
 echo 'Configuring git for pushing...'
 git config --global user.name "Travis-CI Bot"
@@ -41,9 +29,8 @@ git add -f $FILES
 echo '$ git commit -am "'"$MESSAGE"'"'
 git commit -m "$MESSAGE"
 # use the copy of the script which stayed around when we changed branches
-source "$DIR"/push_remote_tmp.sh $BRANCH:$BRANCH -f
+"$DIR"/push_remote_tmp.sh $BRANCH:$BRANCH -f $EXTRA_ARGS
 
 git checkout HEAD@{2} -f
 
-popd 1>/dev/null
 popd 1>/dev/null
---
+++ etc/ci/check_should_dry_run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# in case we're run from out of git repo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" 1>/dev/null
+
+# now change to the git root
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+cd "$ROOT_DIR" 1>/dev/null
+
+BAD_REMOTES="$(git remote -v | grep origin | grep -v 'github.com/HoTT/HoTT')"
+
+"$DIR"/add_upstream.sh 1>&2
+
+git remote update 1>&2
+
+UPSTREAM_DIFF="$(git log HEAD..upstream/master)"
+
+EXTRA_ARGS=""
+
+# only push for real if we line up
+# only push if match upstream/master
+if [ "$1" != "-f" ]; then
+    if [ ! -z "$BAD_REMOTES" ]; then
+	echo 'Not pushing html because there are remotes which are not HoTT/HoTT:' 1>&2
+	echo "$BAD_REMOTES" 1>&2
+	EXTRA_ARGS="--dry-run"
+    fi
+
+    # only make the errata if we're the same as upstream/master
+    if [ ! -z "$UPSTREAM_DIFF" ]; then
+	echo "Not pushing html beause we do not match with upstream/master; call '$0 -f' to force" 1>&2
+	EXTRA_ARGS="--dry-run"
+    fi
+fi
+
+echo "$EXTRA_ARGS"
+
+popd 1>/dev/null
