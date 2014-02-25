9326f927656e293f39ebb1d99d4518709719d0c4
Jason Gross <jgross@mit.edu>
2014-02-25 12:30:21 -0500

    Make dpd_graphs

--- etc/ci/before_script.sh
+++ etc/ci/before_script.sh
@@ -28,5 +28,8 @@ if [ ! -z "$UPDATE_DEP_GRAPHS" ]; then
     ./install_dep_graphs_deps.sh
 fi
 ./install_coq.sh -prefix /usr/local -debug
+if [ ! -z "$UPDATE_DEP_GRAPHS" ]; then
+    ./make_dpd_graphs.sh
+fi
 
 popd 1>/dev/null
