f2b1a295dcd4f8be7496bf148ce330a4368ba5b0
Jason Gross <jgross@mit.edu>
2014-02-26 12:31:37 -0500

    Add another example to 5.2
    This one is a bit less robust in the face of CoqMT, but has the functions
    satisfying the same recurrence judgmentally.

--- contrib/HoTTBookExercises.v
+++ contrib/HoTTBookExercises.v
@@ -553,6 +553,7 @@ End Book_4_5.
 (** Exercise 5.2 *)
 
 Section Book_5_2.
+  (** Here is one example of functions which are propositionally equal but not judgmentally equal.  They satisfy the same reucrrence propositionally. *)
   Let ez : Bool := true.
   Let es : nat -> Bool -> Bool := fun _ => idmap.
   Definition Book_5_2_i : nat -> Bool := nat_rect (fun _ => Bool) ez es.
@@ -563,6 +564,25 @@ Section Book_5_2.
     induction n; simpl; trivial.
   Defined.
 End Book_5_2.
+
+Section Book_5_2'.
+  (** Here's another example where two functions are not (currently) definitionally equal, but satisfy the same reucrrence judgmentally.  This example is a bit less robust; it fails in CoqMT. *)
+  Let ez : nat := 1.
+  Let es : nat -> nat -> nat := fun _ => S.
+  (** This should be moved to some other place. *)
+  Fixpoint plus (n m : nat) :=
+    match n with
+      | O => m
+      | S p => S (plus p m)
+    end.
+  Definition Book_5_2'_i : nat -> nat := fun n => plus n 1.
+  Definition Book_5_2'_ii : nat -> nat := fun n => plus 1 n.
+  Fail Definition Book_5_2'_not_defn_eq : Book_5_2'_i = Book_5_2'_ii := idpath.
+  Definition Book_5_2'_i_eq_ez : Book_5_2'_i 0 = ez := idpath.
+  Definition Book_5_2'_ii_eq_ez : Book_5_2'_ii 0 = ez := idpath.
+  Definition Book_5_2'_i_eq_es n : Book_5_2'_i (S n) = es n (Book_5_2'_i n) := idpath.
+  Definition Book_5_2'_ii_eq_es n : Book_5_2'_ii (S n) = es n (Book_5_2'_ii n) := idpath.
+End Book_5_2'.
 
 (* ================================================== ex:one-function-two-recurrences *)
 (** Exercise 5.3 *)
