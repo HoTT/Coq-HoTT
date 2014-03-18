ebc3dc8f0b4b15751a54fe6f3f71d7cb6645a2fe
Jason Gross <jgross@mit.edu>
2014-03-18 02:28:47 -0400

    Simplify types/UniverseLevel.v
    We don't actually need the fancy equality type, nor the paths.
    Strictly speaking, we don't need [lift] or [lower] or the equivalence,
    either.

--- theories/types/UniverseLevel.v
+++ theories/types/UniverseLevel.v
@@ -6,30 +6,20 @@ Set Implicit Arguments.
 
 (** We provide casting definitions for raising universe levels. *)
 
-(** [type_eq] is a variant of [=] which does not unify universes. *)
-Inductive type_eq (A : Type) : Type -> Type :=
-| type_eq_refl : type_eq A A
-| type_eq_impossible : False -> forall B : Type, type_eq A B.
-
 Section local.
   Let type_cast_up_type : Type.
   Proof.
     let U0 := constr:(Type) in
     let U1 := constr:(Type) in
     let unif := constr:(U0 : U1) in
-    exact (forall T : U0, { T' : U1 & type_eq T T' }).
+    exact (U0 -> U1).
   Defined.
 
-  Definition type_cast_up : type_cast_up_type
-    := fun T => existT (fun T' => type_eq T T') T (type_eq_refl _).
+  (** Because we have cumulativity (that [T : Uᵢ] gives us [T : Uᵢ₊₁]), we may define [Lift : U₀ → U₁] as the identity function with a fancy type; the type says that [U₀ ⊊ U₁]. *)
+  Definition Lift : type_cast_up_type
+    := fun T => T.
 End local.
 
-Definition Lift (T : Type) := projT1 (type_cast_up T).
-Definition paths_Lift (T : Type) : (T : Type) = Lift T
-  := match projT2 (type_cast_up T) in (type_eq _ T') return (T : Type) = T' with
-       | type_eq_refl => idpath
-       | type_eq_impossible bad _ => match bad with end
-     end.
 Definition lift {T} : T -> Lift T := fun x => x.
 Global Instance isequiv_lift T : IsEquiv (@lift T)
   := @BuildIsEquiv
