3add3705d6493a0a65112b0f686137ce416b1e6a
Jason Gross <jgross@mit.edu>
2014-03-31 19:53:32 -0400

    Use a debian repository for HoTT/coq
    Edward Yang (@ezyang) helpfully packaged HoTT/coq a while back.  The
    category theory is getting slow enough that the build that generates the
    html periodically fails, and I have to kick travis.  This should buy us
    another 5-10 minutes.
    
    Also install libcoq-ocaml-dev

--- etc/ci/install_coq_deps.sh
+++ etc/ci/install_coq_deps.sh
@@ -3,5 +3,9 @@
 PS4='$ '
 set -x
 
-sudo apt-get update -qq
-sudo apt-get install -q sed grep wget tar xsltproc mercurial python-lxml python-pexpect libxml2-dev libxslt1-dev ocaml camlp5
+#sudo apt-get update -qq
+sudo apt-get install -q sed grep wget tar xsltproc mercurial python-lxml python-pexpect libxml2-dev libxslt1-dev
+if [ ! -z "$BUILD_COQ" -o ! -z "$UPDATE_DEP_GRAPHS" ]
+then
+    sudo apt-get install -q ocaml camlp5
+fi
--- etc/ci/install_timing_deps.sh
+++ etc/ci/install_timing_deps.sh
@@ -3,5 +3,5 @@
 PS4='$ '
 set -x
 
-sudo apt-get update -qq
+#sudo apt-get update -qq
 sudo apt-get install -q lua5.1
