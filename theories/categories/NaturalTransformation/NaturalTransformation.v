47cd7afb45b391e824d5380cfa50360d41fbb6d3
Jason Gross <jgross@mit.edu>
2014-04-23 22:45:28 -0400

    Fix documentation of NaturalTransformation.v

--- theories/categories/NaturalTransformation/NaturalTransformation.v
+++ theories/categories/NaturalTransformation/NaturalTransformation.v
@@ -1,23 +1,35 @@
+(** * Natural Transformations *)
 (** Since there are only notations in [NaturalTransformation.Notations], we can just export those. *)
 Require Export NaturalTransformation.Notations.
 
-Require NaturalTransformation.Composition.Core.
+(** ** Definition of natural transformation *)
 Require NaturalTransformation.Core.
+(** ** Composition of natural transformations *)
+Require NaturalTransformation.Composition.Core.
+(** ** Dual natural transformations *)
 Require NaturalTransformation.Dual.
+(** ** Identity natural transformation *)
 Require NaturalTransformation.Identity.
+(** ** Natural isomorphisms *)
 Require NaturalTransformation.Isomorphisms.
+(** ** Path space of natural transformation type *)
 Require NaturalTransformation.Paths.
+(** ** Pointwise natural transformations *)
 Require NaturalTransformation.Pointwise.
+(** ** Sums of natural transformations *)
 Require NaturalTransformation.Sum.
+(** ** Products of natural transformations *)
+Require NaturalTransformation.Prod.
 
-Include NaturalTransformation.Composition.Core.
 Include NaturalTransformation.Core.
+Include NaturalTransformation.Composition.Core.
 Include NaturalTransformation.Dual.
 Include NaturalTransformation.Identity.
 Include NaturalTransformation.Isomorphisms.
 Include NaturalTransformation.Paths.
 Include NaturalTransformation.Pointwise.
 Include NaturalTransformation.Sum.
+Include NaturalTransformation.Prod.
 (** We don't want to make utf-8 notations the default, so we don't export them. *)
 
 (** Since [Composition] is a separate sub-directory, we need to re-create the module structure *)
