0ff4aadb63e0a51e2199dd150a433801ddfeb332
Jason Gross <jgross@mit.edu>
2014-05-11 14:01:31 +0200

    Add instances for pointedness of suspension

--- theories/Tests.v
+++ theories/Tests.v
@@ -11,3 +11,8 @@ Fail Check (fun (P : interval -> Type) (a : P Interval.zero) (b : P Interval.one
 Local Open Scope nat_scope.
 Fail Check Lift nat : Type0.
 Check 1 : Lift nat.
+
+(** Check that [ispointed_susp] doesn't require just a [Set] *)
+Check (fun A : Type => _ : IsPointed (Susp A)).
+Check (@ispointed_susp Type).
+Check (@ispointed_susp Set).
