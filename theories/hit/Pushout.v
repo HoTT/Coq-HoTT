c064812a578da0011a4d2acd7fe9ef34dd84a80b
Matthieu Sozeau <mattam@mattam.org>
2014-06-27 18:10:39 +0200

    Part of the proof of isepi -> issurj formalized using the pushout/cone version.

--- theories/hit/Pushout.v
+++ theories/hit/Pushout.v
@@ -7,6 +7,16 @@ Require Import types.Paths types.Forall types.Sigma types.Arrow types.Universe.
 Local Open Scope path_scope.
 Local Open Scope equiv_scope.
 
+(* Record Span :=  *)
+(*   { A : Type; B : Type; C : Type; *)
+(*     f : C -> A; *)
+(*     g : C -> B }. *)
+
+(* Record Cocone (S : Span) (D : Type) :=  *)
+(*   { i : A S -> D; *)
+(*     j : B S -> D; *)
+(*     h : forall c, i (f S c) = j (g S c) }. *)
+
 
 (** First we define the general non-recursive HIT. *)
 
@@ -57,7 +67,7 @@ Proof.
   refine (pushout_rect_beta_pp (fun _ => P) _ _ _).
 Defined.
 
-Require Import types.Unit HSet. 
+Require Import types.Unit HSet types.Sum.
 
 Definition isepi {X Y} (f:X->Y) := forall Z: hSet,
                                     forall g h: Y -> Z, g o f = h o f -> g = h.
@@ -67,29 +77,20 @@ Definition hEpi {X Y} (f : X -> Y) := forall Z : hSet,
                                           Contr  { h : Y -> Z & g o f = h o f }.
 
 Definition const {A B} (b : B) := fun x : A => b.
-Require Import Trunc Contractible.
-Definition trunc_pushout n A B C f g `{IsTrunc n A, IsTrunc n B, IsTrunc n C} : 
-  IsTrunc n (@pushout A B C f g).
+Require Import Trunc Contractible Tactics.
+
+Lemma transport_precomp {A B C} (f : A -> B) (g g' : B -> C) (p : g = g') :
+      transport (fun h : B -> C => g o f = h o f) p 1 =
+      ap (fun h => h o f) p.
 Proof.
-  induction n; simpl; intros.
-  exists (pushl (f:=f) (g:=g) (center _)).
-  intros y.
-  refine (pushout_rect _ _ (fun y => pushl (center A) = y) _ _ y).
-  intros a. destruct a. unfold pushl. apply ap. apply ap. apply path_contr.
-  assert (c = g (center A)). apply path_contr. 
-  eapply concat. apply pp. unfold pushr. apply ap. apply ap.
-  symmetry. apply X.
-  simpl. unfold transitivity. unfold transitive_paths.
-  intros. admit.
+  destruct p.
+  simpl. reflexivity.
+Defined.    
 
-  simpl in *.
-  revert x y. intros x. refine (pushout_rect _ _ (fun x => forall y, IsTrunc n (x = y))  _ _ x). 
-  intros []. simpl. intros b.
-  refine (pushout_rect _ _ (fun x => IsTrunc n (push (inl b) = x))  _ _).  intros []. intros. 
-  
-  
-
-  typeclasses eauto.
+Lemma apD10_ap_precomp {A B C} (f : A -> B) (g g' : B -> C) (p : g = g') a :
+   apD10 (ap (fun h : B -> C => h o f) p) a = apD10 p (f a).
+Proof.
+  destruct p. simpl. reflexivity.
 Defined.
 
 Section Cone.
@@ -98,26 +99,44 @@ Section Cone.
 
   Definition Cf := pushout f one.
 
-  Lemma Cf_hSet : IsHSet Cf.
+  Instance Cf_hSet : IsHSet Cf.
   Proof.
-    intro y.
-    refine (pushout_rect _ _ (fun y => forall y', IsHProp (y = y')) _ _ y).
-    intro.
-    refine (pushout_rect _ _ (fun y => IsHProp (push a = y)) _ _).
-    intros.
-    
+    admit.
+  Defined.    
 
-  Definition t : Cf := push_right tt.
+  Definition t : Cf := push (inr tt).
+
 
   Lemma isepi_isContr `{Funext} : hEpi f -> Contr Cf.
   Proof.
     intros hepi.
     red. simpl. exists t.
-    intros. refine (pushout_rect _ _ _ _ _ _ y); clear y. 
-    intros. pose (α1 := @pp A B Unit f one).
-    pose (tot:= { h : B -> Cf & push_left o f = h o f }).
-    pose (l := (push_left; idpath) : tot).
-    pose (r := (@const B Cf t; path_forall _ _ α1) : tot). 
-    pose (hepi Cf push_left).
 
-Definition mapping
+    pose (α1 := @pp A B Unit f one).
+    pose (tot:= { h : B -> Cf & push o inl o f = h o f }).
+    pose (l := (push o inl; idpath) : tot).
+    pose (r := (@const B Cf t; path_forall _ _ α1) : tot). 
+    subst tot.
+
+    + assert (l = r).
+      pose (hepi {| setT := Cf |} (push o inl)). 
+      apply path_contr.
+    subst l r.
+
+    pose (I0 b := apD10 (X ..1) b).
+    refine (pushout_rect _ _ _ _ _).
+    intros [b|[]]; [|reflexivity]. 
+    apply inverse. apply I0.
+    
+    simpl. subst α1. intros.
+    change (paths t) with (fun x => t = x).
+    rewrite transport_paths_r. subst I0. simpl.
+    pose (X..2). simpl in p. rewrite transport_precomp in p.
+    assert (H':=concat (ap (fun x => apD10 x a) p) (apD10_path_forall _ _ _ a)).
+    clear p.
+    pose (concat (apD10_ap_precomp f _ _ (X ..1) a)^ H').
+    simpl in p.
+    rewrite p. apply concat_Vp.
+  Defined.
+
+End Cone.
