1de30b10a279cca5c112f57d559db95c332d12a9
Jason Gross <jgross@mit.edu>
2014-07-16 08:42:13 -0400

    Add a missing import in convenience files

--- theories/categories/Functor/Composition/Functorial.v
+++ theories/categories/Functor/Composition/Functorial.v
@@ -1,3 +1,5 @@
 Require Functor.Composition.Functorial.Core.
+Require Functor.Composition.Functorial.Attributes.
 
 Include Functor.Composition.Functorial.Core.
+Include Functor.Composition.Functorial.Attributes.
