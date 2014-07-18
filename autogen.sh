104ddb89812bc32f407c449c7d08a971c7f68fa3
Jason Gross <jgross@mit.edu>
2014-07-18 06:45:45 -0400

    Update submodules in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -30,3 +30,18 @@ else
 	echo 'Error: autoreconf failed, and you are not using git.  Try installing autoconf or autoreconf.'
     fi
 fi
+
+if command -v git >/dev/null 2>&1
+then # git found
+    if test -d .git
+    then # we're in a git repository
+	git submodule sync # update possibly changed urls
+	git submodule update --init --recursive
+    elif test ! -d etc/coq-scripts/timing
+    then
+	echo 'You are not in a git repo; the timing scripts at ./etc/coq-scripts/timing will not be available.'
+    fi
+elif test ! -d etc/coq-scripts/timing
+then
+    echo 'You do not have git; the timing scripts at ./etc/coq-scripts/timing will not be available.'
+fi
