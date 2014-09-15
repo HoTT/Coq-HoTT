4a8a5beb38450520e640818680cf9157db175d23
Jason Gross <jgross@mit.edu>
2014-09-15 16:50:45 -0400

    Fix some old names in contrib/HoTTBookExercises.v

--- contrib/HoTTBookExercises.v
+++ contrib/HoTTBookExercises.v
@@ -288,12 +288,11 @@ Definition Book_2_13 := @HoTT.Misc.equiv_bool_equiv_bool_bool.
 Definition Book_3_1_solution_1 {A B} (f : A <~> B) (H : IsHSet A)
   := @HoTT.Basics.Trunc.trunc_equiv' A B f (trunc_S minus_one) H.
 
-(** Alternative solutions: Easy_3_1 using UA, and Replay_3_1 using
-two easy lemmas that may be of independent interest *)
+(** Alternative solutions: [Book_3_1_solution_2] using UA, and [Book_3_1_solution_3] using two easy lemmas that may be of independent interest *)
 
 Lemma Book_3_1_solution_2 `{Univalence} {A B} : A <~> B -> IsHSet A -> IsHSet B.
 Proof.
-  intro e. 
+  intro e.
   rewrite (path_universe_uncurried e).
   exact idmap.
 Defined.
@@ -335,16 +334,16 @@ Defined.
 Definition Book_3_2_solution_1 := @HoTT.types.Sum.hset_sum.
 
 (** Alternative solution for replaying *)
- 
+
 Lemma Book_3_2_solution_2 (A B : Type) : IsHSet A -> IsHSet B -> IsHSet (A+B).
 Proof.
   intros isHSet_A isHSet_B.
   apply @hset_axiomK. unfold axiomK. intros x p. destruct x.
-  rewrite (inverse (eisretr_path_sum p)). 
-  rewrite (axiomK_hset isHSet_A a (path_sum_inv p)). 
+  rewrite (inverse (eisretr_path_sum p)).
+  rewrite (axiomK_hset isHSet_A a (path_sum_inv p)).
   simpl; exact idpath.
-  rewrite (inverse (eisretr_path_sum p)). 
-  rewrite (axiomK_hset isHSet_B b (path_sum_inv p)). 
+  rewrite (inverse (eisretr_path_sum p)).
+  rewrite (axiomK_hset isHSet_B b (path_sum_inv p)).
   simpl; exact idpath.
 Defined.
 
@@ -354,10 +353,9 @@ Defined.
 Definition Book_3_3_solution_1 (A : Type) (B : A -> Type)
    := @HoTT.types.Sigma.trunc_sigma A B 0.
 
-(** This exercise is hard because 2-paths over Sigma types are not treated
-in the first three chapters of the book. Consult theories/types/Sigma.v *)
+(** This exercise is hard because 2-paths over Sigma types are not treated in the first three chapters of the book. Consult theories/types/Sigma.v *)
 
-Lemma Book_3_3_solution_2 (A : Type) (B : A -> Type) : 
+Lemma Book_3_3_solution_2 (A : Type) (B : A -> Type) :
   IsHSet A -> (forall x:A, IsHSet (B x)) -> IsHSet { x:A | B x}.
 Proof.
   intros isHSet_A allBx_HSet.
@@ -1305,6 +1303,3 @@ Definition Book_7_13_part_ii := @HoTT.hit.PropositionalFracture.closed_modality.
 
 (* ================================================== ex:pseudo-ordinals *)
 (** Exercise 11.18 *)
-
-
-
