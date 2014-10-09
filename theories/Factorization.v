0b60426cbd4d5f4139f8c53af11ddb25eca497be
mikeshulman <viritrilbia@gmail.com>
2014-10-09 02:23:24 -0700

    factor an opaque lemma out of path_factorization for a 2x speedup

--- theories/Factorization.v
+++ theories/Factorization.v
@@ -64,97 +64,137 @@ Section Factorization.
   Defined.
 
   (** The converse, however, is more work. In the proof of Theorem 7.6.6, the book glosses over this theorem with the phrase "by univalence and the characterization of paths and transport in Sigma-types, function types, and path types".  Which is arguably fair informally, because it's "obvious", but it turns out to be a good deal of work to keep track of all the transport lemmas and apply naturality in the right places. *)
-  Definition path_factorization `{Univalence} (fact fact' : Factorization)
-  : PathFactorization fact fact' -> (fact = fact').
-  Proof.
-    intros pf.
-    pose (II := path_intermediate _ _ pf).
-    pose (ff1 := path_factor1 _ _ pf).
-    pose (ff2 := path_factor2 _ _ pf).
-    pose (fff := path_fact_factors _ _ pf).
-    (* The idea is of course to decompose [Factorization] as a sigma and use [path_sigma] over and over again.  It's fairly easy to insert [II], [ff1], and [ff2] in the right places. *)
-    apply ((ap issig_Factorization^-1)^-1); cbn.
-    refine (path_sigma' _ _ _); cbn.
-    { (* Here we use [II]. *)
-      refine (path_universe II). }
-    refine (transport_sigma_' _ _ @ _); cbn.
-    refine (path_sigma' _ _ _); cbn.
-    { (* Here we use [ff1]. *)
+  Section PathFactorization.
+    Context `{Univalence} (fact fact' : Factorization)
+            (pf : PathFactorization fact fact').
+
+    (** Let's give short names to the fields of [pf]. *)
+    Let II := path_intermediate _ _ pf.
+    Let ff1 := path_factor1 _ _ pf.
+    Let ff2 := path_factor2 _ _ pf.
+    Let fff := path_fact_factors _ _ pf.
+
+    (** Each of those fields now induces a corresponding (dependent) path.  The first one is easy. *)
+    Local Definition II' : intermediate fact = intermediate fact'
+      := path_universe II.
+
+    (** The second is slightly harder, but at least its type is obvious. *)
+    Local Definition ff1'
+      : transport (fun X => A -> X) II' (factor1 fact) = factor1 fact'.
+    Proof.
       apply path_arrow; intros a.
       refine (transport_arrow_fromconst (C := idmap) _ _ a @ _).
       etransitivity; [ apply transport_path_universe | ].
-      apply ff1. }
-    refine (transport_sigma' _ _ @ _); cbn.
-    refine (path_sigma' _ _ _); cbn.
-    { (* Here we use [ff2].  It's a bit more complicated because the way we stated [ff2] isn't exactly the way it appears naturally here. *)
+      apply ff1.
+    Defined.
+
+    (** The type of the third is just like the type of the second, but its proof is a bit more complicated because the type of [ff2] isn't exactly the way it appears naturally here. *)
+    Local Definition ff2'
+      : transport (fun X => X -> B) II' (factor2 fact) = factor2 fact'.
+    Proof.
       apply path_arrow; intros a.
       refine (transport_arrow_toconst (B := idmap) _ _ a @ _).
       etransitivity; [ apply ap, transport_path_universe_V | ].
       etransitivity; [ apply ff2 | ].
       unfold compose; apply ap.
-      apply eisretr. }
-    (* Now we have a gigantic mess, but fortunately it simplifies a great deal because the last two components are hprops. *)
-    refine (transport_sigma (A := intermediate fact' -> B)
-                            (B := fun g => g o factor1 fact' == f)
-                            _ _ @ _); cbn.
-    refine (path_sigma' _ _ _); cbn.
-    2:apply path_ishprop.
-    (* It's still pretty scary-looking, but we press ahead undaunted, looking for transport redexes and path redexes. *)
-    apply path_forall; intros a.
-    unfold pointwise_paths.
-    rewrite transport_forall_constant, transport_paths_Fl.
-    rewrite ap_apply_l, ap10_path_arrow.
-    rewrite transport_sigma_'; cbn.
-    rewrite transport_forall_constant, transport_paths_Fl.
-    simpl rewrite (@ap_compose _ _ _
-                    (fun g:A->intermediate fact' => g a)
-                    (fun x => transport (fun X => X -> B) (path_universe II)
-                                        (factor2 fact) x)).
-    rewrite ap_apply_l, ap10_path_arrow.
-    (* Now we start having to look for naturality properties, so [long_path_scope] is useful. *)
-    Open Scope long_path_scope.
-    do 2 apply moveR_Vp; repeat rewrite concat_p_pp.
-    (* We begin with [ap_transport_arrow_toconst] *)
-    rewrite (ap_transport_arrow_toconst (B := idmap) (C := B)).
-    (* Now we set up for a naturality *)
-    simpl rewrite (@ap_compose _ _ _ (transport idmap (path_universe II)^)
-                               (factor2 fact)).
-    rewrite <- ap_p_pp; rewrite_moveL_Mp_p.
-    Fail rewrite (concat_Ap ff2). (* Maybe one day rewrite will be smarter *)
-    refine (_ @ (((concat_Ap ff2 _)^ @@ 1) @@ 1)).
-    (* Next is another naturality  *)
-    rewrite ap_compose.
-    Fail rewrite <- ap_p_pp.    (* Yeah, rewrite. *)
-    refine (_ @ (ap_p_pp (factor2 fact') _ _ _ @@ 1)).
-    repeat rewrite (ap_pp (transport idmap (path_universe II)^)).
-    rewrite concat_pA_p.
-    (* And another one *)
-    repeat rewrite (ap_pp II).
-    rewrite <- (ap_compose (II^-1) II); unfold compose.
-    rewrite (concat_pA1_p (eisretr II) (ff1 a)).
-    (* Now we use the triangle identity of our equivalence [II]. *)
-    rewrite (ap_pp (factor2 fact')).
-    unfold compose; rewrite eisadj.
-    (* And one more naturality *)
-    repeat rewrite concat_p_pp.
-    repeat rewrite <- ap_pp.
-    rewrite <- ap_compose.
-    (* Here we [refine] rather than [rewrite] to specify exactly *where* we want to apply naturality. *)
-    refine (_ @ (((concat_Ap ff2 _) @@ 1) @@ 1)).
-    rewrite_moveL_Mp_p.
-    (* Finally we can use [fff]! *)
-    refine (_ @ (fff a)^).
-    do 2 rewrite_moveR_Vp_p.
-    (* Now we still have a mess, but with a little futzing we can put it into a form that we can do path induction over.  The point is to make it all a function of [path_universe II].  In particular, we need to eliminate [eissect II], which we do with [transport_path_universe_Vp]. *)
-    rewrite (ap_pp (transport idmap (path_universe II)^)).
-    with_rassoc ltac:(rewrite (ap_pp (factor2 fact))).
-    rewrite transport_path_universe_Vp.
-    (* This mess is completely general in [path_universe II]! *)
-    fold II. set (p := path_universe II); clearbody p.
-    case p; cbn. auto with path_hints.
-    Close Scope long_path_scope.
-  Qed.
+      apply eisretr.
+    Defined.
 
+    (** Finally, even the type of this one is rather intimidating.  It's hard to justify this type until you get to the end of the proof of [path_factorization], below, and see that it's exactly what we need to complete it.  We prove it separately first so that we can make it opaque for performance reasons (roughly a 2x speedup).  Note that we can't make [II'], [ff1'], or [ff2'] opaque, since they have to be unfolded in this proof and the next. *)
+    Local Definition fff' (a : A)
+    : (transportD2 (fun X => A -> X) (fun X => X -> B)
+                   (fun X g h => {_ : forall a : A, h (g a) = f a &
+                                  {_ : class1 A X g & class2 X B h}})
+                   II' (factor1 fact) (factor2 fact)
+                   (fact_factors fact; (inclass1 fact; inclass2 fact))).1 a =
+      ap (transport (fun X => X -> B) II' (factor2 fact))
+        (transport_arrow_fromconst II' (factor1 fact) a
+          @ transport_path_universe II (factor1 fact a)
+          @ ff1 a)
+        @ transport_arrow_toconst II' (factor2 fact) (factor1 fact' a)
+        @ ap (factor2 fact) (transport_path_universe_V II (factor1 fact' a))
+        @ ff2 (II^-1 (factor1 fact' a))
+        @ ap (factor2 fact') (eisretr II (factor1 fact' a))
+        @ fact_factors fact' a.
+    Proof.
+      (* The name of the game is looking for naturality properties so that we can apply [fff], so [long_path_scope] is helpful to be able to see the shape of the goal better. *)
+      Open Scope long_path_scope.
+      (* Here is a fairly obvious beginning. *)
+      rewrite (ap_transport_arrow_toconst (B := idmap) (C := B)).
+      (* Now we set up for a naturality *)
+      simpl rewrite (@ap_compose _ _ _ (transport idmap (path_universe II)^)
+                                 (factor2 fact)).
+      rewrite <- ap_p_pp; rewrite_moveL_Mp_p.
+      Fail rewrite (concat_Ap ff2). (* Maybe one day rewrite will be smarter *)
+      refine (_ @ (((concat_Ap ff2 _)^ @@ 1) @@ 1)).
+      (* Next is another naturality  *)
+      rewrite ap_compose.
+      Fail rewrite <- ap_p_pp.    (* Yeah, rewrite. *)
+      refine (_ @ (ap_p_pp (factor2 fact') _ _ _ @@ 1)).
+      repeat rewrite (ap_pp (transport idmap (path_universe II)^)).
+      rewrite concat_pA_p.
+      (* And another one *)
+      repeat rewrite (ap_pp II).
+      rewrite <- (ap_compose (II^-1) II); unfold compose.
+      rewrite (concat_pA1_p (eisretr II) (ff1 a)).
+      (* Now we use the triangle identity of our equivalence [II]. *)
+      rewrite (ap_pp (factor2 fact')).
+      unfold compose; rewrite eisadj.
+      (* And one more naturality *)
+      repeat rewrite concat_p_pp.
+      repeat rewrite <- ap_pp.
+      rewrite <- ap_compose.
+      (* Here we [refine] rather than [rewrite] to specify exactly *where* we want to apply naturality. *)
+      refine (_ @ (((concat_Ap ff2 _) @@ 1) @@ 1)).
+      rewrite_moveL_Mp_p.
+      (* Finally we can use [fff]! *)
+      refine (_ @ (fff a)^).
+      do 2 rewrite_moveR_Vp_p.
+      (* Now we still have a mess, but with a little futzing we can put it into a form that we can do path induction over.  The point is to make it all a function of [path_universe II].  In particular, we need to eliminate [eissect II], which we do with [transport_path_universe_Vp]. *)
+      rewrite (ap_pp (transport idmap (path_universe II)^)).
+      with_rassoc ltac:(rewrite (ap_pp (factor2 fact))).
+      unfold II'; rewrite transport_path_universe_Vp.
+      (* This mess is completely general in [path_universe II]! *)
+      fold II. set (p := path_universe II); clearbody p.
+      case p; cbn. auto with path_hints.
+      Close Scope long_path_scope.
+    Qed.
+
+    (** Finally, we're ready to prove the main theorem. *)
+    Definition path_factorization : fact = fact'.
+    Proof.
+      (* The idea is of course to decompose [Factorization] as a sigma and use [path_sigma] over and over again.  It's fairly easy to insert [II], [ff1], and [ff2] in the right places. *)
+      apply ((ap issig_Factorization^-1)^-1); cbn.
+      refine (path_sigma' _ II' _); cbn.
+      refine (transport_sigma_' _ _ @ _); cbn.
+      refine (path_sigma' _ ff1' _); cbn.
+      refine (transport_sigma' _ _ @ _); cbn.
+      refine (path_sigma' _ ff2' _); cbn.
+      (* Now we have a gigantic mess, but fortunately it simplifies a great deal because the last two components are hprops. *)
+      refine (transport_sigma (A := intermediate fact' -> B)
+                              (B := fun g => g o factor1 fact' == f)
+                              _ _ @ _); cbn.
+      refine (path_sigma' _ _ _); cbn.
+      2:apply path_ishprop.
+      (* It's still pretty scary-looking, but we press ahead undaunted, looking for transport redexes and path redexes. *)
+      apply path_forall; intros a.
+      unfold pointwise_paths.
+      rewrite transport_forall_constant, transport_paths_Fl.
+      unfold II', ff1', ff2'.
+      rewrite ap_apply_l, ap10_path_arrow.
+      rewrite transport_sigma_'; cbn.
+      rewrite transport_forall_constant, transport_paths_Fl.
+      simpl rewrite (@ap_compose _ _ _
+                                 (fun g:A->intermediate fact' => g a)
+                                 (fun x => transport (fun X => X -> B) (path_universe II)
+                                                     (factor2 fact) x)).
+      rewrite ap_apply_l, ap10_path_arrow.
+      (* Finally we can apply our monster lemma [fff']. *)
+      do 2 apply moveR_Vp; repeat rewrite concat_p_pp.
+      exact (fff' a).
+    Qed.
+
+  End PathFactorization.
 End Factorization.
 
 Arguments PathFactorization {class1 class2 A B f} fact fact'.
