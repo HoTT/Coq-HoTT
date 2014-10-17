01e029efed638026c17079a8f179099bc1234ef9
Mike Shulman <viritrilbia@gmail.com>
2014-10-16 17:29:28 -0700

    global unset elimination schemes

--- coq/theories/Init/Datatypes.v
+++ coq/theories/Init/Datatypes.v
@@ -18,9 +18,7 @@ Declare ML Module "nat_syntax_plugin".
 Global Set Universe Polymorphism.
 Global Set Asymmetric Patterns.
 Local Set Primitive Projections.
-Global Set Nonrecursive Elimination Schemes.
-
-Unset Elimination Schemes.
+Global Unset Elimination Schemes.
 
 (** [option A] is the extension of [A] with an extra element [None] *)
 
