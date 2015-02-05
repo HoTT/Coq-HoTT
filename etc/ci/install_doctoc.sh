8467be8d547e7ecbd7d87b958a2d35ccfbadfe1c
Jason Gross <jgross@mit.edu>
2015-02-04 20:09:57 -0500

    Atuogenerate TOC with doctoc

---
+++ etc/ci/install_doctoc.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PS4='$ '
+set -x
+
+# in case we're run from out of git repo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" 1>/dev/null
+
+# now change to the git root
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+cd "$ROOT_DIR" 1>/dev/null
+
+npm install -g doctoc
+
+popd 1>/dev/null
