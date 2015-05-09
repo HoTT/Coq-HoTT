283dd7c2e10ba05c2ec8a27e129bbf501734cea7
Jason Gross <jgross@mit.edu>
2015-05-09 14:34:46 -0400

    Add some definitions for lifting equivalences

--- theories/Basics/UniverseLevel.v
+++ theories/Basics/UniverseLevel.v
@@ -1,4 +1,4 @@
-Require Import Overture.
+Require Import Basics.Overture Basics.PathGroupoids.
 
 (** * Universe Levels *)
 
@@ -12,6 +12,15 @@ Definition lift {A} : A -> Lift A := fun x => x.
 
 Definition lower {A} : Lift A -> A := fun x => x.
 
+Definition lift2 {A B} (f : forall x : A, B x) : forall x : Lift A, Lift (B (lower x))
+  := f.
+
+Definition lower2 {A B} (f : forall x : Lift A, Lift (B (lower x))) : forall x : A, B x
+  := f.
+
+(** We make [lift] and [lower] opaque so that typeclass resolution doesn't pick up [isequiv_lift] as an instance of [IsEquiv idmap] and wreck havok. *)
+Typeclasses Opaque lift lower lift2 lower2.
+
 Global Instance isequiv_lift T : IsEquiv (@lift T)
   := @BuildIsEquiv
        _ _
@@ -20,6 +29,40 @@ Global Instance isequiv_lift T : IsEquiv (@lift T)
        (fun _ => idpath)
        (fun _ => idpath)
        (fun _ => idpath).
+
+Global Instance isequiv_lift2 A B : IsEquiv (@lift2 A B)
+  := @BuildIsEquiv
+       _ _
+       (@lift2 A B)
+       (@lower2 A B)
+       (fun _ => idpath)
+       (fun _ => idpath)
+       (fun _ => idpath).
+
+Global Instance lift_isequiv {A B} (f : A -> B) {H : IsEquiv f} : @IsEquiv (Lift A) (Lift B) (lift2 f)
+  := @BuildIsEquiv
+       (Lift A) (Lift B)
+       (lift2 f)
+       (lift2 (f^-1))
+       (fun x => ap lift (eisretr f (lower x)))
+       (fun x => ap lift (eissect f (lower x)))
+       (fun x => ((ap (ap lift) (eisadj f (lower x)))
+                    @ (ap_compose f lift _)^)
+                   @ (@ap_compose A (Lift A) (Lift B) lift (lift2 f) _ _ _)).
+
+Global Instance lower_isequiv {A B} (f : Lift A -> Lift B) {H : IsEquiv f} : @IsEquiv A B (lower2 f)
+  := @BuildIsEquiv
+       _ _
+       (lower2 f)
+       (lower2 (f^-1))
+       (fun x => ap lower (eisretr f (lift x)))
+       (fun x => ap lower (eissect f (lift x)))
+       (fun x => ((ap (ap lower) (eisadj f (lift x)))
+                    @ (ap_compose f lower _)^)
+                   @ (@ap_compose (Lift A) A B lower (lower2 f) _ _ _)).
+
+Definition lower_equiv {A B} (e : Equiv (Lift A) (Lift B)) : Equiv A B
+  := @BuildEquiv A B (lower2 e) _.
 
 (** This version doesn't force strict containment, i.e. it allows the two universes to possibly be the same.  No fancy type is necessary here other than the universe annotations, because of cumulativity. *)
 
@@ -30,6 +73,18 @@ Definition lift' {A : Type@{i}} : A -> Lift'@{i j} A := fun x => x.
 
 Definition lower' {A : Type@{i}} : Lift'@{i j} A -> A := fun x => x.
 
+Definition lift'2 {A : Type@{i}} {B : A -> Type@{i'}} (f : forall x : A, B x)
+: forall x : Lift'@{i j} A, Lift'@{i' j'} (B (lower' x))
+  := f.
+
+Definition lower'2 {A : Type@{i}} {B : A -> Type@{i'}}
+           (f : forall x : Lift'@{i j} A, Lift'@{i' j'} (B (lower' x)))
+: forall x : A, B x
+  := f.
+
+(** We make [lift] and [lower] opaque so that typeclass resolution doesn't pick up [isequiv_lift] as an instance of [IsEquiv idmap] and wreck havok. *)
+Typeclasses Opaque lift' lower' lift'2 lower'2.
+
 Global Instance isequiv_lift' T : IsEquiv (@lift'@{i j} T)
   := @BuildIsEquiv
        _ _
@@ -39,8 +94,39 @@ Global Instance isequiv_lift' T : IsEquiv (@lift'@{i j} T)
        (fun _ => idpath)
        (fun _ => idpath).
 
-(** We make [lift] and [lower] opaque so that typeclass resolution doesn't pick up [isequiv_lift] as an instance of [IsEquiv idmap] and wreck havok. *)
-Typeclasses Opaque lift lower lift' lower'.
+Global Instance isequiv_lift'2 A B : IsEquiv (@lift'2@{i i' j j'} A B)
+  := @BuildIsEquiv
+       _ _
+       (@lift'2 A B)
+       (@lower'2 A B)
+       (fun _ => idpath)
+       (fun _ => idpath)
+       (fun _ => idpath).
+
+Global Instance lift'_isequiv {A B} (f : A -> B) {H : IsEquiv f} : @IsEquiv (Lift'@{i j} A) (Lift'@{i' j'} B) (lift'2 f)
+  := @BuildIsEquiv
+       (Lift' A) (Lift' B)
+       (lift'2 f)
+       (lift'2 (f^-1))
+       (fun x => ap lift' (eisretr f (lower' x)))
+       (fun x => ap lift' (eissect f (lower' x)))
+       (fun x => ((ap (ap lift') (eisadj f (lower' x)))
+                    @ (ap_compose f lift' _)^)
+                   @ (@ap_compose A (Lift' A) (Lift' B) lift' (lift'2 f) _ _ _)).
+
+Global Instance lower'_isequiv {A B} (f : Lift'@{i j} A -> Lift'@{i' j'} B) {H : IsEquiv f} : @IsEquiv A B (lower'2 f)
+  := @BuildIsEquiv
+       _ _
+       (lower'2 f)
+       (lower'2 (f^-1))
+       (fun x => ap lower' (eisretr f (lift' x)))
+       (fun x => ap lower' (eissect f (lift' x)))
+       (fun x => ((ap (ap lower') (eisadj f (lift' x)))
+                    @ (ap_compose f lower' _)^)
+                   @ (@ap_compose (Lift' A) A B lower' (lower'2 f) _ _ _)).
+
+Definition lower'_equiv {A B} (e : Equiv (Lift'@{i j} A) (Lift'@{i' j'} B)) : Equiv A B
+  := @BuildEquiv A B (lower'2 e) _.
 
 (*Fail Check Lift nat : Type0.
 Check 1 : Lift nat.*)
