c423054a596d596bb4256a987b10e1206fc6f71c
John Dougherty <john.e.dougherty.ii@gmail.com>
2015-07-15 10:34:19 -0700

    define TwoSphere recursion
    Define TwoSphere recursion, rename beta rules to *_surf instead of
    *_loop, and add 2D path algebra lemmas to PathGroupoids

--- theories/hit/TwoSphere.v
+++ theories/hit/TwoSphere.v
@@ -17,30 +17,55 @@ Private Inductive S2 : Type0 :=
 
 Axiom surf : idpath base = idpath base.
 
-Definition S2_ind (P : S2 -> Type) (b : P base) (l : transport2 P surf b = idpath b)
+Definition S2_ind (P : S2 -> Type) (b : P base) (s : idpath b = transport2 P surf b)
   : forall (x:S2), P x
-  := fun x => match x with base => fun _ => b end l.
+  := fun x => match x with base => fun _ => b end s.
 
-Axiom S2_ind_beta_loop
-  : forall (P : S2 -> Type) (b : P base) (l : transport2 P surf b = idpath b),
-      apD02 (S2_ind P b l) surf = l^ @ (concat_p1 _)^.
+Axiom S2_ind_beta_surf
+  : forall (P : S2 -> Type) (b : P base) (s : idpath b = transport2 P surf b),
+      apD02 (S2_ind P b s) surf = s @ (concat_p1 _)^.
 
 End TwoSphere.
 
 (* ** The non-dependent eliminator *)
 
-Definition S2_rec (P : Type) (b : P) (l : idpath b = idpath b)
+Definition S2_rec (P : Type) (b : P) (s : idpath b = idpath b)
   : S2 -> P
-  := S2_ind (fun _ => P) b ((concat_p1 _)^ @ (transport2_const surf b)^ @ l).
+  := S2_ind (fun _ => P) b (s @ (transport2_const surf b) @ (concat_p1 _)).
 
-(** TODO: Write the non-dependent eliminator.  It's probably something like
-<<
-Definition S1_rec_beta_loop (P : Type) (b : P) (l : idpath b = idpath b)
-: ap02 (S2_rec P b l) surf = l^ @ (concat_p1 _)^.
+
+Definition S2_rec_beta_surf (P : Type) (b : P) (s : idpath b = idpath b)
+: ap02 (S2_rec P b s) surf = s.
 Proof.
-  unfold S2_rec.
-  refine (cancelL (transport2_const surf b)^ _ _ _).
-  refine (cancelL ((concat_p1 (transport2 (fun _ : S2 => P) surf b))^) _ _ _).
-...
->>
-*)
+  apply (cancel2L (transport2_const surf b)).
+  apply (cancelL (apD_const (S2_rec P b s) (idpath base))).
+  apply (cancelR _ _ (concat_p_pp _ (transport_const _ b) _)^).
+  apply (cancelR _ _ (whiskerL (transport2 _ surf b) (apD_const _ _)^)).
+  refine ((apD02_const (S2_rec P b s) surf)^ @ _).
+  refine ((S2_ind_beta_surf _ _ _) @ _).
+  refine (_ @ (ap (fun w => _ @ w) 
+                  (triangulator 
+                     (transport2 (fun _ : S2 => P) surf b) _))). 
+  cbn. refine (_ @ (ap (fun w => (w @ _) @ _) (concat_1p _)^)).
+  
+  refine (_ @ (concat_p_pp _ _ _)).
+  refine (_ @ (ap (fun w => _ @ w) (concat_pp_p _ _ _))).
+  refine (_ @ (ap (fun w => _ @ (w @ _)) (concat_Vp _)^)).
+  refine (_ @ (ap (fun w => _ @ (w @ _)) 
+                  (concat_pV (concat_p1 
+                                (transport2 (fun _ : S2 => P) surf b @ 1))))).
+  refine (_ @ (ap (fun w => _ @ w) (concat_p_pp _ _ _))).
+  refine (_ @ (concat_pp_p _ _ _)). apply moveR_pV.
+  refine (_ @ (concat_p_pp _ _ _)).
+  refine (_ @ (ap (fun w => _ @ w) (whiskerR_p1 _)^)). f_ap.
+  refine ((ap (fun w => w @ _) (whiskerL_1p_1 _)^) @ _).
+  refine ((ap (fun w => _ @ w) (whiskerR_p1 _)^) @ _). cbn.
+  refine ((concat_p_pp _ _ _) @ _). f_ap.
+  refine ((ap (fun w => _ @ w) (concat_1p _)) @ _).
+  refine ((concat_whisker 1 _ _ 1 (transport2_const surf b) s)^ @ _).
+  
+  symmetry.
+  refine ((ap (fun w => w @@ _) (concat_p1 _)^) @ _).
+  refine ((ap (fun w => _ @@ w) (concat_1p _)^) @ _).
+  refine ((concat_concat2 _ _ _ _)^ @ _). f_ap.
+Defined.
