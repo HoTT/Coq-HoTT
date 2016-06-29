80bd01dc1adfabfa32f967b9999c5f48362aea16
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2016-06-29 17:44:19 +0200

    start defining cauchy reals

---
+++ theories/implementations/cauchy.v
@@ -0,0 +1,156 @@
+Require Import
+  HoTT.Basics.Decidable
+  HoTTClasses.interfaces.abstract_algebra
+  HoTTClasses.interfaces.integers
+  HoTTClasses.interfaces.naturals
+  HoTTClasses.interfaces.rationals
+  HoTTClasses.interfaces.orders
+  HoTTClasses.implementations.field_of_fractions
+  HoTTClasses.implementations.natpair_integers
+  HoTTClasses.theory.rings
+  HoTTClasses.theory.integers
+  HoTTClasses.theory.dec_fields
+  HoTTClasses.theory.rationals.
+
+Module Export Cauchy.
+
+Section VarSec.
+
+Context `{Rationals Q} `{Qap : Apart Q} `{Qlt : Lt Q} `{Qle : Le Q}
+  `{!FullPseudoSemiRingOrder (_:Le Q) (_:Lt Q)}.
+
+Record Qpos := mkQpos { pos : Q; is_pos : 0 < pos }.
+Notation "Q+" := Qpos.
+
+Instance : Cast Qpos Q := pos.
+
+Lemma Qpos_plus_pr : forall a b : Qpos, 0 < 'a + 'b.
+Proof.
+intros.
+apply semirings.pos_plus_compat;apply is_pos.
+Qed.
+
+Instance Qpos_plus : Plus Qpos := fun a b => mkQpos _ (Qpos_plus_pr a b).
+
+Lemma Qpos_eq : forall a b : Qpos, ' a = ' b -> a = b.
+Proof.
+intros [a pa] [b pb] E.
+change (a=b) in E.
+destruct E. apply ap.
+apply path_ishprop.
+Qed.
+
+Private Inductive real : Type :=
+  | rat : Q -> real
+  | lim' : forall (f : Qpos -> real),
+    (forall d e : Qpos, equiv' (d+e) (f d) (f e)) -> real
+
+with equiv' : Qpos -> real -> real -> Type :=
+  | equiv_rat_rat' : forall (q r : Q) (e : Qpos),
+      - (' e : Q) < q + (- r) < ' e ->
+      equiv' e (rat q) (rat r)
+  | equiv_rat_lim' : forall q y Hy (e d d' : Qpos),
+      e = d + d' ->
+      equiv' d' (rat q) (y d) ->
+      equiv' e (rat q) (lim' y Hy)
+  | equiv_lim_rat' : forall x Hx r (e d d' : Qpos),
+      e = d + d' ->
+      equiv' d' (x d) (rat r) ->
+      equiv' e (lim' x Hx) (rat r)
+  | equiv_lim_lim' : forall x Hx y Hy (e d n e' : Qpos),
+      e = d + n + e' ->
+      equiv' e' (x d) (y n) ->
+      equiv' e (lim' x Hx) (lim' y Hy)
+.
+
+Class Requiv e u v := requiv : equiv' e u v.
+
+Axiom equiv_path : forall (u v : real) (u_eq_v : forall e, Requiv e u v), u = v.
+Axiom equiv_hprop : forall e u v, IsHProp (Requiv e u v).
+Global Existing Instance equiv_hprop.
+
+Record Approximation :=
+  { approximate :> Qpos -> real
+  ; approx_equiv : forall d e, Requiv (d+e) (approximate d) (approximate e) }.
+Existing Instance approx_equiv.
+
+Definition lim (x : Approximation) : real :=
+  lim' x (fun _ _ => requiv).
+
+Definition equiv_rat_rat : forall (q r : Q) (e : Qpos),
+  - (' e : Q) < q + (- r) < ' e ->
+  Requiv e (rat q) (rat r)
+  := equiv_rat_rat'.
+
+Definition equiv_rat_lim : forall q (y:Approximation) (e d d' : Qpos),
+  e = d + d' ->
+  Requiv d' (rat q) (y d) ->
+  Requiv e (rat q) (lim y).
+Proof.
+intros. eapply equiv_rat_lim';eauto.
+Defined.
+
+Definition equiv_lim_rat : forall (x:Approximation) r (e d d' : Qpos),
+  e = d + d' ->
+  Requiv d' (x d) (rat r) ->
+  Requiv e (lim x) (rat r).
+Proof. intros;eapply equiv_lim_rat';eauto. Defined.
+
+Definition equiv_lim_lim : forall (x y : Approximation) (e d n e' : Qpos),
+  e = d + n + e' ->
+  Requiv e' (x d) (y n) ->
+  Requiv e (lim x) (lim y).
+Proof.
+intros;eapply equiv_lim_lim';eauto.
+Defined.
+
+Record DApproximation (A : real -> Type)
+  (B : forall x y : real, A x -> A y -> forall e `{Requiv e x y}, Type)
+  (x : Approximation) :=
+  { dapproximation :> forall e, A (x e)
+  ; dapproximation_correct :
+    forall d e, B (x d) (x e) (dapproximation d) (dapproximation e) (d+e) }.
+
+Record Inductors (A : real -> Type)
+  (B : forall x y : real, A x -> A y -> forall e `{Requiv e x y}, Type) :=
+  { ind_rat : forall q, A (rat q)
+  ; ind_lim : forall (x:Approximation) (a : DApproximation A B x),
+    A (lim x)
+
+  ; ind_path_A : forall u v (u_equiv_v : forall e, Requiv e u v),
+    forall (a : A u) (b : A v), (forall e, B u v a b e) ->
+    equiv_path u v (fun _ => requiv) # a = b
+
+  ; ind_rat_rat : forall q r e (Hqr : - ' e < q + (- r) < ' e),
+      @B (rat q) (rat r) (ind_rat q) (ind_rat r) e (equiv_rat_rat _ _ _ Hqr)
+  ; ind_rat_lim : forall q d d' e y (b : DApproximation A B y)
+      (He : e = d + d')
+      (Hclose : Requiv d' (rat q) (y d)),
+      B (rat q) (y d) (ind_rat q) (b d) d' ->
+      @B (rat q) (lim y) (ind_rat q) (ind_lim y b) e
+         (equiv_rat_lim _ _ _ _ _ He _)
+   ; ind_lim_rat : forall r d d' e x (a : DApproximation A B x)
+      (He : e = d + d')
+      (Hclose : Requiv d' (x d) (rat r)),
+      B (x d) (rat r) (a d) (ind_rat r) d' ->
+      @B (lim x) (rat r) (ind_lim x a) (ind_rat r) e
+         (equiv_lim_rat _ _ _ _ _ He _)
+   ; ind_lim_lim : forall x y (a : DApproximation A B x) (b : DApproximation A B y)
+      e d n e' (He : e = d + n + e')
+      (Hclose : Requiv e' (x d) (y n)),
+      B (x d) (y n) (a d) (b n) e' ->
+      @B (lim x) (lim y) (ind_lim x a) (ind_lim y b) e
+         (equiv_lim_lim _ _ _ _ _ _ He _)
+
+   ; ind_hprop_B : forall x y a b e xi, IsHProp (@B x y a b e xi) }.
+
+Fixpoint real_rect A B (I : Inductors A B) (x : real) : A x
+with equiv_rect A B (I : Inductors A B) (x y : real) (e : Qpos) (xi : Requiv e x y)
+  : B x y (real_rect A B I x) (real_rect A B I y) e.
+
+End induction.
+
+End VarSec.
+
+End Cauchy.
+
