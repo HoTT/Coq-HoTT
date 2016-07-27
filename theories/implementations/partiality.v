d7477b4723a069fdb2bae27abd44f99e63bae5dc
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2016-07-27 15:30:17 +0200

    rename delay to partial

--- theories/implementations/partiality.v
+++ theories/implementations/partiality.v
@@ -8,32 +8,32 @@ Require Import
 Coercion trunctype_type : TruncType >-> Sortclass.
 Coercion equiv_fun : Equiv >-> Funclass.
 
-Module Export Delay.
+Module Export Partial.
 
 Section VarSec.
 
 Variable A : Type.
 
-Private Inductive delay : Type :=
-  | eta : A -> delay
-  | bot : Bottom delay
-  | sup : forall f : nat -> delay, (forall n, f n <= f (S n)) -> delay
+Private Inductive partial : Type :=
+  | eta : A -> partial
+  | bot : Bottom partial
+  | sup : forall f : nat -> partial, (forall n, f n <= f (S n)) -> partial
 
-with delayLe : Le delay :=
-  | delay_refl : Reflexive delayLe
+with partialLe : Le partial :=
+  | partial_refl : Reflexive partialLe
   | bot_least : forall x, bot <= x
   | sup_le_l : forall f p x, sup f p <= x -> forall n, f n <= x
   | sup_le_r : forall f p x, (forall n, f n <= x) -> sup f p <= x
 .
-Axiom delay_antisymm : AntiSymmetric delayLe.
-Axiom delayLe_hprop : is_mere_relation delay delayLe.
+Axiom partial_antisymm : AntiSymmetric partialLe.
+Axiom partialLe_hprop : is_mere_relation partial partialLe.
 
-Global Existing Instance delayLe.
-Global Existing Instance delayLe_hprop.
+Global Existing Instance partialLe.
+Global Existing Instance partialLe_hprop.
 
 Section Induction.
 
-Variables (P : delay -> Type)
+Variables (P : partial -> Type)
   (Q : forall x y (u : P x) (v : P y), x <= y -> Type).
 
 Record Inductors :=
@@ -42,7 +42,7 @@ Record Inductors :=
   ; ind_sup : forall f p (If : forall n, P (f n))
       (Ip : forall n, Q (f n) (f (S n)) (If n) (If (S n)) (p n)),
       P (sup f p)
-  ; ind_refl : forall x u, Q x x u u (delay_refl x)
+  ; ind_refl : forall x u, Q x x u u (partial_refl x)
   ; ind_bot_least : forall x v, Q bot x ind_bot v (bot_least x)
   ; ind_sup_le_l : forall f p x E If Ip u, Q (sup f p) x (ind_sup f p If Ip) u E ->
     forall n, Q (f n) x (If n) u (sup_le_l f p x E n)
@@ -50,80 +50,80 @@ Record Inductors :=
     Q (sup f p) x (ind_sup f p If Ip) u (sup_le_r f p x E)
 
   ; ind_antisymm : forall x y u v E1 E2, Q x y u v E1 -> Q y x v u E2 ->
-    delay_antisymm x y E1 E2 # u = v
+    partial_antisymm x y E1 E2 # u = v
   ; ind_prop : forall x y u v E, IsHProp (Q x y u v E)
   }.
 
-Definition delay_rect : Inductors -> forall x, P x :=
+Definition partial_rect : Inductors -> forall x, P x :=
   fun I x =>
-  fix delay_rect (x : delay) {struct x} : Inductors -> P x :=
+  fix partial_rect (x : partial) {struct x} : Inductors -> P x :=
     match x return (Inductors -> P x) with
     | eta x => fun I => ind_eta I x
     | bot => fun I => ind_bot I
-    | sup f p => fun I => ind_sup I f p (fun n => delay_rect (f n) I)
-        (fun n => delayLe_rect _ _ (p n) I)
+    | sup f p => fun I => ind_sup I f p (fun n => partial_rect (f n) I)
+        (fun n => partialLe_rect _ _ (p n) I)
     end
 
-  with delayLe_rect (x y : delay) (E : x <= y) {struct E}
-    : forall I : Inductors, Q x y (delay_rect x I) (delay_rect y I) E :=
-    match E in delayLe x' y'
-    return (forall I : Inductors, Q x' y' (delay_rect x' I) (delay_rect y' I) E)
+  with partialLe_rect (x y : partial) (E : x <= y) {struct E}
+    : forall I : Inductors, Q x y (partial_rect x I) (partial_rect y I) E :=
+    match E in partialLe x' y'
+    return (forall I : Inductors, Q x' y' (partial_rect x' I) (partial_rect y' I) E)
     with
-    | delay_refl x => fun I => ind_refl I x (delay_rect x I)
+    | partial_refl x => fun I => ind_refl I x (partial_rect x I)
     | bot_least x => fun I =>
-      ind_bot_least I x (delay_rect x I)
+      ind_bot_least I x (partial_rect x I)
     | sup_le_l f p x E n => fun I =>
-      ind_sup_le_l I f p x E (fun n => delay_rect (f n) I)
-        (fun n => delayLe_rect _ _ (p n) I)
-        (delay_rect x I)
-        (delayLe_rect _ _ E I) n
+      ind_sup_le_l I f p x E (fun n => partial_rect (f n) I)
+        (fun n => partialLe_rect _ _ (p n) I)
+        (partial_rect x I)
+        (partialLe_rect _ _ E I) n
     | sup_le_r f p x E => fun I =>
-      ind_sup_le_r I f p x E (fun n => delay_rect (f n) I)
-        (fun n => delayLe_rect _ _ (p n) I)
-        (delay_rect x I)
-        (fun n => delayLe_rect _ _ (E n) I)
+      ind_sup_le_r I f p x E (fun n => partial_rect (f n) I)
+        (fun n => partialLe_rect _ _ (p n) I)
+        (partial_rect x I)
+        (fun n => partialLe_rect _ _ (E n) I)
     end
 
-  for delay_rect x I.
+  for partial_rect x I.
 
-Definition delayLe_rect : forall (I : Inductors) (x y : delay) (E : x <= y),
-  Q x y (delay_rect I x) (delay_rect I y) E
+Definition partialLe_rect : forall (I : Inductors) (x y : partial) (E : x <= y),
+  Q x y (partial_rect I x) (partial_rect I y) E
   := fun I x y E =>
-  fix delay_rect (x : delay) {struct x} : Inductors -> P x :=
+  fix partial_rect (x : partial) {struct x} : Inductors -> P x :=
     match x return (Inductors -> P x) with
     | eta x => fun I => ind_eta I x
     | bot => fun I => ind_bot I
-    | sup f p => fun I => ind_sup I f p (fun n => delay_rect (f n) I)
-        (fun n => delayLe_rect _ _ (p n) I)
+    | sup f p => fun I => ind_sup I f p (fun n => partial_rect (f n) I)
+        (fun n => partialLe_rect _ _ (p n) I)
     end
 
-  with delayLe_rect (x y : delay) (E : x <= y) {struct E}
-    : forall I : Inductors, Q x y (delay_rect x I) (delay_rect y I) E :=
-    match E in delayLe x' y'
-    return (forall I : Inductors, Q x' y' (delay_rect x' I) (delay_rect y' I) E)
+  with partialLe_rect (x y : partial) (E : x <= y) {struct E}
+    : forall I : Inductors, Q x y (partial_rect x I) (partial_rect y I) E :=
+    match E in partialLe x' y'
+    return (forall I : Inductors, Q x' y' (partial_rect x' I) (partial_rect y' I) E)
     with
-    | delay_refl x => fun I => ind_refl I x (delay_rect x I)
+    | partial_refl x => fun I => ind_refl I x (partial_rect x I)
     | bot_least x => fun I =>
-      ind_bot_least I x (delay_rect x I)
+      ind_bot_least I x (partial_rect x I)
     | sup_le_l f p x E n => fun I =>
-      ind_sup_le_l I f p x E (fun n => delay_rect (f n) I)
-        (fun n => delayLe_rect _ _ (p n) I)
-        (delay_rect x I)
-        (delayLe_rect _ _ E I) n
+      ind_sup_le_l I f p x E (fun n => partial_rect (f n) I)
+        (fun n => partialLe_rect _ _ (p n) I)
+        (partial_rect x I)
+        (partialLe_rect _ _ E I) n
     | sup_le_r f p x E => fun I =>
-      ind_sup_le_r I f p x E (fun n => delay_rect (f n) I)
-        (fun n => delayLe_rect _ _ (p n) I)
-        (delay_rect x I)
-        (fun n => delayLe_rect _ _ (E n) I)
+      ind_sup_le_r I f p x E (fun n => partial_rect (f n) I)
+        (fun n => partialLe_rect _ _ (p n) I)
+        (partial_rect x I)
+        (fun n => partialLe_rect _ _ (E n) I)
     end
 
-  for delayLe_rect x y E I.
+  for partialLe_rect x y E I.
 
 End Induction.
 
 End VarSec.
 
-End Delay.
+End Partial.
 
 Section contents.
 Context `{Funext} `{Univalence}.
@@ -162,18 +162,18 @@ intros R. simple refine (Build_Inductors A _ _
 - intros _ _ ?? _;exact (rec_prop R _ _).
 Defined.
 
-Definition delay_rec : Recursors -> delay A -> T
-  := fun R => delay_rect _ _ _ (recursors_inductors R).
+Definition partial_rec : Recursors -> partial A -> T
+  := fun R => partial_rect _ _ _ (recursors_inductors R).
 
-Definition delayLe_rec : forall (R : Recursors) (x y : delay A) (E : x <= y),
-  Tle (delay_rec R x) (delay_rec R y)
-  := fun R => delayLe_rect _ _ _ (recursors_inductors R).
+Definition partialLe_rec : forall (R : Recursors) (x y : partial A) (E : x <= y),
+  Tle (partial_rec R x) (partial_rec R y)
+  := fun R => partialLe_rect _ _ _ (recursors_inductors R).
 
 End Recursion.
 
-Definition delayLe_rect0 (P : forall x y : delay A, x <= y -> Type)
+Definition partialLe_rect0 (P : forall x y : partial A, x <= y -> Type)
   {sP : forall x y E, IsHProp (P x y E)}
-  (val_refl : forall x, P x x (delay_refl A x))
+  (val_refl : forall x, P x x (partial_refl A x))
   (val_bot_least : forall x, P _ _ (bot_least A x))
   (val_sup_le_l : forall f p x E (Ip : forall n, P (f n) (f (S n)) (p n)),
     P (sup A f p) x E -> forall n, P (f n) x (sup_le_l A f p x E n))
@@ -181,58 +181,58 @@ Definition delayLe_rect0 (P : forall x y : delay A, x <= y -> Type)
     (forall n, P (f n) x (E n)) -> P (sup A f p) x (sup_le_r A f p x E))
   : forall x y E, P x y E.
 Proof.
-apply (delayLe_rect A (fun _ => Unit) (fun x y _ _ E => P x y E)).
+apply (partialLe_rect A (fun _ => Unit) (fun x y _ _ E => P x y E)).
 split;simpl;auto;simpl.
 intros. apply path_ishprop.
 Defined.
 
-Lemma delayLe_trans : Transitive (@le (delay A) _).
+Lemma partialLe_trans : Transitive (@le (partial A) _).
 Proof.
 hnf. intros x y z E;revert x y E z.
-apply (delayLe_rect0 (fun x y _ => forall z, _ -> _)).
+apply (partialLe_rect0 (fun x y _ => forall z, _ -> _)).
 - auto.
 - intros;apply bot_least.
 - intros;eapply sup_le_l;eauto.
 - intros;apply sup_le_r;auto.
 Qed.
 
-Global Instance delay_set : IsHSet (delay A).
+Global Instance partial_set : IsHSet (partial A).
 Proof.
 apply (@HSet.isset_hrel_subpaths _ (fun x y => x <= y /\ y <= x)).
-- intros x;split;apply delay_refl.
+- intros x;split;apply partial_refl.
 - apply _.
-- intros x y E;apply delay_antisymm;apply E.
+- intros x y E;apply partial_antisymm;apply E.
 Qed.
 
-Global Instance delay_order : PartialOrder (@le (delay A) _).
+Global Instance partial_order : PartialOrder (@le (partial A) _).
 Proof.
 repeat (split;try apply _).
-- apply delay_refl.
-- apply delayLe_trans.
+- apply partial_refl.
+- apply partialLe_trans.
 Qed.
 
-Definition delay_ind0 (P : delay A -> Type) {sP : forall x, IsHProp (P x)}
+Definition partial_ind0 (P : partial A -> Type) {sP : forall x, IsHProp (P x)}
   (val_eta : forall x, P (eta A x))
   (val_bot : P (bot A))
   (val_sup : forall f p, (forall n, P (f n)) -> P (sup A f p))
   : forall x, P x.
 Proof.
-apply (delay_rect A _ (fun _ _ _ _ _ => Unit)).
+apply (partial_rect A _ (fun _ _ _ _ _ => Unit)).
 split;simpl;auto.
 - intros;apply path_ishprop.
 - apply _.
 Defined.
 
-Definition delayLe_ind0 (P : forall a b : delay A, a <= b -> Type)
+Definition partialLe_ind0 (P : forall a b : partial A, a <= b -> Type)
   {sP : forall a b E, IsHProp (P a b E)}
-  (val_refl : forall a, P a a (delay_refl A a))
+  (val_refl : forall a, P a a (partial_refl A a))
   (val_bot_least : forall b, P (bot A) b (bot_least A b))
   (val_sup_le_l : forall f p x E, P _ _ E -> forall n, P _ _ (sup_le_l A f p x E n))
   (val_sup_le_r : forall f p x E, (forall n, P _ _ (E n)) ->
     P _ _ (sup_le_r A f p x E))
   : forall a b E, P a b E.
 Proof.
-apply (delayLe_rect A (fun _ => Unit) (fun a b _ _ E => P a b E)).
+apply (partialLe_rect A (fun _ => Unit) (fun a b _ _ E => P a b E)).
 split;simpl;auto.
 intros;apply path_ishprop.
 Defined.
@@ -251,10 +251,10 @@ simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _);simpl.
 Defined.
 
 Definition sim_le_recursors
-  : Recursors (delay A -> hProp) (fun P Q => forall x, Q x -> P x).
+  : Recursors (partial A -> hProp) (fun P Q => forall x, Q x -> P x).
 Proof.
 simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _);simpl.
-- intros a. apply (delay_rec _ _ (eta_le_recursors a)).
+- intros a. apply (partial_rec _ _ (eta_le_recursors a)).
 - intros _;exists Unit;apply _.
 - intros f p x. exists (forall n, f n x). apply _.
 - trivial.
@@ -265,13 +265,13 @@ simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _);simpl.
   apply TruncType.path_iff_hprop_uncurried. split;auto.
 Defined.
 
-Definition sim_le : delay A -> delay A -> hProp
-  := delay_rec _ _ sim_le_recursors.
+Definition sim_le : partial A -> partial A -> hProp
+  := partial_rec _ _ sim_le_recursors.
 
 Lemma sim_to_le : forall a b, sim_le a b -> a <= b.
 Proof.
-apply (delay_ind0 (fun a => forall b, _ -> _)).
-- intros a. apply (delay_ind0 (fun b => _ -> _)).
+apply (partial_ind0 (fun a => forall b, _ -> _)).
+- intros a. apply (partial_ind0 (fun b => _ -> _)).
   + intros b;simpl. intros []. reflexivity.
   + simpl. intros [].
   + intros f p E1.
@@ -286,12 +286,12 @@ Qed.
 
 Lemma le_sim_le_trans : forall a b, a <= b -> forall c, sim_le b c -> sim_le a c.
 Proof.
-exact (delayLe_rec _ _ sim_le_recursors).
+exact (partialLe_rec _ _ sim_le_recursors).
 Qed.
 
 Lemma sim_le_sup : forall f p x n, sim_le x (f n) -> sim_le x (sup A f p).
 Proof.
-intros f p;apply (delay_ind0 (fun x => forall n, _ -> _)).
+intros f p;apply (partial_ind0 (fun x => forall n, _ -> _)).
 - intros a n E. apply tr;exists n;apply E.
 - simpl. trivial.
 - intros g q IH n E.
@@ -303,7 +303,7 @@ Qed.
 
 Lemma sim_le_refl : forall a, sim_le a a.
 Proof.
-apply (delay_ind0 _).
+apply (partial_ind0 _).
 - reflexivity.
 - simpl;trivial.
 - intros f p IH. change (forall n, sim_le (f n) (sup A f p)).
@@ -312,14 +312,14 @@ Qed.
 
 Lemma le_to_sim : forall a b, a <= b -> sim_le a b.
 Proof.
-apply (delayLe_ind0 _).
+apply (partialLe_ind0 _).
 - apply sim_le_refl.
 - simpl. trivial.
 - intros f p x E IH;exact IH.
 - intros f p x E IH;exact IH.
 Qed.
 
-Lemma le_sim_rw : @le (delay A) _ = sim_le.
+Lemma le_sim_rw : @le (partial A) _ = sim_le.
 Proof.
 apply path_forall;intros a;apply path_forall;intros b.
 apply (ap trunctype_type).
@@ -358,8 +358,8 @@ End basics.
 
 Section monad.
 
-Definition bind_recursors {A B} : (A -> delay B) ->
-  Recursors A (delay B) le.
+Definition bind_recursors {A B} : (A -> partial B) ->
+  Recursors A (partial B) le.
 Proof.
 intros f.
 simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _ _);simpl.
@@ -372,12 +372,12 @@ simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _ _);simpl.
 - simpl. apply sup_le_r.
 Defined.
 
-Definition bind {A B} : delay A -> (A -> delay B) -> delay B
-  := fun x f => delay_rec _ _ _ (bind_recursors f) x.
+Definition bind {A B} : partial A -> (A -> partial B) -> partial B
+  := fun x f => partial_rec _ _ _ (bind_recursors f) x.
 
-Definition bind_le {A B} : forall (f : A -> delay B) a b, a <= b ->
+Definition bind_le {A B} : forall (f : A -> partial B) a b, a <= b ->
   bind a f <= bind b f
-  := fun f a b E => delayLe_rec _ _ _ (bind_recursors f) a b E.
+  := fun f a b E => partialLe_rec _ _ _ (bind_recursors f) a b E.
 
 Definition bind_eta_l {A B} : forall a f, bind (B:=B) (eta A a) f = f a
   := fun _ _ => idpath.
@@ -400,7 +400,7 @@ Qed.
 
 Definition bind_eta_r {A} : forall x, bind x (eta A) = x.
 Proof.
-apply (delay_ind0 _ _);try reflexivity.
+apply (partial_ind0 _ _);try reflexivity.
 intros f p IH.
 change (bind (sup A f p) (eta A)) with
   (sup A (λ n : nat, bind (f n) (eta A))
@@ -411,7 +411,7 @@ Defined.
 Lemma bind_assoc {A B C} : forall x f g, bind (B:=C) (bind (A:=A) (B:=B) x f) g =
   bind x (fun a => bind (f a) g).
 Proof.
-intros x f g;revert x;apply (delay_ind0 _ _).
+intros x f g;revert x;apply (partial_ind0 _ _).
 - reflexivity.
 - reflexivity.
 - intros s p IH.
