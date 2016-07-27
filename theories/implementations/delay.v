f51b0c4944bb93c67f0ae80102e6e6e4d4e12779
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2016-07-27 15:08:48 +0200

    recursive copy of the order on delay, delay forms a monad

--- theories/implementations/delay.v
+++ theories/implementations/delay.v
@@ -5,6 +5,9 @@ Require Import
   HoTTClasses.interfaces.abstract_algebra
   HoTTClasses.interfaces.orders.
 
+Coercion trunctype_type : TruncType >-> Sortclass.
+Coercion equiv_fun : Equiv >-> Funclass.
+
 Module Export Delay.
 
 Section VarSec.
@@ -122,10 +125,12 @@ End VarSec.
 
 End Delay.
 
-Section basics.
+Section contents.
 Context `{Funext} `{Univalence}.
 
+Section basics.
 Variable A : Type.
+Context `{IsHSet A}.
 
 Section Recursion.
 
@@ -161,7 +166,7 @@ Definition delay_rec : Recursors -> delay A -> T
   := fun R => delay_rect _ _ _ (recursors_inductors R).
 
 Definition delayLe_rec : forall (R : Recursors) (x y : delay A) (E : x <= y),
-  delay_rec R x <= delay_rec R y
+  Tle (delay_rec R x) (delay_rec R y)
   := fun R => delayLe_rect _ _ _ (recursors_inductors R).
 
 End Recursion.
@@ -218,8 +223,206 @@ split;simpl;auto.
 - apply _.
 Defined.
 
+Definition delayLe_ind0 (P : forall a b : delay A, a <= b -> Type)
+  {sP : forall a b E, IsHProp (P a b E)}
+  (val_refl : forall a, P a a (delay_refl A a))
+  (val_bot_least : forall b, P (bot A) b (bot_least A b))
+  (val_sup_le_l : forall f p x E, P _ _ E -> forall n, P _ _ (sup_le_l A f p x E n))
+  (val_sup_le_r : forall f p x E, (forall n, P _ _ (E n)) ->
+    P _ _ (sup_le_r A f p x E))
+  : forall a b E, P a b E.
+Proof.
+apply (delayLe_rect A (fun _ => Unit) (fun a b _ _ E => P a b E)).
+split;simpl;auto.
+intros;apply path_ishprop.
+Defined.
+
+Definition eta_le_recursors (a : A) : Recursors hProp (fun P Q => P -> Q).
+Proof.
+simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _);simpl.
+- intros b. exists (a = b). apply _.
+- exists Empty;apply _.
+- intros f p. exact (merely (exists n, f n)).
+- trivial.
+- intros _ [].
+- simpl. intros s p x E n En. apply E. apply tr;exists n;trivial.
+- simpl. intros s p x E. apply (Trunc_ind _);intros [n En]. apply (E n En).
+- hnf. intros. apply TruncType.path_iff_hprop_uncurried. split;trivial.
+Defined.
+
+Definition sim_le_recursors
+  : Recursors (delay A -> hProp) (fun P Q => forall x, Q x -> P x).
+Proof.
+simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _);simpl.
+- intros a. apply (delay_rec _ _ (eta_le_recursors a)).
+- intros _;exists Unit;apply _.
+- intros f p x. exists (forall n, f n x). apply _.
+- trivial.
+- simpl. trivial.
+- simpl. auto.
+- simpl. auto.
+- hnf;intros. apply path_forall. intros ?.
+  apply TruncType.path_iff_hprop_uncurried. split;auto.
+Defined.
+
+Definition sim_le : delay A -> delay A -> hProp
+  := delay_rec _ _ sim_le_recursors.
+
+Lemma sim_to_le : forall a b, sim_le a b -> a <= b.
+Proof.
+apply (delay_ind0 (fun a => forall b, _ -> _)).
+- intros a. apply (delay_ind0 (fun b => _ -> _)).
+  + intros b;simpl. intros []. reflexivity.
+  + simpl. intros [].
+  + intros f p E1.
+    change (merely (exists n, sim_le (eta A a) (f n)) -> eta A a <= sup A f p).
+    apply (Trunc_ind _);intros [n E2].
+    apply E1 in E2. transitivity (f n);trivial.
+    apply sup_le_l with p. reflexivity.
+- simpl. intros b _;apply bot_least.
+- intros f p IH b. change ((forall n, sim_le (f n) b) -> sup A f p <= b).
+  intros E. apply sup_le_r. intros n;apply IH;trivial.
+Qed.
+
+Lemma le_sim_le_trans : forall a b, a <= b -> forall c, sim_le b c -> sim_le a c.
+Proof.
+exact (delayLe_rec _ _ sim_le_recursors).
+Qed.
+
+Lemma sim_le_sup : forall f p x n, sim_le x (f n) -> sim_le x (sup A f p).
+Proof.
+intros f p;apply (delay_ind0 (fun x => forall n, _ -> _)).
+- intros a n E. apply tr;exists n;apply E.
+- simpl. trivial.
+- intros g q IH n E.
+  change (forall m, sim_le (g m) (sup A f p)). intros m.
+  apply IH with n. apply le_sim_le_trans with (sup A g q).
+  + apply sup_le_l with q. reflexivity.
+  + trivial.
+Qed.
+
+Lemma sim_le_refl : forall a, sim_le a a.
+Proof.
+apply (delay_ind0 _).
+- reflexivity.
+- simpl;trivial.
+- intros f p IH. change (forall n, sim_le (f n) (sup A f p)).
+  intros n. apply sim_le_sup with n. trivial.
+Qed.
+
+Lemma le_to_sim : forall a b, a <= b -> sim_le a b.
+Proof.
+apply (delayLe_ind0 _).
+- apply sim_le_refl.
+- simpl. trivial.
+- intros f p x E IH;exact IH.
+- intros f p x E IH;exact IH.
+Qed.
+
+Lemma le_sim_rw : @le (delay A) _ = sim_le.
+Proof.
+apply path_forall;intros a;apply path_forall;intros b.
+apply (ap trunctype_type).
+apply TruncType.path_iff_hprop_uncurried. simpl. split.
+- apply le_to_sim.
+- apply sim_to_le.
+Qed.
+
+Lemma not_eta_le_bot : forall a, ~ eta A a <= bot A.
+Proof.
+intros a. rewrite le_sim_rw. simpl. red;trivial.
+Qed.
+
+Lemma eta_le_eta : forall a b, eta A a <= eta A b -> a = b.
+Proof.
+intros a b;rewrite le_sim_rw;trivial.
+Qed.
+
+Global Instance eta_injective : Injective (eta A).
+Proof.
+intros a b E. apply eta_le_eta. rewrite E;reflexivity.
+Qed.
+
+Lemma eta_le_sup : forall a f p, eta A a <= sup A f p ->
+  merely (exists n, eta A a <= f n).
+Proof.
+intros a f p. rewrite le_sim_rw. trivial.
+Qed.
+
+Lemma sup_is_ub : forall f p n, f n <= sup A f p.
+Proof.
+intros f p n;apply sup_le_l with p. reflexivity.
+Qed.
+
 End basics.
 
+Section monad.
 
+Definition bind_recursors {A B} : (A -> delay B) ->
+  Recursors A (delay B) le.
+Proof.
+intros f.
+simple refine (Build_Recursors _ _ _ _ _ _ _ _ _ _ _ _);simpl.
+- exact f.
+- exact (bot B).
+- intros s p. exact (sup B s p).
+- reflexivity.
+- apply bot_least.
+- simpl. apply sup_le_l.
+- simpl. apply sup_le_r.
+Defined.
 
+Definition bind {A B} : delay A -> (A -> delay B) -> delay B
+  := fun x f => delay_rec _ _ _ (bind_recursors f) x.
 
+Definition bind_le {A B} : forall (f : A -> delay B) a b, a <= b ->
+  bind a f <= bind b f
+  := fun f a b E => delayLe_rec _ _ _ (bind_recursors f) a b E.
+
+Definition bind_eta_l {A B} : forall a f, bind (B:=B) (eta A a) f = f a
+  := fun _ _ => idpath.
+
+Definition bind_bot_l {A B} : forall f, bind (bot A) f = bot B
+  := fun _ => idpath.
+
+Definition bind_sup_l {A B} : forall f s p,
+  bind (sup A s p) f = sup B (fun n => bind (s n) f) (fun n => bind_le f _ _ (p n))
+  := fun _ _ _ => idpath.
+
+Lemma sup_extensionality {A} : forall f p g q, (forall n, f n = g n) ->
+  sup A f p = sup A g q.
+Proof.
+intros f p g q E.
+apply (antisymmetry le).
+- apply sup_le_r. intros n. rewrite E. apply sup_is_ub.
+- apply sup_le_r. intros n. rewrite <-E. apply sup_is_ub.
+Qed.
+
+Definition bind_eta_r {A} : forall x, bind x (eta A) = x.
+Proof.
+apply (delay_ind0 _ _);try reflexivity.
+intros f p IH.
+change (bind (sup A f p) (eta A)) with
+  (sup A (λ n : nat, bind (f n) (eta A))
+    (λ n : nat, bind_le (eta A) (f n) (f (S n)) (p n))).
+apply sup_extensionality. trivial.
+Defined.
+
+Lemma bind_assoc {A B C} : forall x f g, bind (B:=C) (bind (A:=A) (B:=B) x f) g =
+  bind x (fun a => bind (f a) g).
+Proof.
+intros x f g;revert x;apply (delay_ind0 _ _).
+- reflexivity.
+- reflexivity.
+- intros s p IH.
+  change (sup C (λ n, bind (bind (s n) f) g)
+  (λ n, bind_le g (bind (s n) f) (bind (s (S n)) f)
+     (bind_le f (s n) (s (S n)) (p n))) =
+  sup C (λ n, bind (s n) (λ a, bind (f a) g))
+    (λ n, bind_le (λ a, bind (f a) g) (s n) (s (S n)) (p n))).
+  apply sup_extensionality. apply IH.
+Defined.
+
+End monad.
+
+End contents.
