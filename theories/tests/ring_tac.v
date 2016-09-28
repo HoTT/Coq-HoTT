66af53f0a8f0faa41ce6444ad15606a201651bd9
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2016-09-28 17:33:40 +0200

    add ring_tac test on non abstract ring of nat (but still using ring_with_nat)

--- theories/tests/ring_tac.v
+++ theories/tests/ring_tac.v
@@ -38,3 +38,9 @@ apply (ring_quote.Quoting.eval_eqquote R).
 apply (prove_prequoted (naturals_to_semiring nat R)).
 reflexivity.
 Qed.
+
+Lemma test5 : forall x y : nat, x + (y * x) = x * (y + 1).
+Proof.
+intros;ring_with_nat.
+Qed.
+
