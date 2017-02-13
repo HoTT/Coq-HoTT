337f08878c47e4e5eb330997c22064220aee6578
SimonBoulier <simon.boulier@ens-rennes.fr>
2017-02-13 09:40:20 +0100

    Notations are now reserved in theories/Basics/Notations.v

--- theories/Basics/Notations.v
+++ theories/Basics/Notations.v
@@ -1,2 +1,4 @@
 Reserved Notation "f ^*" (at level 20).
 Reserved Notation "g o*E f" (at level 40, left associativity).
+Reserved Notation "D '_f' g" (at level 10).
+Reserved Notation "D1 ~d~ D2" (at level 65).
