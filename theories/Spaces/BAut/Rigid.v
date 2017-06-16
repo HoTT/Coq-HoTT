ace9f140a2bec8da79eeeea64524b951de6e4e49
Mike Shulman <shulman@sandiego.edu>
2017-06-16 00:50:01 -0700

    refactor proof of baut_prod_rigid_equiv

--- theories/Spaces/BAut/Rigid.v
+++ theories/Spaces/BAut/Rigid.v
@@ -11,17 +11,69 @@ Local Open Scope path_scope.
 (** * Rigid types *)
 
 Class IsRigid (A : Type) := 
-  contr_baut_rigid : Contr (BAut A).
-Existing Instance contr_baut_rigid.
+  path_aut_rigid : forall f g : A <~> A, f == g.
 
-Definition aut_idmap_rigid `{Univalence} {A : Type} `{IsRigid A}
-           (f : A <~> A)
-  : f == equiv_idmap.
+(** Assuming funext, rigidity is equivalent to contractibility of [A <~> A]. *)
+
+Global Instance contr_aut_rigid `{Funext} (A : Type) `{IsRigid A}
+  : Contr (A <~> A).
 Proof.
-  apply ap10, (ap equiv_fun).
-  refine ((eissect (path_baut (point _) (point _)) f)^ @ _).
-  apply moveR_equiv_V.
-  apply path_contr.
+  exists equiv_idmap.
+  intros f; apply path_equiv, path_arrow, path_aut_rigid.
+Defined.
+
+(** Assuming univalence, rigidity is equivalent to contractibility of [BAut A]. *)
+
+Global Instance contr_baut_rigid `{Univalence} {A : Type} `{IsRigid A}
+  : Contr (BAut A).
+Proof.
+  refine (contr_trunc_conn (Tr 0)).
+  intros Z W; baut_reduce.
+  refine (trunc_equiv (A <~> A)
+                      (path_baut (point (BAut A)) (point (BAut A)))).
+Defined.
+
+Definition rigid_contr_Baut `{Univalence} {A : Type} `{Contr (BAut A)}
+  : IsRigid A.
+Proof.
+  unfold IsRigid.
+  equiv_intro ((path_baut (point (BAut A)) (point (BAut A)))^-1) f.
+  equiv_intro ((path_baut (point (BAut A)) (point (BAut A)))^-1) g.
+  apply ap10, ap, ap, path_contr.
+Defined.
+
+(** ** HProps are rigid *)
+
+Global Instance rigid_ishprop
+       (A : Type) `{IsHProp A} : IsRigid A.
+Proof.
+  intros f g x; apply path_ishprop.
+Defined.
+
+(** ** Equivalences of BAut *)
+
+(** Under a truncatedness/connectedness assumption, multiplying by a rigid type doesn't change the automorphism oo-group. *)
+
+(** A lemma: a "monoid homomorphism up to homotopy" between endomorphism monoids restricts to automorphism groups. *)
+Definition aut_homomorphism_end `{Funext} {X Y : Type}
+           (M : (X -> X) -> (Y -> Y))
+           (Mid : M idmap == idmap)
+           (MC : forall f g, M (g o f) == M g o M f)
+  : (X <~> X) -> (Y <~> Y).
+Proof.
+  assert (MS : forall f g, Sect f g -> Sect (M f) (M g)).
+  { intros g f s x.
+    transitivity (M (f o g) x).
+    + symmetry. refine (MC g f x).
+    + transitivity (M idmap x).
+      * apply ap10, ap, path_arrow.
+        intros y; apply s.
+      * apply Mid. }
+  assert (ME : (forall f, IsEquiv f -> IsEquiv (M f))).
+  { intros f ?.
+    refine (isequiv_adjointify (M f) (M f^-1) _ _);
+      apply MS; [ apply eisretr | apply eissect ]. }
+  exact (fun f => (BuildEquiv _ _ (M f) (ME f _))).
 Defined.
 
 Definition baut_prod_rigid_equiv `{Univalence}
@@ -29,45 +81,25 @@ Definition baut_prod_rigid_equiv `{Univalence}
            `{IsTrunc n.+1 X} `{IsRigid A} `{IsConnected n.+1 A}
   : BAut X <~> BAut (X * A).
 Proof.
-  pose (f := fun Z:BAut X =>
-               (Z * A ; Trunc_functor -1 (ap (fun W => W * A)) (pr2 Z))
-               : BAut (X * A)).
-  assert (p : f (point (BAut X)) = (point (BAut (X * A))))
-    by (apply path_sigma_hprop; reflexivity).
-  refine (BuildEquiv _ _ f _).
+  refine (BuildEquiv _ _ (baut_prod_r X A) _).
   apply isequiv_surj_emb.
   { apply BuildIsSurjection; intros Z.
     baut_reduce.
-    exact (tr (point _ ; p)). }
+    refine (tr (point _ ; _)).
+    apply path_sigma_hprop; reflexivity. }
   { apply isembedding_isequiv_ap.
-    intros Z W; baut_reduce.
-    pose (L := fun e : X <~> X => equiv_functor_prod_r (B := A) e).
+    intros Z W.
+    pose (L := fun e : Z <~> W => equiv_functor_prod_r (B := A) e).
     refine (isequiv_commsq
-             L _
-             (path_baut (point (BAut X)) (point (BAut X)))
-             (path_baut (point (BAut (X*A))) (point (BAut (X*A))))
-             _).
-    { intros e; revert e; equiv_intro (equiv_path X X) e; cbn.
-      rewrite path_universe_uncurried_equiv_path.
-      apply moveR_equiv_M; cbn; unfold pr1_path.
-      refine (_ @ ap_compose f pr1
-                (path_sigma_hprop (X; tr 1) (X; tr 1) e)).
-      unfold f.
-      refine (_ @ (ap_compose pr1 (fun Z => Z * A)
-                (path_sigma_hprop (X; tr 1) (X; tr 1) e))^).
-      rewrite ap_pr1_path_sigma_hprop. unfold L.
-      assert (G : forall Y W (e:Y=W),
-                 path_universe_uncurried (equiv_functor_prod_r (equiv_path Y W e))
-                 = ap (fun Z => Z * A) e).
-      { intros ? ? []; cbn.
-        apply moveR_equiv_M; cbn.
-        apply path_equiv, path_arrow; intros x; reflexivity. }
-      apply G. }
-    clear f p.
+             L (ap (baut_prod_r X A))
+             (path_baut Z W)
+             (path_baut (baut_prod_r X A Z) (baut_prod_r X A W))
+             (fun e => (ap_baut_prod_r X A e)^)).
     refine ((isconnected_elim (Tr -1) (A := A) _ _).1).
     { apply contr_inhabited_hprop;
         [ exact _ | refine (merely_isconnected n A) ]. }
     intros a0.
+    baut_reduce.
     pose (M := fun f:X*A -> X*A => fun x => fst (f (x,a0))).
     assert (MH : forall (a:A) (f:X*A -> X*A) (x:X),
                fst (f (x,a)) = fst (f (x,a0))).
@@ -79,26 +111,8 @@ Proof.
       transitivity (fst (g (fst (f (x,a0)), snd (f (x,a0))))).
       - reflexivity.
       - apply MH. }
-    transparent assert (ME : (forall (f:X*A -> X*A), IsEquiv f -> IsEquiv (M f))).
-    { intros f ?.
-      refine (isequiv_adjointify (M f) (M f^-1) _ _).
-      - intros x. transitivity (M (f o f^-1) x).
-        + symmetry. refine (MC f^-1 f x).
-        + transitivity (M idmap x).
-          * apply ap10, ap, path_arrow. 
-            intros y; apply eisretr.
-          * reflexivity.
-      - intros x. transitivity (M (f^-1 o f) x).
-        + symmetry. refine (MC f f^-1 x).
-        + transitivity (M idmap x).
-          * apply ap10, ap, path_arrow. 
-            intros y; apply eissect.
-          * reflexivity. }
-    pose (M' := fun f:X*A<~>X*A => (BuildEquiv _ _ (M f) (ME f _))).
-    assert (Misretr : forall e, M' (L e) = e)
-      by (intros e; apply path_equiv, path_arrow;
-          intros x; reflexivity).
-    assert (Mker : forall f, M' f == equiv_idmap -> f == equiv_idmap).
+    pose (M' := aut_homomorphism_end M (fun x => 1) MC).
+    assert (Mker : forall f, M' f == 1%equiv -> f == 1%equiv).
     { unfold M', M; cbn. intros f p.
       pose (fh := fun x a => (MH a f x) @ p x).
       pose (g := fun x a => snd (f (x,a))).
@@ -109,7 +123,7 @@ Proof.
         intros [x a]; cbn.
         apply path_prod; [ apply fh | reflexivity ]. }
       intros [x a].
-      pose (gisid := aut_idmap_rigid (BuildEquiv _ _ (g x) (ge x))).
+      pose (gisid := path_aut_rigid (BuildEquiv _ _ (g x) (ge x)) 1).
       apply path_prod.
       - apply fh.
       - apply gisid. }
@@ -120,10 +134,11 @@ Proof.
       refine (Mker (g^-1 oE f) _).
       intros x.
       refine (MC f g^-1 x @ _).
-      change ((M g)^-1 (M f x) = x).
+      change ((M' g)^-1 (M f x) = x).
       apply moveR_equiv_V, p. }
-    refine (isequiv_adjointify L M' _ Misretr).
-    intros f.
-    apply path_equiv, path_arrow, Minj. 
-    intros x; reflexivity. }
+    refine (isequiv_adjointify L M' _ _);
+      intros e;
+      apply path_equiv, path_arrow;
+      try apply Minj;
+      intros x; reflexivity. }
 Defined.
