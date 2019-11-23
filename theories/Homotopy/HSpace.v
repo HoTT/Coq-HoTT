990dd33b5c0e35d654df1d6c5d2b9105633a040d
Ali Caglayan <alizter@gmail.com>
2019-11-23 14:21:37 +0000

    hspaces

---
+++ theories/Homotopy/HSpace.v
@@ -0,0 +1,55 @@
+Require Import Basics.
+Require Import Types.
+Require Import Truncations.
+Require Export Classes.interfaces.abstract_algebra.
+Import TrM.
+
+Local Open Scope trunc_scope.
+
+Class HSpace (X : pType) := {
+  hspace_op :> SgOp X;
+  hspace_left_identity :> LeftIdentity hspace_op (point _);
+  hspace_right_identity :> RightIdentity hspace_op (point _);
+}.
+
+Global Instance hspace_mon_unit `{HSpace X} : MonUnit X := point _.
+
+Definition hspace_id {X : pType} := point X.
+
+Section HSpaceProperties.
+
+  Context 
+   `{Univalence}
+    {A : pType}
+   `{HSpace A}
+   `{IsConnected 0 A}.
+
+  Global Instance isequiv_hspace_left_op
+    : forall (a : A), IsEquiv (a &).
+  Proof.
+    refine (conn_map_elim (-1) (unit_name hspace_id) _ _).
+    + exact (conn_point_incl hspace_id).
+    + apply Unit_ind.
+      serapply (isequiv_homotopic idmap).
+      intro a; symmetry.
+      apply left_identity.
+  Defined.
+
+  Global Instance isequiv_hspace_right_op
+    : forall (a : A), IsEquiv (& a).
+  Proof.
+    refine (conn_map_elim (-1) (unit_name hspace_id) _ _).
+    + exact (conn_point_incl hspace_id).
+    + apply Unit_ind.
+      serapply (isequiv_homotopic idmap).
+      intro a; symmetry.
+      apply right_identity.
+  Defined.
+
+  Definition equiv_hspace_left_op (a : A) : A <~> A
+    := Build_Equiv _ _ (a &) _.
+
+  Definition equiv_hspace_right_op (a : A) : A <~> A
+    := Build_Equiv _ _ (& a) _.
+
+End HSpaceProperties.
