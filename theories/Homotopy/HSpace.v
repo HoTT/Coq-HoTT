71de3f04ed34dfbc276e67945805201f49fc1389
Ali Caglayan <alizter@gmail.com>
2019-12-05 13:23:15 +0000

    renamed HSpace to IsHSpace

--- theories/Homotopy/HSpace.v
+++ theories/Homotopy/HSpace.v
@@ -6,13 +6,13 @@ Import TrM.
 
 Local Open Scope trunc_scope.
 
-Class HSpace (X : pType) := {
+Class IsHSpace (X : pType) := {
   hspace_op :> SgOp X;
   hspace_left_identity :> LeftIdentity hspace_op (point _);
   hspace_right_identity :> RightIdentity hspace_op (point _);
 }.
 
-Global Instance hspace_mon_unit `{HSpace X} : MonUnit X := point _.
+Global Instance hspace_mon_unit `{IsHSpace X} : MonUnit X := point _.
 
 Definition hspace_id {X : pType} := point X.
 
@@ -21,7 +21,7 @@ Section HSpaceProperties.
   Context 
    `{Univalence}
     {A : pType}
-   `{HSpace A}
+   `{IsHSpace A}
    `{IsConnected 0 A}.
 
   Global Instance isequiv_hspace_left_op
