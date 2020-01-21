c16cc1659f71629368f28e188cd9e1f4721f1b5f
Ali Caglayan <alizter@gmail.com>
2020-01-21 13:00:10 +0000

    seperated instances into module in natpair_integers.v

--- theories/Classes/implementations/natpair_integers.v
+++ theories/Classes/implementations/natpair_integers.v
@@ -1118,4 +1118,38 @@ Global Instance Z_zero_product@{} : ZeroProduct Z
 
 End contents.
 
+Module Instances.
+  Global Existing Instance T_set.
+  Global Existing Instance inject.
+  Global Existing Instance Tle_hprop.
+  Global Existing Instance Tlt_hprop.
+  Global Existing Instance Tapart_hprop.
+  Global Existing Instance Z_of_pair.
+  Global Existing Instance Z_of_N.
+  Global Existing Instance R_dec.
+  Global Existing Instance Z0.
+  Global Existing Instance Z1.
+  Global Existing Instance Z_plus.
+  Global Existing Instance Z_mult.
+  Global Existing Instance Z_negate.
+  Global Existing Instance Z_of_N_injective.
+  Global Existing Instance Zle.
+  Global Existing Instance ishprop_Zle.
+  Global Existing Instance Zle_cast_embedding.
+  Global Existing Instance Z_order.
+  Global Existing Instance Zle_dec.
+  Global Existing Instance Zlt.
+  Global Existing Instance ishprop_Zlt.
+  Global Existing Instance Z_strict_srorder.
+  Global Existing Instance Zlt_dec.
+  Global Existing Instance Zapart.
+  Global Existing Instance ishprop_Zapart.
+  Global Existing Instance Z_trivial_apart.
+  Global Existing Instance Z_to_ring.
+  Global Existing Instance Z_integers.
+  Global Existing Instance Z_abs.
+  Global Existing Instance Z_zero_product.
+  Global Existing Instance Z_of_N_morphism.
+End Instances.
+
 End NatPair.
