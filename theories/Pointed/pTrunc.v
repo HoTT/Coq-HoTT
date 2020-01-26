c2c22dcc230f72351ed84901f4a9ec58b2d3e668
Ali Caglayan <alizter@gmail.com>
2020-01-26 20:55:47 +0000

    results on pTr

--- theories/Pointed/pTrunc.v
+++ theories/Pointed/pTrunc.v
@@ -1,9 +1,11 @@
 Require Import Basics.
 Require Import Types.
+Require Import Spaces.Nat.
 Require Import Pointed.Core.
+Require Import Pointed.pMap.
 Require Import Pointed.pEquiv.
 Require Import Pointed.Loops.
-Require Import HoTT.Truncations.
+Require Import Truncations.
 Import TrM.
 
 Local Open Scope pointed_scope.
@@ -15,6 +17,47 @@ Global Instance ispointed_tr (n : trunc_index) (A : Type) `{IsPointed A}
 
 Definition pTr (n : trunc_index) (A : pType) : pType
   := Build_pType (Tr n A) _.
+
+Definition ptr {n : trunc_index} {A : pType} : A ->* pTr n A.
+Proof.
+  serapply Build_pMap.
+  + apply tr.
+  + reflexivity.
+Defined.
+
+Global Instance isequiv_ptr n (A : pType) `{IsTrunc n A}
+  : IsEquiv (@ptr n A) := _.
+
+Definition pequiv_ptr {n : trunc_index} {A : pType} `{IsTrunc n A}
+  : A <~>* pTr n A := Build_pEquiv _ _ ptr _.
+
+Definition pTr_rec n {X Y : pType} `{IsTrunc n Y}
+  (f : X ->* Y)
+  : pTr n X ->* Y.
+Proof.
+  serapply Build_pMap.
+  1: exact (Trunc_rec f).
+  apply point_eq.
+Defined.
+
+Definition equiv_ptr_rec `{Funext} {n} {X Y : pType} `{IsTrunc n Y}
+  : (pTr n X ->* Y) <~> (X ->* Y).
+Proof.
+  serapply equiv_adjointify.
+  { intro f.
+    refine (f o* ptr). }
+  1: serapply pTr_rec.
+  { intro f.
+    destruct f as [f p].
+    apply (ap (Build_pMap _ _ f)).
+    apply concat_1p. }
+  intro f.
+  apply path_pmap.
+  serapply Build_pHomotopy.
+  1: intro; by strip_truncations.
+  apply moveL_pV.
+  reflexivity.
+Defined.
 
 Definition ptr_functor {X Y : pType} (n : trunc_index) (f : X ->* Y)
   : pTr n X ->* pTr n Y := Build_pMap (pTr n X) (pTr n Y)
@@ -47,6 +90,24 @@ Proof.
   serapply Build_pEquiv'.
   1: apply equiv_path_Tr.
   reflexivity.
+Defined.
+
+Definition ptr_iterated_loops `{Univalence} (n : trunc_index)
+  (k : nat) (A : pType)
+  : pTr n (iterated_loops k A) <~>* iterated_loops k (pTr (n +2+ k).-2 A).
+Proof.
+  revert A n.
+  induction k.
+  { intros A n; cbn.
+    rewrite 2 (trunc_index_add_succ n).
+    rewrite trunc_index_add_minus_two.
+    reflexivity. }
+  intros A n.
+  cbn; etransitivity.
+  1: apply ptr_loops.
+  apply pequiv_loops_functor.
+  rewrite trunc_index_add_succ.
+  apply IHk.
 Defined.
 
 Definition ptr_loops_eq `{Univalence} (n : trunc_index) (A : pType)
