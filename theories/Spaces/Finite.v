6f6920f2466e39882150590ac2df09db71a8024b
Ali Caglayan <alizter@gmail.com>
2020-02-13 13:51:52 +0100

    fixed name

--- theories/Spaces/Finite.v
+++ theories/Spaces/Finite.v
@@ -1,4 +1,4 @@
 Require Export Spaces.Finite.Core.
 Require Export Spaces.Finite.Equiv.
-Require Export Spaces.Finite.IsFinite.
+Require Export Spaces.Finite.Finite.
 Require Export Spaces.Finite.Quotient.
