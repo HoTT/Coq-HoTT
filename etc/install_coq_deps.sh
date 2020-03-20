40ca8d3a9beac32d45b9867aa9314457431a4532
Johan Commelin <johan@commelin.net>
2020-03-20 21:33:54 +0100

    Update install_coq_deps.sh

--- etc/install_coq_deps.sh
+++ etc/install_coq_deps.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 sudo apt-get update -q
-sudo apt-get install -q curl sed grep wget tar libgtk-3-dev libgtk3sourceview-3.0-dev libexpat1-dev
-
+sudo apt-get install -q curl sed grep wget tar m4 autoconf libgtk-3-dev libgtksourceview-3.0-dev libexpat1-dev
