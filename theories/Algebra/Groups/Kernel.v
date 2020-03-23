5dff0f5ec738883c5519e1cc413fecb84298d91f
Ali Caglayan <alizter@gmail.com>
2020-03-23 17:45:19 +0100

    clean up in kernel

--- theories/Algebra/Groups/Kernel.v
+++ theories/Algebra/Groups/Kernel.v
@@ -1,7 +1,4 @@
-Require Import Basics.
-Require Import Types.
-Require Import Fibrations.
-Require Import AbelianGroup.
+Require Import Basics Types.
 Require Import Algebra.Groups.Group.
 Require Import Algebra.Groups.Subgroup.
 Require Import WildCat.
@@ -10,66 +7,50 @@ Require Import WildCat.
 
 Local Open Scope mc_scope.
 
-Definition grp_kernel {A B : Group} (f : A $-> B) : Group.
+Definition grp_kernel {A B : Group} (f : A $-> B) : Subgroup A.
 Proof.
-  srapply (Build_Group (hfiber f group_unit)); repeat split.
-  - (** Operation *)
-    intros [a p] [b q].
-    exists (sg_op a b).
-    rewrite grp_homo_op, p, q.
-    apply left_identity.
-  - (** Unit *)
-    exists mon_unit.
-    apply (grp_homo_unit f).
-  - (** Inverse *)
-    intros [a p].
-    exists (-a).
-    rewrite grp_homo_inv, p.
-    exact negate_mon_unit.
-  - (** HSet *)
-    exact _.
-  - intros [a p] [b q] [c r].
-    srapply path_sigma_hprop.
-    cbn; apply associativity.
-  - intros [a p].
-    srapply path_sigma_hprop.
-    cbn; apply left_identity.
-  - intros [a p].
-    srapply path_sigma_hprop.
-    cbn; apply right_identity.
-  - intros [a p].
-    srapply path_sigma_hprop.
-    cbn; apply left_inverse.
-  - intros [a p].
-    srapply path_sigma_hprop.
-    cbn; apply right_inverse.
+  snrapply Build_Subgroup.
+  { srapply (Build_Group (hfiber f group_unit)); repeat split.
+    - (** Operation *)
+      intros [a p] [b q].
+      exists (sg_op a b).
+      rewrite grp_homo_op, p, q.
+      apply left_identity.
+    - (** Unit *)
+      exists mon_unit.
+      apply (grp_homo_unit f).
+    - (** Inverse *)
+      intros [a p].
+      exists (-a).
+      rewrite grp_homo_inv, p.
+      exact negate_mon_unit.
+    - (** HSet *)
+      exact _.
+    - (** Associativity *)
+      intros [a p] [b q] [c r].
+      srapply path_sigma_hprop.
+      cbn; apply associativity.
+    - (** Left identity *)
+      intros [a p].
+      srapply path_sigma_hprop.
+      cbn; apply left_identity.
+    - (** Right identity *)
+      intros [a p].
+      srapply path_sigma_hprop.
+      cbn; apply right_identity.
+    - (** Left inverse *)
+      intros [a p].
+      srapply path_sigma_hprop.
+      cbn; apply left_inverse.
+    - (** Right inverse *)
+      intros [a p].
+      srapply path_sigma_hprop.
+      cbn; apply right_inverse. }
+  (** The kernel is a subgroup of the source of the map *)
+  snrapply Build_IsSubgroup.
+  { snrapply Build_GroupHomomorphism.
+    1: exact pr1.
+    intros ??; reflexivity. }
+  hnf; cbn; intros x y p.
+  by apply path_sigma_hprop.
 Defined.
-
-Definition grp_homo_kernel_pr1 {A B : Group} (f : A $-> B)
-  : (grp_kernel f) $-> A.
-Proof.
-  srapply Build_GroupHomomorphism.
-  - apply pr1.
-  - intros a b; reflexivity.
-Defined.
-
-Global Instance isinjective_grp_homo_kernel_pr1 {A B : Group}
-  (f : A $-> B) : IsInjective (grp_homo_kernel_pr1 f).
-Proof.
-  intros a b.
-  apply path_sigma_hprop.
-Defined.
-
-Global Instance issubgroup_grp_kernel {A B : Group}
-  (f : A $-> B) : IsSubgroup (grp_kernel f) A.
-Proof.
-  srapply Build_IsSubgroup.
-  + exact (grp_homo_kernel_pr1 f).
-  + exact (isinjective_grp_homo_kernel_pr1 _).
-Defined.
-
-Definition Kernel {A B : Group} (f : A $-> B)
-  : Subgroup A := Build_Subgroup A (grp_kernel f) _.
-
-Global Instance isnormal_abkernel {A : AbGroup} {B : Group} (f : (A : Group) $-> B)
-  : IsNormalSubgroup (Kernel f) := _.
