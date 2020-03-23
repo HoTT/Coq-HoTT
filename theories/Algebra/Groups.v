dacc8b47cfdf1a3ca2f4c57eb7df7427bf0180f2
Johan Commelin <johan@commelin.net>
2020-03-23 16:34:54 +0000

    Add new files to indices

--- theories/Algebra/Groups.v
+++ theories/Algebra/Groups.v
@@ -4,6 +4,8 @@ Require Export HoTT.Algebra.Groups.Group.
 Require Export HoTT.Algebra.Groups.Subgroup.
 Require Export HoTT.Algebra.Groups.QuotientGroup.
 Require Export HoTT.Algebra.Groups.AbelianGroup.
+Require Export HoTT.Algebra.Groups.Image.
+Require Export HoTT.Algebra.Groups.Kernel.
 
 (** Examples *)
 
