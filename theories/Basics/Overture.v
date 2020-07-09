22c1c22d8c681df703f16784987e85ae4f8466cc
Ali Caglayan <alizter@gmail.com>
2020-07-09 16:33:37 +0100

    fixed typo in reflexivity tactic causing universe increase

--- theories/Basics/Overture.v
+++ theories/Basics/Overture.v
@@ -70,7 +70,7 @@ Ltac reflexivity :=
       let R := match goal with |- ?R ?x ?y => constr:(R) end in
       let pre_proof_term_head := constr:(@reflexivity _ R _) in
       let proof_term_head := (eval cbn in pre_proof_term_head) in
-      apply (pre_proof_term_head : forall x, R x x)).
+      apply (proof_term_head : forall x, R x x)).
 
 (** Even if we weren't using [cbn], we would have to redefine symmetry, since the built-in Coq version is sometimes too smart for its own good, and will occasionally fail when it should not. *)
 Ltac symmetry :=
