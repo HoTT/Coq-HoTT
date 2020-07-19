69da2fc8b7a06e7df9c5bd9c02992ad8c83da195
Ali Caglayan <alizter@gmail.com>
2020-07-19 18:12:09 +0100

    cleanup in Circle.v

--- theories/HIT/Circle.v
+++ theories/HIT/Circle.v
@@ -1,235 +1,242 @@
 (* -*- mode: coq; mode: visual-line -*- *)
-
-(** * Theorems about the circle [S1]. *)
-
-Require Import HoTT.Basics HoTT.Types HProp.
-Require Import HSet.
-Require Import Spaces.Pos.
-Require Import Spaces.Int.
+Require Import Basics Types.
+Require Import HProp HSet.
+Require Import Spaces.Pos Spaces.Int.
 Require Import HIT.Coeq.
 Require Import Modalities.Modality Truncations.
 Require Import Cubical.DPath.
+
+(** * Theorems about the [Circle]. *)
 
 Local Open Scope path_scope.
 
 Generalizable Variables X A B f g n.
 
-(* ** Definition of the circle. *)
+(* ** Definition of the [Circle]. *)
 
 (** We define the circle as the coequalizer of two copies of the identity map of [Unit].  This is easily equivalent to the naive definition
 
 <<<
-Private Inductive S1 : Type0 :=
-| base : S1
+Private Inductive Circle : Type0 :=
+| base : Circle
 | loop : base = base.
 >>>
 
 but it allows us to apply the flattening lemma directly rather than having to pass across that equivalence.  *)
 
-Definition S1 := @Coeq Unit Unit idmap idmap.
-Definition base : S1 := coeq tt.
+(** The circle is defined to be the coequalizer of two copies of the identity map on [Unit]. *)
+Definition Circle := @Coeq Unit Unit idmap idmap.
+
+(** It has a basepoint. *)
+Definition base : Circle := coeq tt.
+
+(** And a non-trivial path. *)
 Definition loop : base = base := cglue tt.
 
-Definition S1_ind (P : S1 -> Type) (b : P base) (l : loop # b = b)
-  : forall (x:S1), P x.
+(** Here is a notation for the circle that can be imported. *)
+Module CircleNotation.
+  Notation S1 := Circle.
+End CircleNotation.
+
+(** Circle induction *)
+Definition Circle_ind (P : Circle -> Type)
+  (b : P base) (l : loop # b = b)
+  : forall (x : Circle), P x.
 Proof.
   refine (Coeq_ind P (fun u => transport P (ap coeq (path_unit tt u)) b) _).
   intros []; exact l.
 Defined.
 
-Definition S1_ind_beta_loop
-           (P : S1 -> Type) (b : P base) (l : loop # b = b)
-: apD (S1_ind P b l) loop = l
+(** Computation rule for circle induction. *)
+Definition Circle_ind_beta_loop (P : Circle -> Type)
+  (b : P base) (l : loop # b = b)
+  : apD (Circle_ind P b l) loop = l
   := Coeq_ind_beta_cglue P _ _ tt.
 
-(** But we want to allow the user to forget that we've defined the circle in that way. *)
-Arguments S1 : simpl never.
+(** We mark [Circle], [base] and [loop] to never be simplified by [simpl] or [cbn] in order to hide how we defined it from the user. *) 
+Arguments Circle : simpl never.
 Arguments base : simpl never.
 Arguments loop : simpl never.
-Arguments S1_ind_beta_loop : simpl never.
+Arguments Circle_ind_beta_loop : simpl never.
 
-(* ** The non-dependent eliminator *)
+(** The recursion princple or non-dependent eliminator. *)
+Definition Circle_rec (P : Type) (b : P) (l : b = b)
+  : Circle -> P
+  := Circle_ind (fun _ => P) b (transport_const _ _ @ l).
 
-Definition S1_rec (P : Type) (b : P) (l : b = b)
-  : S1 -> P
-  := S1_ind (fun _ => P) b (transport_const _ _ @ l).
-
-Definition S1_rec_beta_loop (P : Type) (b : P) (l : b = b)
-  : ap (S1_rec P b l) loop = l.
+(** Computation rule for non-dependent eliminator. *)
+Definition Circle_rec_beta_loop (P : Type) (b : P) (l : b = b)
+  : ap (Circle_rec P b l) loop = l.
 Proof.
-  unfold S1_rec.
+  unfold Circle_rec.
   refine (cancelL (transport_const loop b) _ _ _).
-  refine ((apD_const (S1_ind (fun _ => P) b _) loop)^ @ _).
-  refine (S1_ind_beta_loop (fun _ => P) _ _).
+  refine ((apD_const (Circle_ind (fun _ => P) b _) loop)^ @ _).
+  refine (Circle_ind_beta_loop (fun _ => P) _ _).
 Defined.
 
-(* ** The loop space of the circle is the Integers. *)
+(** The [Circle] is pointed by [base]. *)
+Global Instance ispointed_Circle : IsPointed Circle := base.
 
-Global Instance ispointed_S1 : IsPointed S1 := base.
+(** ** In this section we prove that the loop space of the [Circle] is the Integers [Int]. This is the encode-decode style proof a la Licata-Shulman. *)
 
-(** We use encode-decode. *)
+Section EncodeDecode.
+  (** We assume univalence throughout this section. *)
+  Context `{Univalence}.
 
-Section AssumeUnivalence.
-Context `{Univalence}.
+  (** First we define the type of codes, this is a type family over the circle. This can be thought of as the covering space by the homotopical real numbers. It is defined by mapping loop to the path given by univalence applied to the automorphism of the integers. We will show that the section of this family at [base] is equivalent to the loop space of the circle. Giving us an equivalence [base = base <~> Int]. *)
+  Definition Circle_code : Circle -> Type
+    := Circle_rec Type Int (path_universe int_succ).
 
-Definition S1_code : S1 -> Type
-  := S1_rec Type Int (path_universe int_succ).
+  (** Transporting along [loop] gives us the successor automorphism on [Int]. *)
+  Definition transport_Circle_code_loop (z : Int)
+    : transport Circle_code loop z = int_succ z.
+  Proof.
+    refine (transport_compose idmap Circle_code loop z @ _).
+    unfold Circle_code; rewrite Circle_rec_beta_loop.
+    apply transport_path_universe.
+  Defined.
 
-(* Transporting in the codes fibration is the successor autoequivalence. *)
+  (** Transporting along [loop^] gives us the predecessor on [Int]. *)
+  Definition transport_Circle_code_loopV (z : Int)
+    : transport Circle_code loop^ z = int_pred z.
+  Proof.
+    refine (transport_compose idmap Circle_code loop^ z @ _).
+    rewrite ap_V.
+    unfold Circle_code; rewrite Circle_rec_beta_loop.
+    rewrite <- (path_universe_V int_succ).
+    apply transport_path_universe.
+  Defined.
 
-Definition transport_S1_code_loop (z : Int)
-  : transport S1_code loop z = int_succ z.
-Proof.
-  refine (transport_compose idmap S1_code loop z @ _).
-  unfold S1_code; rewrite S1_rec_beta_loop.
-  apply transport_path_universe.
-Defined.
+  (** To turn a path in [Circle] based at [base] into a code we transport along it. We call this encoding. *)
+  Definition Circle_encode (x:Circle) : (base = x) -> Circle_code x
+    := fun p => p # zero.
 
-Definition transport_S1_code_loopV (z : Int)
-  : transport S1_code loop^ z = int_pred z.
-Proof.
-  refine (transport_compose idmap S1_code loop^ z @ _).
-  rewrite ap_V.
-  unfold S1_code; rewrite S1_rec_beta_loop.
-  rewrite <- (path_universe_V int_succ).
-  apply transport_path_universe.
-Defined.
-
-(* Encode by transporting *)
-
-Definition S1_encode (x:S1) : (base = x) -> S1_code x
-  := fun p => p # zero.
-
-(* Decode by iterating loop. *)
-
-Definition S1_decode (x : S1) : S1_code x -> (base = x).
-Proof.
-  revert x; refine (S1_ind (fun x => S1_code x -> base = x) (loopexp loop) _).
-  apply path_forall; intros z; simpl in z.
-  refine (transport_arrow _ _ _ @ _).
-  refine (transport_paths_r loop _ @ _).
-  rewrite transport_S1_code_loopV.
-  destruct z as [n| |n].
-  2: apply concat_Vp.
-  { rewrite <- int_neg_pos_succ.
-    unfold loopexp, loopexp_pos.
+  (** TODO: explain this proof in more detail. *)
+  (** Turning a code into a path based at [base]. We call this decoding. *)
+  Definition Circle_decode (x : Circle) : Circle_code x -> (base = x).
+  Proof.
+    revert x; refine (Circle_ind (fun x => Circle_code x -> base = x) (loopexp loop) _).
+    apply path_forall; intros z; simpl in z.
+    refine (transport_arrow _ _ _ @ _).
+    refine (transport_paths_r loop _ @ _).
+    rewrite transport_Circle_code_loopV.
+    destruct z as [n| |n].
+    2: apply concat_Vp.
+    { rewrite <- int_neg_pos_succ.
+      unfold loopexp, loopexp_pos.
+      rewrite pos_peano_ind_beta_pos_succ.
+      apply concat_pV_p. }
+    induction n as [|n nH] using pos_peano_ind.
+    1: apply concat_1p.
+    rewrite <- pos_add_1_r.
+    change (pos (n + 1)%pos)
+      with (int_succ (pos n)).
+    rewrite int_pred_succ.
+    cbn; rewrite pos_add_1_r.
+    unfold loopexp_pos.
     rewrite pos_peano_ind_beta_pos_succ.
-    apply concat_pV_p. }
-  induction n as [|n nH] using pos_peano_ind.
-  1: apply concat_1p.
-  rewrite <- pos_add_1_r.
-  change (pos (n + 1)%pos)
-    with (int_succ (pos n)).
-  rewrite int_pred_succ.
-  cbn; rewrite pos_add_1_r.
-  unfold loopexp_pos.
-  rewrite pos_peano_ind_beta_pos_succ.
-  reflexivity.
-Defined.
+    reflexivity.
+  Defined.
 
-(* The nontrivial part of the proof that decode and encode are equivalences is showing that decoding followed by encoding is the identity on the fibers over [base]. *)
+  (** The non-trivial part of the proof that decode and encode are equivalences is showing that decoding followed by encoding is the identity on the fibers over [base]. *)
+  Definition Circle_encode_loopexp (z:Int)
+    : Circle_encode base (loopexp loop z) = z.
+  Proof.
+    destruct z as [n | | n]; unfold Circle_encode.
+    - induction n using pos_peano_ind; simpl in *.
+      + refine (moveR_transport_V _ loop _ _ _).
+        by symmetry; apply transport_Circle_code_loop.
+      + unfold loopexp_pos.
+        rewrite pos_peano_ind_beta_pos_succ.
+        rewrite transport_pp.
+        refine (moveR_transport_V _ loop _ _ _).
+        refine (_ @ (transport_Circle_code_loop _)^).
+        refine (IHn @ _^).
+        rewrite int_neg_pos_succ.
+        by rewrite int_succ_pred.
+    - reflexivity.
+    - induction n using pos_peano_ind; simpl in *.
+      + by apply transport_Circle_code_loop.
+      + unfold loopexp_pos.
+        rewrite pos_peano_ind_beta_pos_succ.
+        rewrite transport_pp.
+        refine (moveR_transport_p _ loop _ _ _).
+        refine (_ @ (transport_Circle_code_loopV _)^).
+        refine (IHn @ _^).
+        rewrite <- pos_add_1_r.
+        change (int_pred (int_succ (pos n)) = pos n).
+        apply int_pred_succ.
+  Defined.
 
-Definition S1_encode_loopexp (z:Int)
-  : S1_encode base (loopexp loop z) = z.
-Proof.
-  destruct z as [n | | n]; unfold S1_encode.
-  - induction n using pos_peano_ind; simpl in *.
-    + refine (moveR_transport_V _ loop _ _ _).
-        by symmetry; apply transport_S1_code_loop.
-    + unfold loopexp_pos.
-      rewrite pos_peano_ind_beta_pos_succ.
-      rewrite transport_pp.
-      refine (moveR_transport_V _ loop _ _ _).
-      refine (_ @ (transport_S1_code_loop _)^).
-      refine (IHn @ _^).
-      rewrite int_neg_pos_succ.
-      by rewrite int_succ_pred.
-  - reflexivity.
-  - induction n using pos_peano_ind; simpl in *.
-    + by apply transport_S1_code_loop.
-    + unfold loopexp_pos.
-      rewrite pos_peano_ind_beta_pos_succ.
-      rewrite transport_pp.
-      refine (moveR_transport_p _ loop _ _ _).
-      refine (_ @ (transport_S1_code_loopV _)^).
-      refine (IHn @ _^).
-      rewrite <- pos_add_1_r.
-      change (int_pred (int_succ (pos n)) = pos n).
-      apply int_pred_succ.
-Defined.
+  (** Now we put it together. *)
+  Definition Circle_encode_isequiv (x:Circle) : IsEquiv (Circle_encode x).
+  Proof.
+   refine (isequiv_adjointify (Circle_encode x) (Circle_decode x) _ _).
+    (* Here we induct on [x:Circle].  We just did the case when [x] is [base]. *)
+    - refine (Circle_ind (fun x => Sect (Circle_decode x) (Circle_encode x))
+        Circle_encode_loopexp _ _).
+      (* What remains is easy since [Int] is known to be a set. *)
+      by apply path_forall; intros z; apply hset_path2.
+    (* The other side is trivial by path induction. *)
+    - intros []; reflexivity.
+  Defined.
 
-(* Now we put it together. *)
+  (** Finally giving us an equivalence between the loop space of the [Circle] and [Int]. *)
+  Definition equiv_loopCircle_int : (base = base) <~> Int
+    := Build_Equiv _ _ (Circle_encode base) (Circle_encode_isequiv base).
 
-Definition S1_encode_isequiv (x:S1) : IsEquiv (S1_encode x).
-Proof.
- refine (isequiv_adjointify (S1_encode x) (S1_decode x) _ _).
-  (* Here we induct on [x:S1].  We just did the case when [x] is [base]. *)
-  - refine (S1_ind (fun x => Sect (S1_decode x) (S1_encode x))
-                   S1_encode_loopexp _ _).
-    (* What remains is easy since [Int] is known to be a set. *)
-    by apply path_forall; intros z; apply hset_path2.
-  (* The other side is trivial by path induction. *)
-  - intros []; reflexivity.
-Defined.
+End EncodeDecode.
 
-Definition equiv_loopS1_int : (base = base) <~> Int
-  := Build_Equiv _ _ (S1_encode base) (S1_encode_isequiv base).
-
-(** ** Connectedness and truncatedness *)
+(** ** Connectedness and truncatedness of the [Circle] *)
 
 (** The circle is 0-connected. *)
-Global Instance isconnected_S1 : IsConnected 0 S1.
+Global Instance isconnected_Circle `{Univalence} : IsConnected 0 Circle.
 Proof.
   apply is0connected_merely_allpath.
-  - exact (tr base).
-  - simple refine (S1_ind _ _ _).
-    + simple refine (S1_ind _ _ _).
-      * exact (tr 1).
-      * apply path_ishprop.
+  1: exact (tr base).
+  srefine (Circle_ind _ _ _).
+  - simple refine (Circle_ind _ _ _).
+    + exact (tr 1).
     + apply path_ishprop.
+  - apply path_ishprop.
 Defined.
 
 (** It follows that the circle is a 1-type. *)
-Global Instance istrunc_S1 : IsTrunc 1 S1.
+Global Instance istrunc_Circle `{Univalence} : IsTrunc 1 Circle.
 Proof.
   intros x y.
-  assert (p := merely_path_is0connected S1 base x).
-  assert (q := merely_path_is0connected S1 base y).
+  assert (p := merely_path_is0connected Circle base x).
+  assert (q := merely_path_is0connected Circle base y).
   strip_truncations.
   destruct p, q.
-  refine (trunc_equiv' (n := 0) Int equiv_loopS1_int^-1).
+  refine (trunc_equiv' (n := 0) Int equiv_loopCircle_int^-1).
 Defined.
-
 
 (** ** Iteration of equivalences *)
 
-(** If [P : S1 -> Type] is defined by a type [X] and an autoequivalence [f], then the image of [n:Int] regarded as in [base = base] is [iter_int f n]. *)
-Definition S1_action_is_iter X (f : X <~> X) (n : Int) (x : X)
-: transport (S1_rec Type X (path_universe f)) (equiv_loopS1_int^-1 n) x
+(** If [P : Circle -> Type] is defined by a type [X] and an autoequivalence [f], then the image of [n : Int] regarded as in [base = base] is [iter_int f n]. *)
+Definition Circle_action_is_iter `{Univalence} X (f : X <~> X) (n : Int) (x : X)
+: transport (Circle_rec Type X (path_universe f)) (equiv_loopCircle_int^-1 n) x
   = int_iter f n x.
 Proof.
   refine (_ @ loopexp_path_universe _ _ _).
   refine (transport_compose idmap _ _ _ @ _).
   refine (ap (fun p => transport idmap p x) _).
-  unfold equiv_loopS1_int; cbn.
-  unfold S1_decode; simpl.
+  unfold equiv_loopCircle_int; cbn.
+  unfold Circle_decode; simpl.
   rewrite ap_loopexp.
   refine (ap (fun p => loopexp p n) _).
-  apply S1_rec_beta_loop.
-Qed.
-
-End AssumeUnivalence.
-
-(* An induction principle for S1 that produces a DPath *)
-Definition S1_ind_dp (P : S1 -> Type) (b : P base)
-  (bl : DPath P loop b b) (x : S1) : P x
-  := S1_ind P b (dp_path_transport^-1 bl) x.
-
-Definition S1_ind_dp_beta_loop (P : S1 -> Type) (b : P base)
-  (bl : DPath P loop b b) : dp_apD (S1_ind_dp P b bl) loop = bl.
-Proof.
-  apply dp_apD_path_transport.
-  exact (S1_ind_beta_loop _ _ _).
+  apply Circle_rec_beta_loop.
 Defined.
 
+(** An alternative induction principle for Circle that produces a DPath. *)
+Definition Circle_ind_dp (P : Circle -> Type) (b : P base)
+  (bl : DPath P loop b b) (x : Circle) : P x
+  := Circle_ind P b (dp_path_transport^-1 bl) x.
 
+Definition Circle_ind_dp_beta_loop (P : Circle -> Type) (b : P base)
+  (bl : DPath P loop b b) : dp_apD (Circle_ind_dp P b bl) loop = bl.
+Proof.
+  apply dp_apD_path_transport.
+  exact (Circle_ind_beta_loop _ _ _).
+Defined.
