89787b5e8474e67486174651cc6b1422b930e7b0
Jason Gross <jgross@mit.edu>
2021-04-13 13:40:23 -0400

    Excise the alternate standard library, use -noinit
    We now use `-noinit` rather than passing `-coqlib`.  I've also fixed
    some of the previously dead paths such as `README.markdown`, and updated
    various files to know about the recent update of binding `contrib` to
    `HoTT.Contrib` rather than `""`.
    
    N.B. I haven't tested the install target, so there's a chance I broke
    something.

--- coq/theories/.dir-locals.el
+++
@@ -1,4 +0,0 @@
-((coq-mode . ((eval . (let ((default-directory (locate-dominating-file
-                                                buffer-file-name ".dir-locals.el")))
-                        (make-local-variable 'coq-prog-args)
-                        (setq coq-prog-args `("-indices-matter" "-nois" "-coqlib" ,(expand-file-name "..") "-R" ,(expand-file-name ".") "Coq" "-emacs")))))))
--- coq/theories/Init/Prelude.v
+++
@@ -1,1 +0,0 @@
-(** This file is empty; it only exists because the current makefile expects it to. *)
