fe4cbfb6c541139e95e9650005014e736668117f
Jarl G. Taxeraas Flaten <jtaxers@uwo.ca>
2022-09-01 08:52:14 -0400

    Make Ext definitionally a pointed type

--- theories/Algebra/AbGroups/AbSES/Ext.v
+++ theories/Algebra/AbGroups/AbSES/Ext.v
@@ -4,9 +4,7 @@ Require Import AbSES.Pullback AbSES.Pushout BaerSum.
 
 (** * The set [Ext B A] of abelian group extensions *)
 
-Definition Ext (B A : AbGroup) := Tr 0 (AbSES B A).
-
-Global Instance ispointed_ext {B A : AbGroup} : IsPointed (Ext B A) := tr (point _).
+Definition Ext (B A : AbGroup) := pTr 0 (AbSES B A).
 
 (** An extension [E : AbSES B A] is trivial in [Ext B A] if and only if [E] merely splits. *)
 Proposition iff_ab_ext_trivial_split `{Univalence} {B A : AbGroup} (E : AbSES B A)
