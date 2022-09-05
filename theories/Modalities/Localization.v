71f92633f8904bad2b70bcd8dc677e78b4f8b727
Ali Caglayan <alizter@gmail.com>
2022-09-06 01:41:06 +0200

    remove 8.15 compat
    ps-id: 9ff58390-9be8-4335-84d7-7631ef822c7a

--- theories/Modalities/Localization.v
+++ theories/Modalities/Localization.v
@@ -354,13 +354,9 @@ Proof.
     intros ? T; unfold IsLocal.
     nrefine (istrunc_forall@{a i i}); try assumption.
     intros i.
-    (* coq#14758 compat (coq 8.15) *)
-    first [apply ishprop_ooextendable@{a a i i i
-                                       i i i i i
-                                       i i i}
-           | apply ishprop_ooextendable@{a a i i i
-                                         i i i i i
-                                         i i i i i}].
+    apply ishprop_ooextendable@{a a i i i
+                                i i i i i
+                                i i i i i}.
   - apply islocal_equiv_islocal.
   - apply islocal_localize.
   - cbn. intros Q Q_inO.
