cc1a7f6400d86876975de5fd01e201970ca758f2
Dan Christensen <jdc@uwo.ca>
2022-10-11 21:06:17 -0400

    BoundedSearch: don't require P to be a family of HProps

--- theories/BoundedSearch.v
+++ theories/BoundedSearch.v
@@ -6,7 +6,6 @@ Require Import HoTT.Spaces.Nat.
 Section bounded_search.
 
   Context (P : nat -> Type)
-          {P_hprop : forall n, IsHProp (P n)}
           (P_dec : forall n, Decidable (P n))
           (P_inhab : hexists (fun n => P n)).
   
@@ -17,9 +16,9 @@ Section bounded_search.
   Local Definition minimal (n : nat) : Type := forall m : nat, P m -> n <= m.
 
   (** If we assume [Funext], then [minimal n] is a proposition.  But to avoid needing [Funext], we propositionally truncate it. *)
-  Local Definition min_n_Type : Type := { n : nat & (P n) * merely (minimal n) }.
+  Local Definition min_n_Type : Type := { n : nat & merely (P n) * merely (minimal n) }.
 
-  Local Definition ishpropmin_n : IsHProp min_n_Type.
+  Local Instance ishpropmin_n : IsHProp min_n_Type.
   Proof.
     apply ishprop_sigma_disjoint.
     intros n n' [p m] [p' m'].
@@ -78,21 +77,20 @@ Section bounded_search.
   Proof.
     assert (smaller n + forall l, (l <= n) -> not (P l)) as X by apply bounded_search.
     destruct X as [[l [[Pl ml] leqln]]|none].
-    - exact (l;(Pl,tr ml)).
+    - exact (l;(tr Pl,tr ml)).
     - destruct (none n (leq_refl n) Pn).
   Defined.
 
   Local Definition prop_n_to_min_n : min_n_Type.
   Proof.
     refine (Trunc_rec _ P_inhab).
-    - exact ishpropmin_n.
-    - destruct 1 as [n Pn]. exact (n_to_min_n n Pn).
+    intros [n Pn]. exact (n_to_min_n n Pn).
   Defined.
 
   Definition minimal_n : { n : nat & P n }.
   Proof.
     destruct prop_n_to_min_n as [n pl]. destruct pl as [p _].
-    exact (n;p).
+    exact (n; fst merely_inhabited_iff_inhabited_decidable p).
   Defined.
 
 End bounded_search.
@@ -102,7 +100,6 @@ Section bounded_search_alt_type.
   Context (X : Type)
           (e : nat <~> X)
           (P : X -> Type)
-          {P_hprop : forall x, IsHProp (P x)}
           (P_dec : forall x, Decidable (P x))
           (P_inhab : hexists (fun x => P x)).
 
@@ -110,7 +107,6 @@ Section bounded_search_alt_type.
   Definition minimal_n_alt_type : {x : X & P x}.
   Proof.
     set (P' n := P (e n)).
-    assert (P'_hprop : forall n, IsHProp (P' n)) by apply _.
     assert (P'_dec : forall n, Decidable (P' n)) by apply _.
     assert (P'_inhab : hexists (fun n => P' n)).
     {
