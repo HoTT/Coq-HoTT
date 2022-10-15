128e2f387d448483132d05d0d501e8362b007cb9
Patrick Nicodemus <gadget142@gmail.com>
2022-10-15 03:36:31 -0400

    Update Decidable.v

--- theories/Basics/Decidable.v
+++ theories/Basics/Decidable.v
@@ -46,6 +46,11 @@ Proof.
   - apply Empty_rect,dn,n.
 Qed.
 
+Global Instance stable_negation P : Stable (~ P).
+Proof.
+  intros nnnp p; contradiction nnnp; intro np; contradiction np; exact p.
+Defined.
+
 (**
   Because [vm_compute] evaluates terms in [PROP] eagerly
   and does not remove dead code we
