a90d539a274eb6a014430bf725ebf2a6217378a1
Dan Christensen <jdc@uwo.ca>
2023-01-07 12:09:06 -0500

    Don't Require Export AbSES in AbGroups.v

--- theories/Algebra/AbGroups.v
+++ theories/Algebra/AbGroups.v
@@ -4,9 +4,10 @@ Require Export HoTT.Algebra.AbGroups.AbelianGroup.
 Require Export HoTT.Algebra.AbGroups.Abelianization.
 Require Export HoTT.Algebra.AbGroups.AbPullback.
 Require Export HoTT.Algebra.AbGroups.AbPushout.
-Require Export HoTT.Algebra.AbGroups.AbSES.
 Require Export HoTT.Algebra.AbGroups.Biproduct.
 Require Export HoTT.Algebra.AbGroups.AbHom.
+
+(* We do not export AbGroups.AbSES here, to allow the build to be more parallel. *)
 
 (** Examples *)
 
