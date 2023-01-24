23b9b0213f94ae26c1d57f0b6a1b02b3e3418e86
Jarl G. Taxeraas Flaten <jtaxers@uwo.ca>
2023-01-24 12:59:02 -0500

    pointed functoriality of a RSU

--- theories/Pointed/pModality.v
+++ theories/Pointed/pModality.v
@@ -19,3 +19,31 @@ Definition pequiv_pto `{O : ReflectiveSubuniverse} {X : pType} `{In O X}
 Definition pO_rec `{O : ReflectiveSubuniverse} {X Y : pType}
   `{In O Y} (f : X ->* Y) : [O X, _] ->* Y
   := Build_pMap [O X, _] _ (O_rec f) (O_rec_beta _ _ @ point_eq f).
+
+(** ** Pointed functoriality *)
+
+Definition O_pfunctor `(O : ReflectiveSubuniverse) {X Y : pType}
+  (f : X ->* Y) : [O X, _] ->* [O Y, _].
+Proof.
+  snrapply Build_pMap.
+  1: exact (O_functor O f).
+  refine (O_rec_beta _ _ @ _).
+  exact (ap (to O Y) (point_eq f)).
+Defined.
+
+(** Coq knows that [O_pfunctor O f] is an equivalence whenever [f] is. *)
+Definition equiv_O_pfunctor `(O : ReflectiveSubuniverse) {X Y : pType}
+  (f : X ->* Y) `{IsEquiv _ _ f} : [O X, _] <~>* [O Y, _]
+  := Build_pEquiv _ _ (O_pfunctor O f) _.
+
+(** Pointed naturality of [O_pfunctor]. *)
+Definition pto_O_natural `(O : ReflectiveSubuniverse) {X Y : pType}
+  (f : X ->* Y) : O_pfunctor O f o* pto O X ==* pto O Y o* f.
+Proof.
+  srapply Build_pHomotopy.
+  1: apply to_O_natural.
+  cbn. unfold to_O_natural.
+  apply moveL_pV.
+  refine (whiskerL _ _ @ (concat_1p _)^).
+  apply concat_p1.
+Defined.
