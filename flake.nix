91347876f84cad65945687e3b65c6a5fd4444fc4
Ali Caglayan <alizter@gmail.com>
2023-03-04 01:31:02 +0100

    Add nix flake
    If you have Nix, you can do `nix develop` to quickly get the
    dependencies for building the library. You can also build the library
    directly with `nix build`.
    
    - Add Nix jobs to CI. Uses cachix so should be fast.
    - Update checkout@v2 to checkout@v3
    - Update download-artifact@v2 and upload-artifact@v2 to @v3
    
    <!-- ps-id: 3a337c23-b2ea-45f8-a86a-18841f1f2210 -->
    
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A Coq library for Homotopy Type Theory";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        coq_version = pkgs.coq_8_17;
+      in
+      {
+        packages.default = pkgs.coqPackages.mkCoqDerivation {
+          pname = "hott";
+          version = "8.17";
+          src = self;
+          useDune = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            dune_3
+            ocaml
+            coq
+          ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
