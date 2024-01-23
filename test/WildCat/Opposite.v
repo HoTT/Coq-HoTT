053bd90702ac31f132f939413f40cfa1f88aecc5
Ali Caglayan <alizter@gmail.com>
2024-01-23 17:18:36 +0000

    add op graph involutive test
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- test/WildCat/Opposite.v
+++ test/WildCat/Opposite.v
@@ -2,9 +2,10 @@ From HoTT Require Import Basics WildCat.Core WildCat.Opposite.
 
 (* Opposites are (almost) definitionally involutive. *)
 
-Definition test1 A `{Is01Cat A} : A = (A^op)^op := 1.
-Definition test2 A `{x : Is01Cat A} : x = is01cat_op (A := A^op) := 1.
-Definition test3 A `{x : Is2Graph A} : x = is2graph_op (A := A^op) := 1.
+Definition test1 A : A = (A^op)^op := 1.
+Definition test2 A `{x : IsGraph A} : x = isgraph_op (A := A^op) := 1.
+Definition test3 A `{x : Is01Cat A} : x = is01cat_op (A := A^op) := 1.
+Definition test4 A `{x : Is2Graph A} : x = is2graph_op (A := A^op) := 1.
 
 (** Is1Cat is not definitionally involutive. *)
 Fail Definition test4 A `{x : Is1Cat A} : x = is1cat_op (A := A^op) := 1.
