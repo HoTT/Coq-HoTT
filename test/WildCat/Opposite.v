4c4cec3b51c998b201cd336b7418d05c18e7b442
Ali Caglayan <alizter@gmail.com>
2024-01-24 12:08:20 +0000

    Update test/WildCat/Opposite.v

--- test/WildCat/Opposite.v
+++ test/WildCat/Opposite.v
@@ -2,7 +2,7 @@ From HoTT Require Import Basics WildCat.Core WildCat.Opposite.
 
 (* Opposites are (almost) definitionally involutive. *)
 
-Definition test1 A : A = (A^op)^op := 1.
+Definition test1 A : A = (A^op)^op :> Type := 1.
 Definition test2 A `{x : IsGraph A} : x = isgraph_op (A := A^op) := 1.
 Definition test3 A `{x : Is01Cat A} : x = is01cat_op (A := A^op) := 1.
 Definition test4 A `{x : Is2Graph A} : x = is2graph_op (A := A^op) := 1.
