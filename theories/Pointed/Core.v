b2e4925c1ae5c70b5fc6957d44ad185089864090
Dan Christensen <jdc@uwo.ca>
2024-02-05 11:12:51 -0500

    Pointed/Core: fix pelim so pEquiv.v builds

--- theories/Pointed/Core.v
+++ theories/Pointed/Core.v
@@ -204,7 +204,7 @@ Ltac pointed_reduce_pmap f
 (** A general tactic to replace pointedness paths in a pForall with reflexivity.  Because it generalizes [f pt], it can usually only be applied once the function itself is not longer needed.  Compared to [pointed_reduce], an advantage is that the pointed types do not need to be destructed. *)
 Ltac pelim f :=
   try match type of f with
-    | pEquiv ?X ?Y => destruct f as [f ?iseq]
+    | pEquiv ?X ?Y => destruct f as [f ?iseq]; unfold pointed_fun in *
   end;
   destruct f as [f ?ptd];
   cbn in f, ptd |- *;
