90a1908cf196bd127fb8c679272ab79794c0b176
Ali Caglayan <alizter@gmail.com>
2024-02-11 23:46:59 +0000

    new better proof for PushoutFlattening avoiding coh result
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- theories/Colimits/PushoutFlattening.v
+++ theories/Colimits/PushoutFlattening.v
@@ -18,81 +18,6 @@ Section Flattening.
     exact _.
   Defined.
 
-  Lemma flattening_coh
-    : forall x, pushout_flattening_fam x <~> POCase_P A0 B0 C0 f0 g0 (equiv_pushout_PO x).
-  Proof.
-    (** Proving that the type families are equivalent takes some work but is mostly trivial. We don't know how to transport over families of equivalences yet, so we use univalence to turn it into a transport over a family of paths. *)
-    snrapply Pushout_ind.
-    1,2: hnf; reflexivity.
-    intros a.
-    hnf.
-    lhs nrapply transport_idmap_ap.
-    unshelve erewrite ap_homotopic.
-    2: {
-      intros w.
-      rapply path_universe_uncurried.
-      apply equiv_path_universe. }
-    rewrite 2 transport_pp.
-    rewrite transport_path_universe_uncurried.
-    rewrite transport_path_universe_V.
-    apply moveR_equiv_V.
-    change (?x = path_universe_uncurried 1) with (x = path_universe equiv_idmap).
-    rewrite path_universe_1.
-    lhs nrapply (transport_idmap_ap _ _ _)^.
-    rewrite transport_paths_FlFr.
-    rewrite Pushout_rec_beta_pglue.
-    rewrite concat_pp_p.
-    apply moveR_Vp.
-    rewrite concat_p1.
-    rewrite ap_compose.
-    rewrite Pushout_rec_beta_pglue.
-    unfold popp'.
-    simpl.
-    rewrite 2 concat_1p.
-    change(path_universe 1%equiv @
-      ap (Colimit_Pushout_Flattening.POCase_P A0 B0 C0 f0 g0)
-      (popp a) = path_universe (g0 a oE (f0 a)^-1)).
-    rewrite PO_rec_beta_pp.
-    rewrite path_universe_1.
-    rewrite concat_1p.
-    reflexivity.
-  Defined.
-
-  Lemma flattening_coh_2
-    : forall x, pushout_flattening_fam x <~> POCase_P A0 B0 C0 f0 g0 (equiv_pushout_PO x).
-  Proof.
-    intros x.
-    snrapply Build_Equiv.
-    - revert x.
-      snrapply Pushout_ind.
-      1,2: intros x; exact idmap.
-      intros x.
-      rapply dpath_arrow.
-      intros y.
-      lhs nrapply transport_compose.
-      lhs nrapply transport2.
-      1: apply Pushout_rec_beta_pglue.
-      unfold popp'.
-      simpl.
-      rewrite 2 concat_1p.
-      lhs nrapply (transport_compose idmap (POCase_P A0 B0 C0 f0 g0) (popp x)).
-      lhs nrapply ap011.
-      1: reflexivity.
-      1: nrapply PO_rec_beta_pp.
-      rhs nrapply (transport_idmap_ap _ (pglue x) _).
-      lhs nrapply transport_path_universe_uncurried.
-      rhs nrapply ap011.
-      2: reflexivity.
-      2: apply Pushout_rec_beta_pglue.
-      rhs nrapply transport_path_universe_uncurried.
-      reflexivity.
-    - revert x.
-      snrapply Pushout_ind.
-      1,2: intro; exact (isequiv_idmap _).
-      intros a.
-      apply path_ishprop.
-  Defined.
-
   Definition pushout_flattening
     : Pushout (functor_sigma f f0) (functor_sigma g g0)
       <~> exists x, pushout_flattening_fam x.
@@ -102,7 +27,9 @@ Section Flattening.
     symmetry.
     snrapply equiv_functor_sigma'.
     1: apply equiv_pushout_PO.
-    apply flattening_coh_2.
+    intro x.
+    apply equiv_path.
+    nrapply Pushout_rec_PO_rec.
   Defined.
 
 End Flattening.
