62b8d2b4ebbee7a758065b87c6052a4354b84ed9
gio256 <102917377+gio256@users.noreply.github.com>
2024-03-08 12:02:47 -0700

    AbGroups/AbHom.v: prove is1bifunctor_ab_hom

--- theories/Algebra/AbGroups/AbHom.v
+++ theories/Algebra/AbGroups/AbHom.v
@@ -63,10 +63,44 @@ Proof.
   by apply equiv_path_grouphomomorphism.
 Defined.
 
+Global Instance is1functor_ab_hom01 `{Funext} {A : Group^op}
+  : Is1Functor (ab_hom A).
+Proof.
+  nrapply Build_Is1Functor.
+  - intros B B' f g p phi.
+    apply equiv_path_grouphomomorphism; intro a; cbn.
+    exact (p (phi a)).
+  - intros B phi.
+    by apply equiv_path_grouphomomorphism.
+  - intros B C D f g phi.
+    by apply equiv_path_grouphomomorphism.
+Defined.
+
+Global Instance is1functor_ab_hom10 `{Funext} {B : AbGroup@{u}}
+  : Is1Functor (flip (ab_hom : Group^op -> AbGroup -> AbGroup) B).
+Proof.
+  nrapply Build_Is1Functor.
+  - intros A A' f g p phi.
+    apply equiv_path_grouphomomorphism; intro a; cbn.
+    exact (ap phi (p a)).
+  - intros A phi.
+    by apply equiv_path_grouphomomorphism.
+  - intros A C D f g phi.
+    by apply equiv_path_grouphomomorphism.
+Defined.
+
 Global Instance is0bifunctor_ab_hom `{Funext}
   : Is0Bifunctor (ab_hom : Group^op -> AbGroup -> AbGroup).
 Proof.
   rapply Build_Is0Bifunctor.
+Defined.
+
+Global Instance is1bifunctor_ab_hom `{Funext}
+  : Is1Bifunctor (ab_hom : Group^op -> AbGroup -> AbGroup).
+Proof.
+  rapply Build_Is1Bifunctor.
+  intros A A' f B B' g phi; cbn.
+  by apply equiv_path_grouphomomorphism.
 Defined.
 
 (** ** Properties of [ab_hom] *)
