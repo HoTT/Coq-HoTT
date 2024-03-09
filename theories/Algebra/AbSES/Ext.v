2c9fce63b21da1300b967be1a893eeb24e3aeb88
gio256 <102917377+gio256@users.noreply.github.com>
2024-03-09 11:20:42 -0700

    AbSES/Ext.v: remove universe annotations

--- theories/Algebra/AbSES/Ext.v
+++ theories/Algebra/AbSES/Ext.v
@@ -33,12 +33,11 @@ Defined.
 Definition Ext' (B A : AbGroup@{u}) := Tr 0 (AbSES' B A).
 
 Global Instance is0bifunctor_ext'@{u v w | u < v, v < w} `{Univalence}
-  : Is0Bifunctor@{v v w u u v} (Ext' : AbGroup@{u}^op -> AbGroup@{u} -> Type@{v})
+  : Is0Bifunctor (Ext' : AbGroup@{u}^op -> AbGroup@{u} -> Type@{v})
   := is0bifunctor_compose _ _ (bf:=is0bifunctor_abses').
 
-(**TODO: universes*)
 Global Instance is1bifunctor_ext'@{u v w | u < v, v < w} `{Univalence}
-  : Is1Bifunctor@{v v w u u v v u u} (Ext' : AbGroup@{u}^op -> AbGroup@{u} -> Type@{v})
+  : Is1Bifunctor (Ext' : AbGroup@{u}^op -> AbGroup@{u} -> Type@{v})
   := is1bifunctor_compose _ _ (bf:=is1bifunctor_abses').
 
 (** [Ext B A] is an abelian group for any [A B : AbGroup]. The proof of commutativity is a bit faster if we separate out the proof that [Ext B A] is a group. *)
