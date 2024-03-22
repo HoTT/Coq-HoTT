5aff671021e18da1b053a4ddc8d2b8d8b6e95809
MintChocoManju <mnln2468@gmail.com>
2024-03-23 01:03:50 +0800

    HoTTBookExercises: Formatting

--- contrib/HoTTBookExercises.v
+++ contrib/HoTTBookExercises.v
@@ -469,8 +469,7 @@ End Book_1_13.
 (* ================================================== ex:without-K *)
 (** Exercise 1.14 *)
 
-(* There is no adequate type family C : Pi_{x, y, p} U  such that C(x, x, p)
-  is p = refl x definitionally *)
+(* There is no adequate type family C : Pi_{x, y, p} U  such that C(x, x, p) is p = refl x definitionally *)
 
 (* ================================================== ex:subtFromPathInd *)
 (** Exercise 1.15 *)
