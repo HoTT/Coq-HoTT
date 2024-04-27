78e7646a08bbc5ea3552ad2a022db647fb402341
Dan Christensen <jdc@uwo.ca>
2024-04-27 10:31:47 -0400

    test/WildCat/Opposite: clarify that the _op functions are used twice

--- test/WildCat/Opposite.v
+++ test/WildCat/Opposite.v
@@ -1,11 +1,11 @@
 From HoTT Require Import Basics WildCat.Core WildCat.Opposite.
 
-(* Opposites are (almost) definitionally involutive. *)
+(** * Opposites are (almost) definitionally involutive. *)
 
 Definition test1 A : A = (A^op)^op :> Type := 1.
-Definition test2 A `{x : IsGraph A} : x = isgraph_op (A := A^op) := 1.
-Definition test3 A `{x : Is01Cat A} : x = is01cat_op (A := A^op) := 1.
-Definition test4 A `{x : Is2Graph A} : x = is2graph_op (A := A^op) := 1.
+Definition test2 A `{x : IsGraph A} : x = @isgraph_op A^op (@isgraph_op A x) := 1.
+Definition test3 A `{x : Is01Cat A} : x = @is01cat_op A^op _ (@is01cat_op A _ x) := 1.
+Definition test4 A `{x : Is2Graph A} : x = @is2graph_op A^op _ (@is2graph_op A _ x) := 1.
 
-(** Is1Cat is not definitionally involutive. *)
-Fail Definition test4 A `{x : Is1Cat A} : x = is1cat_op (A := A^op) := 1.
+(** [Is1Cat] is not definitionally involutive. *)
+Fail Definition test5 A `{x : Is1Cat A} : x = @is1cat_op A^op _ _ _ (@is1cat_op A _ _ _ x) := 1.
