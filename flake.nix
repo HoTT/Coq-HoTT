8e4955ca8402fb897438eac10af55315896b3f71
Ali Caglayan <alizter@gmail.com>
2024-04-28 03:57:40 +0100

    nix: add 8.18 dev shell
    Signed-off-by: Ali Caglayan <alizter@gmail.com>
    
    <!-- ps-id: f8f7d4dc-beb4-4cf0-8250-b10f80d03517 -->

--- flake.nix
+++ flake.nix
@@ -7,17 +7,20 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+        makeDevShell = coqVersion:
+          pkgs.mkShell {
+            buildInputs = with pkgs."coqPackages_${coqVersion}"; [
+              pkgs.dune_3
+              pkgs.ocaml
+              coq
+              coq-lsp
+            ];
+          };
+      in {
         packages.default = pkgs.coqPackages.mkCoqDerivation {
           pname = "hott";
           version = "8.18";
@@ -25,16 +28,9 @@
           useDune = true;
         };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs.coqPackages_8_19; [
-            pkgs.dune_3
-            pkgs.ocaml
-            coq
-            coq-lsp
-          ];
-        };
+        devShells.default = makeDevShell "8_19";
+        devShells.coq_8_18 = makeDevShell "8_18";
 
         formatter = pkgs.nixpkgs-fmt;
-      }
-    );
+      });
 }
