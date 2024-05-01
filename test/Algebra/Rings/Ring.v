42f43aa1b31337ba987d960e5f3f4c9c847697b1
Ali Caglayan <alizter@gmail.com>
2024-05-01 17:00:12 +0100

    update comment in test
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- test/Algebra/Rings/Ring.v
+++ test/Algebra/Rings/Ring.v
@@ -6,16 +6,6 @@ Local Open Scope path_scope.
 
 (** Test that opposite rings are definitionally involutive. *)
 Definition test1 (R : Ring) : R = (rng_op (rng_op R)) :> Ring := 1.
+
+(** This also works because `:>` coercions are by default reversible. *)
 Definition test2 (R : CRing) : R = (rng_op (rng_op R)) :> CRing := 1.
-
-Axiom fake_comm : forall (R : CRing) (x y : R), x * y = y * x.
-Definition augment (R : CRing) : CRing.
-Proof.
-  snrapply Build_CRing.
-  - exact R.
-  - exact (fake_comm R).
-Defined.
-
-Fail Definition test3 (R : CRing) : R = augment R :> CRing := 1.
-(* !? *)
-Definition test4 (R : CRing) : R = (rng_op (rng_op (augment R))) :> CRing := 1.
