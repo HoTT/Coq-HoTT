01b4b96f827f38d624aa5635bbad42c5604debbb
Ali Caglayan <alizter@gmail.com>
2024-06-10 12:59:01 +0200

    remove unnecessary universe annotations in List.Core.v
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- theories/Spaces/List/Core.v
+++ theories/Spaces/List/Core.v
@@ -39,7 +39,7 @@ Notation "[ x , y , .. , z ]" := (x :: (y :: .. (z :: nil) ..)) : list_scope.
 (** Notice that the definition of a list looks very similar to the definition of [nat]. It is as if each [S] constructor from [nat] has an element of [A] attached to it. We can discard this extra element and get a list invariant that we call [length]. *)
 
 (** The length (number of elements) of a list. *)
-Fixpoint length@{i|} {A : Type@{i}} (l : list@{i} A) :=
+Fixpoint length {A : Type} (l : list A) :=
   match l with
   | nil => O
   | _ :: l => S (length l)
@@ -48,7 +48,7 @@ Fixpoint length@{i|} {A : Type@{i}} (l : list@{i} A) :=
 (** ** Concatenation *)
 
 (** Given two lists [ [a1; a2; ...; an] ] and [ [b1; b2; ...; bm] ], we can concatenate them to get [ [a1; a2; ...; an; b1; b2; ...; bm] ]. *)
-Definition app@{i|} {A : Type@{i}} : list A -> list A -> list A :=
+Definition app {A : Type} : list A -> list A -> list A :=
   fix app l m :=
   match l with
    | nil => m
@@ -62,16 +62,14 @@ Infix "++" := app : list_scope.
 (** Folding is a very important operation on lists. It is a way to reduce a list to a single value. The [fold_left] function starts from the left and the [fold_right] function starts from the right. *)
 
 (** [fold_left f l a0] computes [f (... (f (f a0 x1) x2) ...) xn] where [l = [x1; x2; ...; xn]]. *)
-Fixpoint fold_left@{i j|} {A : Type@{i}} {B : Type@{j}}
-  (f : A -> B -> A) (l : list B) (a0 : A) : A :=
+Fixpoint fold_left {A B} (f : A -> B -> A) (l : list B) (default : A) : A :=
   match l with
-    | nil => a0
-    | cons b l => fold_left f l (f a0 b)
+    | nil => default
+    | cons b l => fold_left f l (f default b)
   end.
 
 (** [fold_right f a0 l] computes [f x1 (f x2 ... (f xn a0) ...)] where [l = [x1; x2; ...; xn]]. *)
-Fixpoint fold_right@{i j|} {A : Type@{i}} {B : Type@{j}}
-  (f : B -> A -> A) (default : A) (l : list B) : A :=
+Fixpoint fold_right {A B} (f : B -> A -> A) (default : A) (l : list B) : A :=
   match l with
     | nil => default
     | cons b l => f b (fold_right f default l)
@@ -80,16 +78,15 @@ Fixpoint fold_right@{i j|} {A : Type@{i}} {B : Type@{j}}
 (** ** Maps - Functoriality of Lists *)
 
 (** The [list_map] function applies a function to each element of a list. In other words [ list_map f [a1; a2; ...; an] = [f a1; f a2; ...; f an] ]. *)
-Fixpoint list_map@{i j|} {A : Type@{i}} {B : Type@{j}} (f : A -> B) (l : list A) :=
+Fixpoint list_map {A B : Type} (f : A -> B) (l : list A) :=
   match l with
   | nil => nil
   | x :: l => (f x) :: (list_map f l)
   end.
 
 (** The [list_map2] function applies a binary function to corresponding elements of two lists. When one of the lists run out, it uses one of the default functions to fill in the rest. *)
-Fixpoint list_map2@{i j k|} {A : Type@{i}} {B : Type@{j}} {C : Type@{k}}
-  (f : A -> B -> C) (def_l : list A -> list C) (def_r : list B -> list C)
-  l1 l2 :=
+Fixpoint list_map2 {A B C : Type} (f : A -> B -> C)
+  (def_l : list A -> list C) (def_r : list B -> list C) l1 l2 :=
   match l1, l2 with
   | nil, nil => nil
   | nil, _ => def_r l2
@@ -100,33 +97,33 @@ Fixpoint list_map2@{i j k|} {A : Type@{i}} {B : Type@{j}} {C : Type@{k}}
 (** ** Reversal *)
 
 (** Tail-recursive list reversal. *)
-Fixpoint reverse_acc@{i|} {A : Type@{i}} (acc : list A) (l : list A) : list A :=
+Fixpoint reverse_acc {A : Type} (acc : list A) (l : list A) : list A :=
   match l with
   | nil => acc
   | x :: l => reverse_acc (x :: acc) l
   end.
 
 (** Reversing the order of a list. The list [ [a1; a2; ...; an] ] becomes [ [an; ...; a2; a1] ]. *)
-Definition reverse@{i|} {A : Type@{i}} (l : list A) : list A := reverse_acc nil l.
+Definition reverse {A : Type} (l : list A) : list A := reverse_acc nil l.
 
 (** ** Getting Elements *)
 
 (** The head of a list is its first element. Returns [None] If the list is empty. *)
-Definition head@{i|} {A : Type@{i}} (l : list A) : option A :=
+Definition head {A : Type} (l : list A) : option A :=
   match l with
   | nil => None
   | a :: _ => Some a
   end.
 
 (** The tail of a list is the list without its first element. *)
-Definition tail@{i|} {A : Type@{i}} (l : list A) : list A :=
+Definition tail {A : Type} (l : list A) : list A :=
   match l with
     | nil => nil
     | a :: m => m
   end.
 
 (** The last element of a list. If the list is empty, it returns [None]. *)
-Fixpoint last@{i|} {A : Type@{i}} (l : list A) : option A :=
+Fixpoint last {A : Type} (l : list A) : option A :=
   match l with
   | nil => None
   | a :: nil => Some a
@@ -134,7 +131,7 @@ Fixpoint last@{i|} {A : Type@{i}} (l : list A) : option A :=
   end.
 
 (** The [n]-th element of a list. If the list is too short, it returns [None]. *)
-Fixpoint nth@{i|} {A : Type@{i}} (l : list A) (n : nat) : option A :=
+Fixpoint nth {A : Type} (l : list A) (n : nat) : option A :=
   match n, l with
   | O, x :: _ => Some x
   | S n, _ :: l => nth l n
@@ -144,7 +141,7 @@ Fixpoint nth@{i|} {A : Type@{i}} (l : list A) (n : nat) : option A :=
 (** ** Removing Elements *)
 
 (** Remove the last element of a list and do nothing if it is empty. *)
-Fixpoint remove_last@{i|} {A : Type@{i}} (l : list A) : list A :=
+Fixpoint remove_last {A : Type} (l : list A) : list A :=
   match l with
   | nil => nil
   | _ :: nil => nil
@@ -166,7 +163,7 @@ Definition seq (n : nat) : list nat := reverse (seq_rev n).
 (** ** Repeat *)
 
 (** Repeat an element [n] times. *)
-Fixpoint repeat@{i|} {A : Type@{i}} (x : A) (n : nat) : list A :=
+Fixpoint repeat {A : Type} (x : A) (n : nat) : list A :=
   match n with
   | O => nil
   | S n => x :: repeat x n
