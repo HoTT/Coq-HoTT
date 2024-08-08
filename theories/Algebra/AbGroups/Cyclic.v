659d6fe2ae628d4aab3d68dda16efe140830cfec
D <nick.decroos@gmail.com>
2024-08-08 21:02:04 +0200

    Making n in cyclic_in explicit

--- theories/Algebra/AbGroups/Cyclic.v
+++ theories/Algebra/AbGroups/Cyclic.v
@@ -85,11 +85,11 @@ Definition cyclic `{Funext} (n : nat) : AbGroup
 Definition cyclic' (n : nat) : AbGroup
   := ab_cokernel (ab_mul (A:=abgroup_Z) n).
 
-Definition cyclic_in {n : nat} : abgroup_Z $-> cyclic' n
+Definition cyclic_in (n : nat) : abgroup_Z $-> cyclic' n
   := grp_quotient_map. 
 
 Definition ab_mul_cyclic_in (n : nat) (x y : abgroup_Z)
-  : ab_mul y (@cyclic_in n x) = cyclic_in (y * x)%int.
+  : ab_mul y (cyclic_in n x) = cyclic_in n (y * x)%int.
 Proof.
   lhs_V nrapply ab_mul_natural.
   apply ap, abgroup_Z_ab_mul.
