f9325d8df500ac74da66a90029054231313bd22d
Ali Caglayan <alizter@gmail.com>
2025-01-04 00:16:39 +0000

    capitalize cofiber -> Cofiber
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- theories/Homotopy/Cofiber.v
+++ theories/Homotopy/Cofiber.v
@@ -11,17 +11,17 @@ Set Universe Minimization ToSet.
 
 (** Homotopy cofibers or mapping cones are spaces constructed from a map [f : X -> Y] by contracting the image of [f] inside [Y] to a point. They can be thought of as a kind of coimage or quotient. *)
 
-Definition cofiber {X Y : Type} (f : X -> Y)
+Definition Cofiber {X Y : Type} (f : X -> Y)
   := Pushout (const_tt X) f.
 
 (** *** Constructors *)
 
 (** Any element of [Y] can be included in the cofiber. *)
-Definition cofib {X Y : Type} (f : X -> Y) : Y -> cofiber f
+Definition cofib {X Y : Type} (f : X -> Y) : Y -> Cofiber f
   := pushr.
 
 (** We have a distinguised point in the cofiber, where the image of [f] is contracted to. *)
-Definition apex {X Y : Type} (f : X -> Y) : cofiber f
+Definition apex {X Y : Type} (f : X -> Y) : Cofiber f
   := pushl tt.
 
 (** Given an element [x : X], we have the path [leg] from the [f x] to the [apex]. *)
@@ -33,7 +33,7 @@ Definition leg {X Y : Type} (f : X -> Y) (x : X) : cofib f (f x) = apex f
 (** The recursion principle for the cofiber of [f : X -> Y] requires a function [g : Y -> Z] such that [g o f] is null homotopic, i.e. constant. *)
 Definition cofiber_rec {X Y Z : Type} (f : X -> Y) (g : Y -> Z)
   (null : NullHomotopy (g o f))
-  : cofiber f -> Z.
+  : Cofiber f -> Z.
 Proof.
   snrapply Pushout_rec.
   - intros; exact null.1.
@@ -43,7 +43,7 @@ Proof.
 Defined.
 
 (** The induction principle is similar, although requries a dependent form of null homotopy. *)
-Definition cofiber_ind {X Y : Type} (f : X -> Y) (P : cofiber f -> Type)
+Definition cofiber_ind {X Y : Type} (f : X -> Y) (P : Cofiber f -> Type)
   (g : forall y, P (cofib f y))
   (null : exists b, forall x, transport P (leg f x) (g (f x)) = b)
   : forall x, P x.
@@ -64,7 +64,7 @@ Defined.
 (** 1-functorial action. *)
 Definition functor_cofiber {X Y X' Y' : Type} {f : X -> Y} {f' : X' -> Y'}
   (g : X -> X') (h : Y -> Y') (p : h o f == f' o g)
-  : cofiber f -> cofiber f'
+  : Cofiber f -> Cofiber f'
   := functor_pushout g idmap h (fun _ => idpath) p.
 
 (** 2-functorial action. *)
