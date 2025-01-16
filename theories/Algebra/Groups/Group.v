3cdf2c672a9e89fd9bf7d2ed5ba43dc9c43a4c95
Ali Caglayan <alizter@gmail.com>
2025-01-16 01:00:27 +0100

    style tweak in grp_prod_decompose
    Signed-off-by: Ali Caglayan <alizter@gmail.com>
    
    <!-- ps-id: 0c8a6e24-89bd-490a-8ed3-cb6c06be623e -->

--- theories/Algebra/Groups/Group.v
+++ theories/Algebra/Groups/Group.v
@@ -995,7 +995,7 @@ Defined.
 
 (** Pairs in direct products can be decomposed *)
 Definition grp_prod_decompose {G H : Group} (g : G) (h : H)
-  : (g, h) = ((g, group_unit) : grp_prod G H) * (group_unit, h).
+  : (g, h) = ((g, 1) : grp_prod G H) * (1, h).
 Proof.
   snrapply path_prod; symmetry.
   - snrapply grp_unit_r.
