6de9c5600d821aa1418856e950ea7158cb5d1537
Ali Caglayan <alizter@gmail.com>
2025-01-21 19:55:28 +0100

    rename Groups/Lagrange.v -> Groups/Finite.v and factor proofs
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- theories/Algebra/Groups/Finite.v
+++ theories/Algebra/Groups/Finite.v
@@ -6,12 +6,15 @@ Require Import Spaces.Finite.Finite.
 Require Import Spaces.Nat.Core Spaces.Nat.Division.
 Require Import Truncations.Core.
 
-(** ** Lagrange's theorem *)
-
 Local Open Scope mc_mult_scope.
 Local Open Scope nat_scope.
 
-(** TODO: move to file about finite groups *)
+Set Universe Minimization ToSet.
+
+(** * Finite Groups *)
+
+(** ** Basic Properties *)
+
 (** The order of a group is strictly positive. *)
 Global Instance lt_zero_fcard_grp (G : Group) (fin_G : Finite G) : 0 < fcard G.
 Proof.
@@ -22,27 +25,43 @@ Proof.
   - exact _.
 Defined.
 
-(** TODO: move to file about finite groups *)
-Definition subgroup_index {U : Univalence} (G : Group) (H : Subgroup G)
-  (fin_G : Finite G) (fin_H : Finite H)
+(** ** Index of a subgroup *)
+
+Definition subgroup_index@{i j | i < j} {U : Univalence}
+  (G : Group@{i}) (H : Subgroup@{i i} G)
+  {fin_G : Finite@{i i i} G} {fin_H : Finite@{i i i} H}
   : nat.
 Proof.
-  refine (fcard (Quotient (in_cosetL H))).
-  nrapply finite_quotient.
+  refine (fcard@{i i i} (Quotient@{i i i} (in_cosetL@{i i} H))).
+  (* 54 universes! :'( *) 
+  nrapply finite_quotient
+    @{i i i i i i i i i j
+      i i i i i i i i i i
+      i i i i i i i i i i
+      j i i i i i i i i i
+      i i i i i i j i i i
+      i i i i}.
   1-5: exact _.
   intros x y.
-  pose (dec_H := detachable_finite_subset H).
+  (* 33 universes! :'( *) 
+  pose (dec_H := detachable_finite_subset
+    @{i i i i i i i i j i
+      i i i i i i i i i i
+      i i i i i i i i i i
+      i i i} H).
   apply dec_H.
 Defined.
+
+(** ** Lagrange's theorem *)
 
 (** Lagrange's Theorem - Given a finite group [G] and a finite subgroup [H] of [G], the order of [H] divides the order of [G].
 
 Note that constructively, a subgroup of a finite group cannot be shown to be finite without excluded middle. We therefore have to assume it is. This in turn implies that the subgroup is decidable. *)
-Definition divides_fcard_subgroup {U : Univalence} (G : Group) (H : Subgroup G)
-  (fin_G : Finite G) (fin_H : Finite H)
+Definition divides_fcard_subgroup {U : Univalence}
+  (G : Group) (H : Subgroup G) {fin_G : Finite G} {fin_H : Finite H}
   : (fcard H | fcard G).
 Proof.
-  exists (subgroup_index G H _ _).
+  exists (subgroup_index G H).
   symmetry.
   refine (fcard_quotient (in_cosetL H) @ _).
   refine (_ @ finadd_const _ _).
@@ -59,11 +78,20 @@ Proof.
   exact _.
 Defined.
 
-(** As a corollary, we can show that the order of the quotient group [G/H] is the order of [G] divided by the order of [H]. *)
-Definition fcard_grp_quotient {U : Univalence} (G : Group) (H : NormalSubgroup G)
-  (fin_G : Finite G) (fin_H : Finite H)
-  : fcard (QuotientGroup G H) = fcard G / fcard H.
+(** As a corollary, the index of a subgroup is the order of the group divided by the order of the subgroup. *)
+Definition subgroup_index_fcard_div {U : Univalence}
+  (G : Group) (H : Subgroup G) (fin_G : Finite G) (fin_H : Finite H)
+  : subgroup_index G H = fcard G / fcard H.
 Proof.
   rapply nat_div_moveL_nV.
   apply divides_fcard_subgroup.
+Defined.
+
+(** Therefore we can show that the order of the quotient group [G / H] is the order of [G] divided by the order of [H]. *)
+Definition fcard_grp_quotient {U : Univalence}
+  (G : Group) (H : NormalSubgroup G)
+  (fin_G : Finite G) (fin_H : Finite H)
+  : fcard (QuotientGroup G H) = fcard G / fcard H.
+Proof.
+  apply subgroup_index_fcard_div.
 Defined.
