4fef289284425dd803e9cb64b57c40c89912007a
Ali Caglayan <alizter@gmail.com>
2025-01-27 14:51:21 +0100

    simplify proof of Freudenthal
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- theories/Homotopy/Freudenthal.v
+++ theories/Homotopy/Freudenthal.v
@@ -1,33 +1,22 @@
-Require Import Basics.
-Require Import Types.
-Require Import Colimits.Pushout.
-Require Import Colimits.SpanPushout.
-Require Import HoTT.Truncations.
-Require Import Homotopy.Suspension.
-Require Import Homotopy.BlakersMassey.
+Require Import Basics Types Limits.Pullback Colimits.Pushout.
+Require Import Truncations.Core Truncations.Connectedness.
+Require Import Homotopy.Suspension Homotopy.BlakersMassey.
 
 (** * The Freudenthal Suspension Theorem *)
 
-(** The Freudenthal suspension theorem is a fairly trivial corollary of the Blakers-Massey theorem.  The only real work is to relate the span-pushout that we used for Blakers-Massey to the naive pushout that we used to define suspension. *)
+(** The Freudenthal suspension theorem is a fairly trivial corollary of the Blakers-Massey theorem. *)
 Local Definition freudenthal' `{Univalence} (n : trunc_index)
            (X : Type) `{IsConnected n.+1 X}
   : IsConnMap (n +2+ n) (@merid X).
 Proof.
   snrapply cancelL_equiv_conn_map.
-  2: { refine (equiv_ap' (B:=SPushout (fun (u v : Unit) => X)) _ North South).
-       exact (equiv_pushout (equiv_contr_sigma (fun _ : Unit * Unit => X))^-1
-                            (equiv_idmap Unit) (equiv_idmap Unit)
-                            (fun x : X => idpath) (fun x : X => idpath)). }
-  refine (conn_map_homotopic _ _ _ _
-                             (blakers_massey n n (fun (u v:Unit) => X) tt tt)).
-  intros x.
-  refine (_ @ (equiv_pushout_pglue
-                 (equiv_contr_sigma (fun _ : Unit * Unit => X))^-1
-                 (equiv_idmap Unit) (equiv_idmap Unit)
-                 (fun x : X => idpath) (fun x : X => idpath) x)^).
-  exact ((concat_p1 _ @ concat_1p _)^).
+  - exact (Pullback (pushl (f:=const_tt X) (g:=const_tt X)) pushr).
+  - symmetry.
+    do 2 refine (_ oE equiv_contr_sigma _).
+    reflexivity.
+  - rapply blakers_massey_po.
 Defined.
 
-Definition freudenthal@{u v | u < v} := Eval unfold freudenthal' in @freudenthal'@{u u u u u v u u u u u}.
+Definition freudenthal@{u v | u < v} := Eval unfold freudenthal' in @freudenthal'@{u u v u u u u u u}.
 
 Global Existing Instance freudenthal.
