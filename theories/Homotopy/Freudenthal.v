b7584624b34d08cadaa4cd0049ae74eec08177ba
Ali Caglayan <alizter@gmail.com>
2025-01-31 23:30:26 +0100

    remove universe annotations from freudenthal
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- theories/Homotopy/Freudenthal.v
+++ theories/Homotopy/Freudenthal.v
@@ -5,11 +5,11 @@ Require Import Homotopy.Suspension Homotopy.BlakersMassey.
 (** * The Freudenthal Suspension Theorem *)
 
 (** The Freudenthal suspension theorem is a fairly trivial corollary of the Blakers-Massey theorem.  It says that [merid : X -> North = South] is highly connected. *)
-Global Instance freudenthal@{u v | u < v} `{Univalence} (n : trunc_index)
+Global Instance freudenthal `{Univalence} (n : trunc_index)
            (X : Type@{u}) `{IsConnected n.+1 X}
   : IsConnMap (n +2+ n) (@merid X).
 Proof.
   (* If we post-compose [merid : X -> North = South] with an equivalence [North = South <~> P], where [P] is the pullback of the inclusions [Unit -> Susp X] hitting [North] and [South], we get the canonical comparison map [X -> P] whose connectivity follows from the Blakers-Massey theorem. *)
   rapply (cancelL_equiv_conn_map _ _ (equiv_pullback_unit_unit_paths _ _)^-1%equiv).
-  rapply blakers_massey_po@{u u u u v u u u u}.
+  rapply blakers_massey_po.
 Defined.
