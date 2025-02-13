4cfc3436ef135461255466e0fd7742e5357bc754
Dan Christensen <jdc@uwo.ca>
2025-02-13 13:15:21 -0500

    AbPushout: slightly speed up and clean up ab_pushout_embedding_inl

--- theories/Algebra/AbGroups/AbPushout.v
+++ theories/Algebra/AbGroups/AbPushout.v
@@ -133,17 +133,17 @@ Definition ab_pushout_embedding_inl `{Univalence} {A B C : AbGroup}
 Proof.
   apply isembedding_isinj_hset.
   intros c0 c1.
-  refine (_ o (path_ab_pushout f g (grp_prod_inl c0) (grp_prod_inl c1))^-1).
+  nrefine (_ o (path_ab_pushout f g (grp_prod_inl c0) (grp_prod_inl c1))^-1%equiv).
   rapply Trunc_ind.
   cbn; intros [a p].
-  assert (z : a = mon_unit).
+  assert (z : a = 0).
   - rapply (isinj_embedding g).
-    refine (ap snd p @ _); cbn.
-    exact (left_inverse mon_unit @ (grp_homo_unit g)^).
-  - apply (grp_moveR_M1).
-    refine (_ @ ap fst p); cbn; symmetry.
-    refine (_ @ grp_inv_unit).
-    refine (ap _ _).
+    lhs exact (ap snd p); unfold snd.
+    exact (left_inverse 0 @ (grp_homo_unit g)^).
+  - apply grp_moveR_M1.
+    rhs_V exact (ap fst p); unfold fst; symmetry.
+    rhs_V nrapply grp_inv_unit.
+    apply ap.
     exact (ap f z @ grp_homo_unit f).
 Defined.
 
