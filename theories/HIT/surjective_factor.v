88db2c5380df91fbc6370858599491dbe9d62f5e
Dan Christensen <jdc@uwo.ca>
2025-02-17 20:10:00 -0500

    surjective_factor: cleanup

--- theories/HIT/surjective_factor.v
+++ theories/HIT/surjective_factor.v
@@ -1,36 +1,32 @@
-Require Import
-        HoTT.Basics
-        HoTT.Truncations.Core Modalities.Modality.
+Require Import Basics Types.Sigma Truncations.Core Modalities.Modality.
 
-(** Definition by factoring through a surjection. *)
+(** * Definition of a map by factoring through a surjection. *)
 
 Section surjective_factor.
-  Context `{Funext}.
+  Context `{Funext}. (* Can [Funext] be avoided? *)
   Context {A B C} `{IsHSet C} `(f : A -> C) `(g : A -> B) {Esurj : IsSurjection g}.
   Variable (Eg : forall x y, g x = g y -> f x = f y).
 
-  Lemma ishprop_surjective_factor_aux : forall b, IsHProp (exists c : C, forall a, g a = b -> f a = c).
+  (** We first define a (dependent) function from [B] to a type that we'll show is a proposition. *)
+  Definition surjective_factor_aux : forall b : B, {c : C & forall a : A, b = g a -> c = f a}.
   Proof.
-    intros. apply Sigma.ishprop_sigma_disjoint.
-    intros c1 c2 E1 E2.
-    generalize (@center _ (Esurj b)); apply (Trunc_ind _).
-    intros [a p];destruct p.
-    path_via (f a).
-  Qed.
-
-  Definition surjective_factor_aux :=
-    @conn_map_elim
-      _ _ _ _ Esurj (fun b => exists c : C, forall a, g a = b -> f a = c)
-      ishprop_surjective_factor_aux
-      (fun a => exist (fun c => forall a, _ -> _ = c) (f a) (fun a' => Eg a' a)).
+    apply (conn_map_elim _ g).
+    - intro b. apply ishprop_sigma_disjoint.
+      intros c1 c2.
+      rapply (conn_map_elim _ g _ _ b).
+      intros a E1 E2.
+      path_via (f a).
+    - intro a.
+      exact (f a; Eg a).
+  Defined.
 
   Definition surjective_factor : B -> C
     := fun b => (surjective_factor_aux b).1.
 
-  Lemma surjective_factor_pr : f == compose surjective_factor g.
+  Lemma surjective_factor_pr : surjective_factor o g == f.
   Proof.
     intros a.
-    apply (surjective_factor_aux (g a)).2. trivial.
-  Qed.
+    by apply (surjective_factor_aux (g a)).2.
+  Defined.
 
 End surjective_factor.
