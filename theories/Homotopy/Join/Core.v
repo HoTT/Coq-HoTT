150abdf2c9a89e69ee5e38b7c511f091c5760fb2
Dan Christensen <jdc@uwo.ca>
2025-02-22 08:25:49 -0500

    Join/Core: cleaner fix

--- theories/Homotopy/Join/Core.v
+++ theories/Homotopy/Join/Core.v
@@ -131,7 +131,8 @@ Section Join.
     (P_B : B -> P) (P_g : forall a b, P_A a = P_B b) a b
     : ap (Join_rec P_A P_B P_g) (jglue a b) = P_g a b.
   Proof.
-    exact (Pushout_rec_beta_pglue _ _ _ (fun ab => P_g (fst ab) (snd ab)) _).
+    unfold Join_rec.
+    snrapply Pushout_rec_beta_pglue.
   Defined.
 
   (** If [A] is ipointed, so is [Join A B]. *)
