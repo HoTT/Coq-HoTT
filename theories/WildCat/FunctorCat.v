b0eaa6981daed12ab7d22a6b1d7376611c2b8c56
Patrick Nicodemus <nicodemus@math.wisc.edu>
2025-02-22 00:07:11 -0500

    removed functorCat

--- theories/WildCat/FunctorCat.v
+++
@@ -1,9 +0,0 @@
-Require Import Basics.Overture Basics.Tactics.
-Require Import WildCat.Core.
-Require Import WildCat.Opposite.
-Require Import WildCat.Equiv.
-Require Import WildCat.Induced.
-Require Import WildCat.NatTrans.
-
-(** * Wild functor categories *)
-
