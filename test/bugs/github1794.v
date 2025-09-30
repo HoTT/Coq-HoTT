4e8c11dd25d31c634e2ea3562d84965d699d62c4
nicolas tabareau <nicolas.tabareau@gmail.com>
2025-09-30 11:14:53 +0200

    Add overlay

--- test/bugs/github1794.v
+++ test/bugs/github1794.v
@@ -1,6 +1,9 @@
 From HoTT Require Import Basics.Overture.
 
-(** When [rewrite] is first used, it defines helper lemmas.  If the first use is in a Section that has universe variables, then these lemmas get unnecessary universe variables.  Overture uses [rewrite] outside of a section to ensure that they have the expected number of universe variables, and we test that here. *)
 
+(** After PR#21098, helper lemmas are not generated as rewrite now relies on typeclass instances *)
+
+(*
 Check internal_paths_rew@{u1 u2}.
 Check internal_paths_rew_r@{u1 u2}.
+*)
