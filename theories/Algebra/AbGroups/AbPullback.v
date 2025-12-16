f79ca4715b426780db28f9df70ca2ccdcb1a7e7d
Dan Christensen <jdc@uwo.ca>
2025-12-16 10:12:24 -0500

    AbPullback: prove Is(Ab)EpiStable AbGroup

--- theories/Algebra/AbGroups/AbPullback.v
+++ theories/Algebra/AbGroups/AbPullback.v
@@ -1,8 +1,9 @@
-From HoTT Require Import Basics.
+From HoTT Require Import Basics Types.Universe.
+Require Import Truncations.Core HIT.epi Modalities.ReflectiveSubuniverse.
 Require Import Limits.Pullback Cubical.PathSquare.
 Require Export Algebra.Groups.GrpPullback.
-Require Import Algebra.AbGroups.AbelianGroup.
-Require Import WildCat.Core WildCat.Pullbacks.
+Require Import AbelianGroup AbHom.
+Require Import WildCat.Core WildCat.Pullbacks WildCat.EpiStable.
 
 Local Open Scope mc_add_scope.
 
@@ -29,10 +30,28 @@ Section AbPullback.
 
 End AbPullback.
 
-Definition haspullbacks_abgroup : HasPullbacks AbGroup.
+Instance haspullbacks_abgroup : HasPullbacks AbGroup.
 Proof.
   rapply (haspullbacks_induced abgroup_group).
   intros A B C f g.
   exists (ab_pullback f g).
   reflexivity.
 Defined.
+
+Instance isepistable_abgroup `{Univalence} : IsEpiStable AbGroup.
+Proof.
+  srapply Build_IsEpiStable.
+  1: exact _. (* By the above, [AbGroup] has pullbacks. *)
+  (* We have to show that the categorical epimorphisms are stable under pullback. *)
+  intros A B C f g ef.
+  intros Z k1 k2.
+  (* Since surjections are epimorphisms in [Type], it's enough to show that the pulled back map is a surjection. *)
+  rapply issurj_isepi_funext.
+  (* Since surjections are stable under pullback, it's enough to show that [f] is a surjection. *)
+  apply issurj_pullback_pr2.
+  (* This follows from the fact the epimorphisms of abelian groups are surjections. *)
+  by apply ab_issurj_epi.
+Defined.
+
+(** Combining [isepistable_abgroup] with [abenriched_abgroup] from AbHom.v gives: *)
+Instance isabepistable_abgroup `{Univalence} : IsAbEpiStable AbGroup := {}.
