ce8a6b03f4fc90f78ed36c49e8a92642a942c1f4
Dan Christensen <jdc@uwo.ca>
2025-12-21 17:34:21 -0500

    Regular.v: categories in which one can do diagram chasing
    Co-authored-by: Thomas Wilskow Thorbjørnsen <thomas.w.thorbjornsen@gmail.com>

--- theories/WildCat.v
+++ theories/WildCat.v
@@ -19,6 +19,7 @@ Require Export WildCat.Displayed.
 Require Export WildCat.DisplayedEquiv.
 Require Export WildCat.Pullbacks.
 Require Export WildCat.AbEnriched.
+Require Export WildCat.Regular.
 
 Require Export WildCat.SetoidRewrite.
 
