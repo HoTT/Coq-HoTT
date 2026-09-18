5b28483e8192da12a5539dae1bb1849e528b4493
Dan Christensen <jdc@uwo.ca>
2026-09-18 12:00:55 -0400

    Loops: add isconnmap_isconnmap_fmap_loops; simplify nearby results

--- theories/Pointed/Loops.v
+++ theories/Pointed/Loops.v
@@ -143,37 +143,57 @@ Proof.
   apply equiv_moveR_pM.
 Defined.
 
+(** The fiber of [ap f] is equivalent to a fiber of [fmap loops f]. *)
+Definition hfiber_ap_loops {A B : pType} (f : A ->* B) (p : f pt = f pt)
+  : {q : loops A & fmap loops f q = (point_eq f)^ @ (p @ point_eq f)}
+    <~> hfiber (ap (x:=pt) (y:=pt) f) p.
+Proof.
+  apply equiv_functor_sigma_id; intros q; cbn.
+  exact (equiv_cancelR _ _ _ oE equiv_cancelL _ _ _).
+Defined.
+
 (** The loop space functor decreases the truncation level by one.  *)
-Instance istrunc_fmap_loops {n} (A B : pType) (f : A ->* B)
+Instance istrunc_fmap_loops {n} {A B : pType} (f : A ->* B)
   `{IsTruncMap n.+1 _ _ f} : IsTruncMap n (fmap loops f).
 Proof.
   intro p. exact (istrunc_equiv_istrunc _ (hfiber_fmap_loops f p)).
 Defined.
 
 (** And likewise the connectedness.  *)
-Instance conn_map_fmap_loops `{Univalence} {n : trunc_index}
-  (A B : pType) (f : A ->* B) `{IsConnMap n.+1 _ _ f}
+Instance isconnmap_fmap_loops `{Univalence} {n : trunc_index}
+  {A B : pType} (f : A ->* B) `{IsConnMap n.+1 _ _ f}
   : IsConnMap n (fmap loops f).
 Proof.
   intros p; eapply isconnected_equiv'.
-  - refine (hfiber_fmap_loops f p oE _).
-    symmetry; apply hfiber_ap.
+  - refine (hfiber_fmap_loops f p).
   - exact _.
 Defined.
 
-Definition conn_map_iterated_fmap_loops `{Univalence}
-  (n : trunc_index) (k : nat) (A B : pType) (f : A ->* B)
+Definition isconnmap_iterated_fmap_loops `{Univalence}
+  (n : trunc_index) (k : nat) {A B : pType} (f : A ->* B)
   (C : IsConnMap (trunc_index_inc' n k) f)
   : IsConnMap n (fmap (iterated_loops k) f).
 Proof.
   induction k in n, C |- *.
   - exact C.
-  - apply conn_map_fmap_loops.
-    apply IHk.
-    exact C.
+  - apply isconnmap_fmap_loops, IHk, C.
 Defined.
 
-(** It follows that loop spaces "commute with images". *)
+(** A converse to [isconnmap_fmap_loops] for connected types. *)
+Instance isconnmap_isconnmap_fmap_loops `{Univalence} {n : trunc_index}
+  {A B : pType} `{IsConnected 0 A} `{IsConnected 0 B}
+  (f : A ->* B) {cfmap : IsConnMap n (fmap loops f)}
+  : IsConnMap n.+1 f.
+Proof.
+  apply (isconnmap_isconnmap_ap_surj n).
+  - rapply (isconnmap_isconnected (-1)).
+  - rapply (conn_point_elim (-1)).
+    rapply (conn_point_elim (-1)).
+    intro p.
+    exact (isconnected_equiv' _ _ (hfiber_ap_loops f p) (cfmap _)).
+Defined.
+
+(** It follows from [istrunc_fmap_loops] and [isconnmap_fmap_loops] that loop spaces "commute with images". *)
 Definition equiv_loops_image `{Univalence} n {A B : pType} (f : A ->* B)
   : loops ([image n.+1 f, factor1 (image n.+1 f) (point A)])
   <~> image n (fmap loops f).
